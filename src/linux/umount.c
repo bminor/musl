a5dec11306d7988ec136bd86143591257dd3c56f
Rich Felker <dalias@aerifal.cx>
2011-02-13 21:28:43 -0500

    use umount2 syscall for umount (new targets lack old 1-arg umount)

--- src/linux/umount.c
+++ src/linux/umount.c
@@ -1,8 +1,7 @@
 #include <sys/mount.h>
-#define SYSCALL_STANDALONE
 #include "syscall.h"
 
 int umount(const char *special)
 {
-	return syscall1(__NR_umount, (long)special);
+	return syscall2(__NR_umount2, (long)special, 0);
 }
