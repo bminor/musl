a130d33cad10546dd6b9bc2be71b0e3f5b0cf06b
Rich Felker <dalias@aerifal.cx>
2011-02-13 23:26:51 -0500

    fixed missing cast in the non-i386 version of shmat (preparation for ports)

--- src/ipc/shmat.c
+++ src/ipc/shmat.c
@@ -5,7 +5,7 @@
 #ifdef __NR_shmat
 void *shmat(int id, const void *addr, int flag)
 {
-	return syscall3(__NR_shmat, id, (long)addr, flag);
+	return (void *)syscall3(__NR_shmat, id, (long)addr, flag);
 }
 #else
 void *shmat(int id, const void *addr, int flag)
