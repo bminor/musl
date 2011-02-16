e3657ba5992468f0340efdb741ec32ac0e0acb5c
Rich Felker <dalias@aerifal.cx>
2011-02-16 09:50:57 -0500

    patch by njk, simplifies thread register initialization for x86_64
    It's not necessary to save any registers on the stack across syscall in
    x86_64 __set_thread_area.  Don't waste cycles or bytes on it.

--- src/thread/x86_64/__set_thread_area.s
+++ src/thread/x86_64/__set_thread_area.s
@@ -3,13 +3,9 @@
 .global __set_thread_area
 .type   __set_thread_area,%function
 __set_thread_area:
-        push %rbx               /* save x86_64 abi clobbered registers */
-        push %r11
         mov %rdi,%rsi           /* shift for syscall */
         movl $0x1002,%edi       /* SET_FS register */
         movl $158,%eax          /* set fs segment to */
         syscall                 /* arch_prctl(SET_FS, arg)*/
-        pop %r11                /* restore clobbered registers */
-        pop %rbx
 	ret
 .size __set_thread_area,.-__set_thread_area
