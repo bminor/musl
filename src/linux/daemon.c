187fe29d5b89644b68cade75a34257a1c32a75f6
Rich Felker <dalias@aerifal.cx>
2011-02-17 10:30:00 -0500

    make daemon try the operations that might fail before fork rather than after

--- src/linux/daemon.c
+++ src/linux/daemon.c
@@ -3,7 +3,16 @@
 
 int daemon(int nochdir, int noclose)
 {
-	int fd;
+	if (!nochdir && chdir("/"))
+		return -1;
+	if (!noclose) {
+		int fd, failed = 0;
+		if ((fd = open("/dev/null", O_RDWR)) < 0) return -1;
+		if (dup2(fd, 0) < 0 || dup2(fd, 1) < 0 || dup2(fd, 2) < 0)
+			failed++;
+		if (fd > 2) close(fd);
+		if (failed) return -1;
+	}
 
 	switch(fork()) {
 	case 0: break;
@@ -17,17 +26,6 @@ int daemon(int nochdir, int noclose)
 	case 0: break;
 	case -1: return -1;
 	default: _exit(0);
-	}
-
-	if (!nochdir && chdir("/"))
-		return -1;
-	if (!noclose) {
-		int failed = 0;
-		if ((fd = open("/dev/null", O_RDWR)) < 0) return -1;
-		if (dup2(fd, 0) < 0 || dup2(fd, 1) < 0 || dup2(fd, 2) < 0)
-			failed++;
-		if (fd > 2) close(fd);
-		if (failed) return -1;
 	}
 
 	return 0;
