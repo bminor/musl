19eb13b9a4cf2f787f60b6e2a6d26a6cd7d3ffd2
Rich Felker <dalias@aerifal.cx>
2011-02-19 11:04:36 -0500

    race condition fix: block all signals before decrementing thread count
    the existence of a (kernelspace) thread must never have observable
    effects after the thread count is decremented. if signals are not
    blocked, it could end up handling the signal for rsyscall and
    contributing towards the count of threads which have changed ids,
    causing a thread to be missed. this could lead to one thread retaining
    unwanted privilege level.
    
    this change may also address other subtle race conditions in
    application code that uses signals.

--- src/thread/x86_64/__unmapself.s
+++ src/thread/x86_64/__unmapself.s
@@ -3,19 +3,6 @@
 .global __unmapself
 .type   __unmapself,%function
 __unmapself:
-	call 1f         /* glibc ABI compat */
-	.long -1
-	.long -1
-1:      push %rsi       /* save arg2 for munmap */
-	push %rdx       /* save arg3 for munmap */
-	mov %rdi,%rsi   /* rt_sigprocmask() args: move arg1 to rsi */
-	xor %rdi,%rdi
-	xor %rdx,%rdx
-	movq $8,%r10
-	movl $14,%eax   /* __NR_rt_sigprocmask */
-	syscall         /* call rt_sigprocmask(0,arg1,0,8) */
-	pop %rsi        /* munmap() args: reload from stack */
-	pop %rdi
 	movl $11,%eax   /* __NR_munmap */
 	syscall         /* munmap(arg2,arg3) */
 	xor %rdi,%rdi   /* exit() args: always return success */
