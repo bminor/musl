5d0d6d983664cc3816d63432397271719dade83c
Rich Felker <dalias@aerifal.cx>
2011-03-07 18:32:36 -0500

    add macros for use with d_type extension field in dirent

--- include/dirent.h
+++ include/dirent.h
@@ -34,6 +34,20 @@ int            dirfd(DIR *);
 int alphasort(const struct dirent **, const struct dirent **);
 int scandir(const char *, struct dirent ***, int (*)(const struct dirent *), int (*)(const struct dirent **, const struct dirent **));
 
+#ifdef _GNU_SOURCE
+#define DT_UNKNOWN 0
+#define DT_FIFO 1
+#define DT_CHR 2
+#define DT_DIR 4
+#define DT_BLK 6
+#define DT_REG 8
+#define DT_LNK 10
+#define DT_SOCK 12
+#define DT_WHT 14
+#define IFTODT(x) ((x)>>12 & 017)
+#define DTTOIF(x) ((x)<<12)
+#endf
+
 #ifdef __cplusplus
 extern }
 #endif
