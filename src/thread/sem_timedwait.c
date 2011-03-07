9fe28f703d44ff1ddecdca945bfb5fc87de5e8ad
Rich Felker <dalias@aerifal.cx>
2011-03-07 18:09:24 -0500

    fix off-by-one error in sem_(timed)wait (using old sem value instead of new)

--- src/thread/sem_timedwait.c
+++ src/thread/sem_timedwait.c
@@ -7,7 +7,7 @@ int sem_timedwait(sem_t *sem, const struct timespec *at)
 
 	for (;;) {
 		if (a_fetch_add(sem->__val, -1) > 0) return 0;
-		val = a_fetch_add(sem->__val, 1);
+		val = a_fetch_add(sem->__val, 1)+1;
 		CANCELPT_BEGIN;
 		if (val <= 0 && __timedwait(sem->__val, val, CLOCK_REALTIME, at, 0) == ETIMEDOUT) {
 			errno = ETIMEDOUT;
