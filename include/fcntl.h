ac12eb91bd7aa6f1f1642de1350c4f29ea0f4d08
Rich Felker <dalias@aerifal.cx>
2011-03-08 12:35:35 -0500

    fcntl.h should make availabel the mode constants from sys/stat.h
    also remove some legacy nonsense that crept in.

--- include/fcntl.h
+++ include/fcntl.h
@@ -36,6 +36,24 @@ int posix_fallocate(int, off_t, off_t);
 #define SEEK_CUR 1
 #define SEEK_END 2
 
+#ifndef S_IRUSR
+#define S_ISUID 04000
+#define S_ISGID 02000
+#define S_ISVTX 01000
+#define S_IRUSR 0400
+#define S_IWUSR 0200
+#define S_IXUSR 0100
+#define S_IRWXU 0700
+#define S_IRGRP 0040
+#define S_IWGRP 0020
+#define S_IXGRP 0010
+#define S_IRWXG 0070
+#define S_IROTH 0004
+#define S_IWOTH 0002
+#define S_IXOTH 0001
+#define S_IRWXO 0007
+#endif
+
 #ifdef __cplusplus
 }
 #endif
--- include/sys/stat.h
+++ include/sys/stat.h
@@ -47,29 +47,23 @@ extern "C" {
 #define S_ISLNK(mode)  (((mode) & S_IFMT) == S_IFLNK)
 #define S_ISSOCK(mode) (((mode) & S_IFMT) == S_IFSOCK)
 
+#ifndef S_IRUSR
 #define S_ISUID 04000
 #define S_ISGID 02000
 #define S_ISVTX 01000
-
-#define S_IREAD  0400
-#define S_IWRITE 0200
-#define S_IEXEC  0100
-
 #define S_IRUSR 0400
 #define S_IWUSR 0200
 #define S_IXUSR 0100
 #define S_IRWXU 0700
-
 #define S_IRGRP 0040
 #define S_IWGRP 0020
 #define S_IXGRP 0010
 #define S_IRWXG 0070
-
 #define S_IROTH 0004
 #define S_IWOTH 0002
 #define S_IXOTH 0001
 #define S_IRWXO 0007
-
+#endif
 
 int stat(const char *, struct stat *);
 int fstat(int, struct stat *);
