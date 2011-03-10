3f5420bcda134de80ed6b0f0da1d7d23f147a4cc
Rich Felker <dalias@aerifal.cx>
2011-03-09 20:23:44 -0500

    make fork properly initialize the main thread in the child process

--- src/process/fork.c
+++ src/process/fork.c
@@ -1,12 +1,19 @@
 #include <unistd.h>
 #include "syscall.h"
 #include "libc.h"
+#include "pthread_impl.h"
 
 pid_t fork(void)
 {
 	pid_t ret;
 	if (libc.fork_handler) libc.fork_handler(-1);
 	ret = syscall0(__NR_fork);
+	if (libc.lock && !ret) {
+		pthread_t self = __pthread_self();
+		self->pid = syscall0(__NR_getpid);
+		self->tid = syscall0(__NR_gettid);
+		libc.threads_minus_1 = 0;
+	}
 	if (libc.fork_handler) libc.fork_handler(!ret);
 	return ret;
 }
