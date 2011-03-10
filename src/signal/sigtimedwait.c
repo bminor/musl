5b9429adb78aeb5e98fa26bd9b2b3eef981e4c52
Rich Felker <dalias@aerifal.cx>
2011-03-10 10:59:50 -0500

    make sigtimedwait a cancellation point

--- src/signal/sigtimedwait.c
+++ src/signal/sigtimedwait.c
@@ -1,12 +1,15 @@
 #include <signal.h>
 #include <errno.h>
 #include "syscall.h"
+#include "libc.h"
 
 int sigtimedwait(const sigset_t *mask, siginfo_t *si, const struct timespec *timeout)
 {
 	int ret;
+	CANCELPT_BEGIN;
 	do {
 		ret = syscall4(__NR_rt_sigtimedwait, (long)mask, (long)si, (long)timeout, SYSCALL_SIGSET_SIZE);
 	} while (ret<0 && errno==EINTR);
+	CANCELPT_END;
 	return ret;
 }
