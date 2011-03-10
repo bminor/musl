0558683d3db05bc7022be57bc8ece9699df24228
Rich Felker <dalias@aerifal.cx>
2011-03-10 10:26:16 -0500

    fix sigsuspend syscall

--- src/signal/sigsuspend.c
+++ src/signal/sigsuspend.c
@@ -3,5 +3,5 @@
 
 int sigsuspend(const sigset_t *mask)
 {
-	return syscall2(__NR_rt_sigsuspend, (long)mask, sizeof(sigset_t));
+	return syscall2(__NR_rt_sigsuspend, (long)mask, SYSCALL_SIGSET_SIZE);
 }
