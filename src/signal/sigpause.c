c8c4ef7d4404b94483c68b7479eb98b9b09f996c
Rich Felker <dalias@aerifal.cx>
2011-03-10 11:02:29 -0500

    remove useless return value checks for functions that cannot fail

--- src/signal/sigpause.c
+++ src/signal/sigpause.c
@@ -4,8 +4,7 @@
 int sigpause(int sig)
 {
 	sigset_t mask;
-
-	if (sigprocmask(0, NULL, &mask) < 0 || sigdelset(&mask, sig) < 0)
-		return -1;
+	sigprocmask(0, 0, &mask);
+	sigdelset(&mask, sig);
 	return sigsuspend(&mask);
 }
