11dbbe9fbac33ede053803ebf7193550ec5cfb48
Rich Felker <dalias@aerifal.cx>
2011-03-10 11:06:50 -0500

    more cancellation points: tcdrain, clock_nanosleep

--- src/termios/tcdrain.c
+++ src/termios/tcdrain.c
@@ -1,7 +1,12 @@
 #include <termios.h>
 #include <sys/ioctl.h>
+#include "libc.h"
 
 int tcdrain(int fd)
 {
-	return ioctl(fd, TCSBRK, 1);
+	int ret;
+	CANCELPT_BEGIN;
+	ret = ioctl(fd, TCSBRK, 1);
+	CANCELPT_END;
+	return ret;
 }
--- src/time/clock_nanosleep.c
+++ src/time/clock_nanosleep.c
@@ -1,8 +1,13 @@
 #define SYSCALL_RETURN_ERRNO
 #include <time.h>
 #include "syscall.h"
+#include "libc.h"
 
 int clock_nanosleep(clockid_t clk, int flags, const struct timespec *req, struct timespec *rem)
 {
-	return syscall4(__NR_clock_nanosleep, clk, flags, (long)req, (long)rem);
+	int ret;
+	CANCELPT_BEGIN;
+	ret = syscall4(__NR_clock_nanosleep, clk, flags, (long)req, (long)rem);
+	CANCELPT_END;
+	return ret;
 }
