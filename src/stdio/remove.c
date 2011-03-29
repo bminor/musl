0b240ccf523b9af23dd1efa78274f397fcc90cdd
Rich Felker <dalias@aerifal.cx>
2011-03-29 08:24:28 -0400

    learned something new - remove is supposed to support directories on POSIX

--- src/stdio/remove.c
+++ src/stdio/remove.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <errno.h>
 #include "syscall.h"
 
 int remove(const char *path)
 {
-	return syscall(SYS_unlink, path);
+	return (syscall(SYS_unlink, path) && errno == EISDIR)
+		? syscall(SYS_rmdir, path) : 0;
 }
