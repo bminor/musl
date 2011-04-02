8de03e1a90e60f62806c488cfa4985f99e869f87
Rich Felker <dalias@aerifal.cx>
2011-04-02 13:55:54 -0400

    don't disable seeking after first seek failure
    this could cause problems if the application uses dup2(fd,fileno(f))
    to redirect, and the old fd was not seekable but the new fd is.

--- src/stdio/__stdio_seek.c
+++ src/stdio/__stdio_seek.c
@@ -1,11 +1,5 @@
 #include "stdio_impl.h"
 
-static off_t retneg1(FILE *f, off_t off, int whence)
-{
-	errno = ESPIPE;
-	return -1;
-}
-
 off_t __stdio_seek(FILE *f, off_t off, int whence)
 {
 	off_t ret;
@@ -15,7 +9,5 @@ off_t __stdio_seek(FILE *f, off_t off, int whence)
 #else
 	ret = syscall(SYS_lseek, f->fd, off, whence);
 #endif
-	/* Detect unseekable files and optimize future failures out */
-	if (ret < 0 && errno == ESPIPE) f->seek = retneg1;
 	return ret;
 }
