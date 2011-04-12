c546be175c169399420fff4706cd1d4fcefd7d31
Rich Felker <dalias@aerifal.cx>
2011-04-12 13:04:17 -0400

    move bswap functions to static inline in byteswap.h

--- src/misc/bswap_32.c
+++
@@ -1,7 +0,0 @@
-#include <endian.h>
-#include <stdint.h>
-
-uint32_t bswap_32(uint32_t x)
-{
-	return x>>24 | x>>8&0xff00 | x<<8&0xff0000 | x<<24;
-}
--- src/misc/bswap_64.c
+++
@@ -1,9 +0,0 @@
-#include <endian.h>
-#include <stdint.h>
-
-uint32_t bswap_32(uint32_t);
-
-uint64_t bswap_64(uint64_t x)
-{
-	return bswap_32(x)+0LL<<32 | bswap_32(x>>32);
-}
