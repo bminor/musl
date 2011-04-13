6f0259a4fc7d827db027fe122377687ef780e17c
Rich Felker <dalias@aerifal.cx>
2011-04-13 14:52:23 -0400

    fix bug whereby getopt_long allowed mismatch in last char of option name

--- src/linux/getopt_long.c
+++ src/linux/getopt_long.c
@@ -14,7 +14,7 @@ static int __getopt_long(int argc, char *const *argv, const char *optstring, con
 			const char *name = longopts[i].name;
 			char *opt = argv[optind]+1;
 			if (*opt == '-') opt++;
-			while (*name && *name++ == *opt++);
+			for (; *name && *name == *opt; name++, opt++);
 			if (*name || (*opt && *opt != '=')) continue;
 			if (*opt == '=') {
 				if (!longopts[i].has_arg) continue;
