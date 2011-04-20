29a15ad077bd7af683d3c838d21be1c216f22f8f
Rich Felker <dalias@aerifal.cx>
2011-04-20 19:15:27 -0400

    fix initgroups (uninitialized count passed to getgrouplist)

--- src/linux/initgroups.c
+++ src/linux/initgroups.c
@@ -9,7 +9,7 @@ int setgroups(size_t, const gid_t *);
 int initgroups(const char *user, gid_t gid)
 {
 	gid_t groups[NGROUPS_MAX];
-	int count;
+	int count = NGROUPS_MAX;
 	if (getgrouplist(user, gid, groups, &count) < 0) return -1;
 	return setgroups(count, groups);
 }
