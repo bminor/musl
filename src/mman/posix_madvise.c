23573509244d4d5b4dc6d114b5807f72418f3411
Rich Felker <dalias@aerifal.cx>
2011-04-20 15:25:28 -0400

    support posix_madvise (previous a stub)
    the check against MADV_DONTNEED to because linux MADV_DONTNEED
    semantics conflict dangerously with the POSIX semantics

--- src/mman/posix_madvise.c
+++ src/mman/posix_madvise.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE
 #include <sys/mman.h>
 
 int posix_madvise(void *addr, size_t len, int advice)
 {
-	return 0;
+	if (advice == MADV_DONTNEED) return 0;
+	return -__syscall(SYS_madvise, addr, len, advice);
 }
