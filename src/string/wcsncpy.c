e98136207ad1a6df1cdc1578e4ad56f8f0db4047
Rich Felker <dalias@aerifal.cx>
2011-05-22 21:54:42 -0400

    fix wcsncpy writing past end of buffer

--- src/string/wcsncpy.c
+++ src/string/wcsncpy.c
@@ -3,7 +3,7 @@
 wchar_t *wcsncpy(wchar_t *d, const wchar_t *s, size_t n)
 {
 	wchar_t *a = d;
-	while (n && (*d++ = *s++)) n--;
+	for (; n && (*d = *s); n--, d++, s++);
 	wmemset(d, 0, n);
 	return a;
 }
