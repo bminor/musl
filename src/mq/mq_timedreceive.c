86f8c72bb1cc1fad05e1ed1b2a6f4433defc9cf7
Rich Felker <dalias@aerifal.cx>
2011-06-07 11:14:39 -0400

    mq send/recv functions are cancellation points

--- src/mq/mq_timedreceive.c
+++ src/mq/mq_timedreceive.c
@@ -3,5 +3,5 @@
 
 ssize_t mq_timedreceive(mqd_t mqd, char *msg, size_t len, unsigned *prio, const struct timespec *at)
 {
-	return syscall(SYS_mq_timedreceive, mqd, msg, len, prio, at);
+	return syscall_cp(SYS_mq_timedreceive, mqd, msg, len, prio, at);
 }
--- src/mq/mq_timedsend.c
+++ src/mq/mq_timedsend.c
@@ -3,5 +3,5 @@
 
 int mq_timedsend(mqd_t mqd, const char *msg, size_t len, unsigned prio, const struct timespec *at)
 {
-	return syscall(SYS_mq_timedsend, mqd, msg, len, prio, at);
+	return syscall_cp(SYS_mq_timedsend, mqd, msg, len, prio, at);
 }
