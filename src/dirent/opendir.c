f937031800e3130b3b4bb42d0d547bb69a5d32db
Rich Felker <dalias@aerifal.cx>
2011-06-25 01:38:25 -0400

    optimize opendir using O_CLOEXEC

--- src/dirent/opendir.c
+++ src/dirent/opendir.c
@@ -14,9 +14,8 @@ DIR *opendir(const char *name)
 	int fd;
 	DIR *dir;
 
-	if ((fd = open(name, O_RDONLY|O_DIRECTORY)) < 0)
+	if ((fd = open(name, O_RDONLY|O_DIRECTORY|O_CLOEXEC)) < 0)
 		return 0;
-	fcntl(fd, F_SETFD, FD_CLOEXEC);
 	if (!(dir = calloc(1, sizeof *dir))) {
 		__syscall(SYS_close, fd);
 		return 0;
