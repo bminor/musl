230f1813ad8264f09d5247fa0ed1e8e361c21e0e
Rich Felker <dalias@aerifal.cx>
2011-06-27 01:02:28 -0400

    further fixup dlfcn.h

--- include/dlfcn.h
+++ include/dlfcn.h
@@ -1,19 +1,25 @@
 #ifndef	_DLFCN_H
 #define	_DLFCN_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define RTLD_LAZY   1
 #define RTLD_NOW    2
 #define RTLD_GLOBAL 256
 #define RTLD_LOCAL  0
 
-#if 1
-#define RTLD_NEXT    ((void *) -1l)
-#define RTLD_DEFAULT ((void *) 0)
-#endif
+#define RTLD_NEXT    ((void *)-1)
+#define RTLD_DEFAULT ((void *)0)
 
 int    dlclose(void *);
 char  *dlerror(void);
 void  *dlopen(const char *, int);
 void  *dlsym(void *, const char *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
