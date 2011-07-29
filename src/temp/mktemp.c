aed707f679cce80afd929f0efaf080f1e8481330
Rich Felker <dalias@aerifal.cx>
2011-07-28 22:03:54 -0400

    remove ugly prng from mk*temp and just re-poll time on retry

--- src/temp/mktemp.c
+++ src/temp/mktemp.c
@@ -13,20 +13,19 @@ char *__mktemp(char *template)
 	struct timespec ts;
 	size_t i, l = strlen(template);
 	int retries = 10000;
-	unsigned long r, t;
+	unsigned long r;
 
 	if (l < 6 || strcmp(template+l-6, "XXXXXX")) {
 		errno = EINVAL;
 		*template = 0;
 		return template;
 	}
-	clock_gettime(CLOCK_REALTIME, &ts);
-	r = ts.tv_nsec + (uintptr_t)&ts / 16 + (uintptr_t)template;
 	while (retries--) {
-		for (t=r, i=1; i<=6; i++, t>>=4)
-			template[l-i] = 'A'+(t&15);
+		clock_gettime(CLOCK_REALTIME, &ts);
+		r = ts.tv_nsec + (uintptr_t)&ts / 16 + (uintptr_t)template;
+		for (i=1; i<=6; i++, r>>=4)
+			template[l-i] = 'A'+(r&15);
 		if (access(template, F_OK) < 0) return template;
-		r = r * 1103515245 + 12345;
 	}
 	*template = 0;
 	errno = EEXIST;
