813d37839b97960d38f10ea93d9c4a391c1c91a5
Rich Felker <dalias@aerifal.cx>
2011-09-16 14:29:24 -0400

    use a_swap rather than old name a_xchg

--- src/thread/__lock.c
+++ src/thread/__lock.c
@@ -5,7 +5,7 @@ void __lock(volatile int *l)
 	int spins=10000;
 	/* Do not use futexes because we insist that unlocking is a simple
 	 * assignment to optimize non-pathological code with no contention. */
-	while (a_xchg(l, 1))
+	while (a_swap(l, 1))
 		if (spins) spins--, a_spin();
 		else __syscall(SYS_sched_yield);
 }
