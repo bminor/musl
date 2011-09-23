d5bde7babb88f0279f3ad592d585f3f954090696
Rich Felker <dalias@aerifal.cx>
2011-09-22 22:56:06 -0400

    "optimize" arm __pthread_self
    actually this is just to avoid gcc being stupid and refusing to inline
    the function version, even when the size cost is essentially identical
    whether it's inlined or not.

--- arch/arm/pthread_arch.h
+++ arch/arm/pthread_arch.h
@@ -1,7 +1,4 @@
-static inline struct pthread *__pthread_self()
-{
-	return ((void *(*)(void))0xffff0fe0)();
-}
+#define __pthread_self ((pthread_t (*)(void))0xffff0fe0)
 
 #define CANCEL_REG_SP 16
 #define CANCEL_REG_IP 18
