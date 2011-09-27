f135ef3f343b824c5039e3dbbc31b5545fbe7a12
Rich Felker <dalias@aerifal.cx>
2011-09-27 16:47:00 -0400

    fix missing va_end in prctl syscall wrapper

--- src/linux/prctl.c
+++ src/linux/prctl.c
@@ -9,5 +9,6 @@ int prctl(int op, ...)
 	va_list ap;
 	va_start(ap, op);
 	for (i=0; i<4; i++) x[i] = va_arg(ap, unsigned long);
+	va_end(ap);
 	return syscall(SYS_prctl, op, x[0], x[1], x[2], x[3]);
 }
