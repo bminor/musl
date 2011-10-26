26120950e26478740a29fd0b1fd78a0bd6b880c8
Rich Felker <dalias@aerifal.cx>
2011-10-26 00:28:47 -0400

    report sem value overflows in sem_post
    this is not required by the standard, but it's nicer than corrupting
    the state and rather inexpensive.

--- src/thread/sem_post.c
+++ src/thread/sem_post.c
@@ -7,6 +7,10 @@ int sem_post(sem_t *sem)
 	do {
 		val = sem->__val[0];
 		waiters = sem->__val[1];
+		if (val == SEM_VALUE_MAX) {
+			errno = EOVERFLOW;
+			return -1;
+		}
 	} while (a_cas(sem->__val, val, val+1+(val<0)) != val);
 	if (val<0 || waiters) __wake(sem->__val, 1, 0);
 	return 0;
