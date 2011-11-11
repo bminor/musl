0c4188f6d76fad021a93eb1012630c717bda80a1
Rich Felker <dalias@aerifal.cx>
2011-11-10 20:44:44 -0500

    fix signed overflows at most-negative values in ato(i|l|ll)
    patch by Pascal Cuoq (with minor tweaks to comments)

--- src/stdlib/atoi.c
+++ src/stdlib/atoi.c
@@ -9,7 +9,8 @@ int atoi(const char *s)
 	case '-': neg=1;
 	case '+': s++;
 	}
+	/* Compute n as a negative number to avoid overflow on INT_MIN */
 	while (isdigit(*s))
-		n = 10*n + *s++ - '0';
-	return neg ? -n : n;
+		n = 10*n - (*s++ - '0');
+	return neg ? n : -n;
 }
--- src/stdlib/atoll.c
+++ src/stdlib/atoll.c
@@ -10,7 +10,8 @@ long long atoll(const char *s)
 	case '-': neg=1;
 	case '+': s++;
 	}
+	/* Compute n as a negative number to avoid overflow on LLONG_MIN */
 	while (isdigit(*s))
-		n = 10*n + *s++ - '0';
-	return neg ? -n : n;
+		n = 10*n - (*s++ - '0');
+	return neg ? n : -n;
 }
