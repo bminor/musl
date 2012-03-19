6f26cf3dacf9d426bbd18cecb4d486da56c17351
Rich Felker <dalias@aerifal.cx>
2012-03-18 21:43:43 -0400

    asm exponential functions for i386

---
+++ src/math/i386/exp.s
@@ -0,0 +1,46 @@
+.global exp2f
+.type exp2f,@function
+exp2f:
+	flds 4(%esp)
+	jmp 1f
+
+.global exp2l
+.type exp2l,@function
+exp2l:
+	fldt 4(%esp)
+	jmp 1f
+
+.global expf
+.type expf,@function
+expf:
+	flds 4(%esp)
+	jmp 2f
+
+.global expl
+.type expl,@function
+expl:
+	fldt 4(%esp)
+	jmp 2f
+
+.global exp
+.type exp,@function
+exp:
+	fldl 4(%esp)
+2:	fldl2e
+	fmulp
+	jmp 1f
+
+.global exp2
+.type exp2,@function
+exp2:
+	fldl 4(%esp)
+1:	fld %st(0)
+	frndint
+	fxch %st(1)
+	fsub %st(1)
+	f2xm1
+	fld1
+	faddp
+	fscale
+	fstp %st(1)
+	ret
