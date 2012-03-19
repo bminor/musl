8051e08e10d2b739fcfcbc6bc7466e8d77fa49f1
nsz <nsz@port70.net>
2012-03-19 10:54:07 +0100

    simplify scalbn*.c implementations
    The old scalbn.c was wrong and slow, the new one is just slow.
    (scalbn(0x1p+1023,-2097) should give 0x1p-1074, but the old code gave 0)

--- src/math/scalbn.c
+++ src/math/scalbn.c
@@ -1,62 +1,28 @@
-/* origin: FreeBSD /usr/src/lib/msun/src/s_scalbn.c */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
-/*
- * scalbn (double x, int n)
- * scalbn(x,n) returns x* 2**n  computed by  exponent
- * manipulation rather than by actually performing an
- * exponentiation or a multiplication.
- */
-
 #include "libm.h"
-
-static const double
-two54  = 1.80143985094819840000e+16, /* 0x43500000, 0x00000000 */
-twom54 = 5.55111512312578270212e-17, /* 0x3C900000, 0x00000000 */
-huge   = 1.0e+300,
-tiny   = 1.0e-300;
 
 double scalbn(double x, int n)
 {
-// FIXME: k+n check depends on signed int overflow.. use unsigned hx
-// TODO: when long != int:
-//       scalbln(x,long n) { if(n>9999)n=9999; else if(n<-9999)n=-9999; return scalbn(x,n); }
-// TODO: n < -50000 ...
-	int32_t k,hx,lx;
+	double scale;
 
-	EXTRACT_WORDS(hx, lx, x);
-	k = (hx&0x7ff00000)>>20;                /* extract exponent */
-	if (k == 0) {                           /* 0 or subnormal x */
-		if ((lx|(hx&0x7fffffff)) == 0)  /* +-0 */
-			return x;
-		x *= two54;
-		GET_HIGH_WORD(hx, x);
-		k = ((hx&0x7ff00000)>>20) - 54;
-		if (n < -50000)
-			return tiny*x;          /*underflow*/
+	if (n > 1023) {
+		x *= 0x1p1023;
+		n -= 1023;
+		if (n > 1023) {
+			x *= 0x1p1023;
+			n -= 1023;
+			if (n > 1023)
+				return x * 0x1p1023;
+		}
+	} else if (n < -1022) {
+		x *= 0x1p-1022;
+		n += 1022;
+		if (n < -1022) {
+			x *= 0x1p-1022;
+			n += 1022;
+			if (n < -1022)
+				return x * 0x1p-1022;
+		}
 	}
-	if (k == 0x7ff)                         /* NaN or Inf */
-		return x + x;
-	k = k + n;
-	if (k > 0x7fe)
-		return huge*copysign(huge, x);  /* overflow  */
-	if (k > 0) {                            /* normal result */
-		SET_HIGH_WORD(x, (hx&0x800fffff)|(k<<20));
-		return x;
-	}
-	if (k <= -54)
-		if (n > 50000)      /* in case integer overflow in n+k */
-			return huge*copysign(huge, x);  /*overflow*/
-		return tiny*copysign(tiny, x);  /*underflow*/
-	k += 54;                                /* subnormal result */
-	SET_HIGH_WORD(x, (hx&0x800fffff)|(k<<20));
-	return x*twom54;
+	INSERT_WORDS(scale, (uint32_t)(0x3ff+n)<<20, 0);
+	return x * scale;
 }
--- src/math/scalbnl.c
+++ src/math/scalbnl.c
@@ -1,21 +1,3 @@
-/* origin: FreeBSD /usr/src/lib/msun/src/s_scalbnl.c */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
-/*
- * scalbnl (long double x, int n)
- * scalbnl(x,n) returns x* 2**n  computed by  exponent
- * manipulation rather than by actually performing an
- * exponentiation or a multiplication.
- */
-
 #include "libm.h"
 
 #if LDBL_MANT_DIG == 53 && LDBL_MAX_EXP == 1024
@@ -24,40 +6,31 @@ long double scalbnl(long double x, int n)
 	return scalbn(x, n);
 }
 #elif (LDBL_MANT_DIG == 64 || LDBL_MANT_DIG == 113) && LDBL_MAX_EXP == 16384
-static const long double
-huge = 0x1p16000L,
-tiny = 0x1p-16000L;
-
 long double scalbnl(long double x, int n)
 {
-	union IEEEl2bits u;
-	int k;
+	union IEEEl2bits scale;
 
-	u.e = x;
-	k = u.bits.exp;                    /* extract exponent */
-	if (k == 0) {                      /* 0 or subnormal x */
-		if ((u.bits.manh|u.bits.manl) == 0)  /* +-0 */
-			return x;
-		u.e *= 0x1p128;
-		k = u.bits.exp - 128;
-		if (n < -50000)
-			return tiny*x;  /*underflow*/
+	if (n > 16383) {
+		x *= 0x1p16383L;
+		n -= 16383;
+		if (n > 16383) {
+			x *= 0x1p16383L;
+			n -= 16383;
+			if (n > 16383)
+				return x * 0x1p16383L;
+		}
+	} else if (n < -16382) {
+		x *= 0x1p-16382L;
+		n += 16382;
+		if (n < -16382) {
+			x *= 0x1p-16382L;
+			n += 16382;
+			if (n < -16382)
+				return x * 0x1p-16382L;
+		}
 	}
-	if (k == 0x7fff)                   /* NaN or Inf */
-		return x + x;
-	k = k + n;
-	if (k >= 0x7fff)
-		return huge*copysignl(huge, x);  /* overflow  */
-	if (k > 0) {                       /* normal result */
-		u.bits.exp = k;
-		return u.e;
-	}
-	if (k <= -128)
-		if (n > 50000)  /* in case integer overflow in n+k */
-			return huge*copysign(huge, x);  /*overflow*/
-		return tiny*copysign(tiny, x);  /*underflow*/
-	k += 128;                          /* subnormal result */
-	u.bits.exp = k;
-	return u.e*0x1p-128;
+	scale.e = 1.0L;
+	scale.bits.exp = 0x3fff + n;
+	return x * scale.e;
 }
 #endif
