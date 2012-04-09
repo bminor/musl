3be616c1df7ee176b5e00b9f493136ca7385ec46
Rich Felker <dalias@aerifal.cx>
2012-04-09 16:22:05 -0400

    fix alloca issue in stdlib.h too
    I forgot _GNU_SOURCE also has it declared here...

--- include/stdlib.h
+++ include/stdlib.h
@@ -128,7 +128,7 @@ void lcong48 (unsigned short [7]);
 #endif
 
 #if defined(_GNU_SOURCE)
-void *alloca(size_t);
+#include <alloca.h>
 char *mktemp (char *);
 void *valloc (size_t);
 void *memalign(size_t, size_t);
