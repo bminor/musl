cc762434d91a2f441a1d2f44962ab1d4854b607b
Rich Felker <dalias@aerifal.cx>
2012-04-16 15:36:18 -0400

    fix buggy limiter handling in shgetc
    this is needed for upcoming new scanf

--- src/internal/shgetc.c
+++ src/internal/shgetc.c
@@ -4,7 +4,7 @@ void __shlim(FILE *f, off_t lim)
 {
 	f->shlim = lim;
 	f->shcnt = f->rend - f->rpos;
-	if (lim && f->rend - f->rpos > lim)
+	if (lim && f->shcnt > lim)
 		f->shend = f->rpos + lim;
 	else
 		f->shend = f->rend;
@@ -13,15 +13,14 @@ void __shlim(FILE *f, off_t lim)
 int __shgetc(FILE *f)
 {
 	int c;
-	if (f->shlim && f->shcnt >= f->shlim) {
+	if (f->shlim && f->shcnt >= f->shlim || (c=__uflow(f)) < 0) {
 		f->shend = 0;
 		return EOF;
 	}
-	c = __uflow(f);
 	if (f->shlim && f->rend - f->rpos > f->shlim - f->shcnt - 1)
 		f->shend = f->rpos + (f->shlim - f->shcnt - 1);
 	else
 		f->shend = f->rend;
-	if (f->rend) f->shcnt += f->rend - f->buf;
+	if (f->rend) f->shcnt += f->rend - f->rpos + 1;
 	return c;
 }
