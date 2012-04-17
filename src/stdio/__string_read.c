8b57a81577980e4b440addaa8f2bbed454be2a59
Rich Felker <dalias@aerifal.cx>
2012-04-17 13:17:01 -0400

    avoid depending on POSIX symbol in code used from plain C functions

--- src/stdio/__string_read.c
+++ src/stdio/__string_read.c
@@ -3,7 +3,9 @@
 size_t __string_read(FILE *f, unsigned char *buf, size_t len)
 {
 	char *src = f->cookie;
-	size_t k = strnlen(src, len+256);
+	size_t k = len+256;
+	char *end = memchr(src, 0, k);
+	if (end) k = end-src;
 	if (k < len) len = k;
 	memcpy(buf, src, len);
 	f->rpos = (void *)(src+len);
