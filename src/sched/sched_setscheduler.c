b4560a6da918ce3b63e87a78a3a3956f514e24ce
Rich Felker <dalias@aerifal.cx>
2012-05-03 23:19:29 -0400

    avoid setting nondefault scheduler too

--- src/sched/sched_setscheduler.c
+++ src/sched/sched_setscheduler.c
@@ -4,5 +4,5 @@
 int sched_setscheduler(pid_t pid, int sched, const struct sched_param *param)
 {
 	static const struct sched_param def;
-	return syscall(SYS_sched_setscheduler, pid, sched, &def);
+	return syscall(SYS_sched_setscheduler, pid, 0, &def);
 }
