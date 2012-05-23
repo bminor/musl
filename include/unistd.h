96601e3c617bb2122265419f0895730ed794f5af
Rich Felker <dalias@aerifal.cx>
2012-05-22 22:07:42 -0400

    _GNU_SOURCE implies all BSD features except ones GNU rejects

--- include/unistd.h
+++ include/unistd.h
@@ -13,7 +13,7 @@ extern "C" {
 #define SEEK_CUR 1
 #define SEEK_END 2
 
-#if defined(_BSD_SOURCE)
+#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 #define L_SET 0
 #define L_INCR 1
 #define L_XTND 2
