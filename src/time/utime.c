c37afdfdf393261e18364aed52571d0a5b011044
Rich Felker <dalias@aerifal.cx>
2012-05-24 10:55:58 -0400

    linux deprecated SYS_utime on some archs, so use SYS_utimes instead
    the old code could be kept for cases where SYS_utime is available, but
    it's not really worth the ifdef ugliness. and better to avoid
    deprecated stuff just in case the kernel devs ever get crazy enough to
    start removing it from archs where it was part of the ABI and breaking
    static bins...

--- src/time/utime.c
+++ src/time/utime.c
@@ -1,7 +1,14 @@
 #include <utime.h>
+#include <sys/time.h>
 #include "syscall.h"
 
 int utime(const char *path, const struct utimbuf *times)
 {
-	return syscall(SYS_utime, path, times);
+	if (times) {
+		struct timeval tv[2] = {
+			{ .tv_sec = times->actime },
+			{ .tv_sec = times->modtime } };
+		return syscall(SYS_utimes, path, tv);
+	}
+	return syscall(SYS_utimes, path, 0);
 }
