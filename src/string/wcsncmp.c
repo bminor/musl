054ba185999d29f6ae9a8e19549da95f783655b6
Rich Felker <dalias@aerifal.cx>
2012-05-26 18:04:17 -0400

    fix overrun (n essentially ignored) in wcsncmp
    bug report and solution by Richard Pennington

--- src/string/wcsncmp.c
+++ src/string/wcsncmp.c
@@ -2,6 +2,6 @@
 
 int wcsncmp(const wchar_t *l, const wchar_t *r, size_t n)
 {
-	for (; n && *l==*r && *l && *r; l++, r++);
+	for (; n && *l==*r && *l && *r; n--, l++, r++);
 	return n ? *l - *r : 0;
 }
