b94608ca10051e7911250aaa53f1435b0512018c
Rich Felker <dalias@aerifal.cx>
2012-05-27 14:49:55 -0400

    cleanup dynamic linker start code cruft
    two actual issues: one is that __dynlink no longer wants/needs a GOT
    pointer argument, so the code to generate that argument can be
    removed. the other issue was that in the i386 code, argc/argv were
    being loaded into registers that would be call-clobbered, then copied
    to preserved registers, rather than just being loaded into the proper
    call-preserved registers to begin with.
    
    this cleanup is in preparation for adding new dynamic linker
    functionality (ability to explicitly invoke the dynamic linker to run
    a program).

--- src/ldso/i386/start.s
+++ src/ldso/i386/start.s
@@ -2,16 +2,13 @@
 .global _start
 _start:
 	xor %ebp,%ebp
-	pop %ecx
-	mov %esp,%eax
+	pop %edi
+	mov %esp,%esi
 	and $-16,%esp
-	mov %eax,%esi
-	mov %ecx,%edi
 	push %ebp
-	call 1f
-1:	addl $_GLOBAL_OFFSET_TABLE_,(%esp)
-	push %eax
-	push %ecx
+	push %ebp
+	push %esi
+	push %edi
 	call __dynlink
 	mov %esi,%esp
 	push %edi
--- src/ldso/x86_64/start.s
+++ src/ldso/x86_64/start.s
@@ -3,7 +3,6 @@
 _start:
 	mov (%rsp),%rdi
 	lea 8(%rsp),%rsi
-	lea _GLOBAL_OFFSET_TABLE_(%rip),%rdx
 	call __dynlink
 	xor %edx,%edx
 	jmp *%rax
