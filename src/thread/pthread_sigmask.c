40bd1726b6aa93a8311a77a1fec6da0fe0489fd0
Rich Felker <dalias@aerifal.cx>
2012-06-02 20:04:27 -0400

    remove implementation-reserved bits when saving signal mask
    this fix is necessary because a program could be started with some of
    the implementation-reserved signals masked (e.g. due to exec having
    been called from a signal handler, or from a non-musl program) and
    then could obtain an invalid-to-use-later sigset_t as the old/saved
    signal mask.

--- src/thread/pthread_sigmask.c
+++ src/thread/pthread_sigmask.c
@@ -5,6 +5,16 @@
 
 int pthread_sigmask(int how, const sigset_t *set, sigset_t *old)
 {
+	int ret;
 	if (how > 2U) return EINVAL;
-	return -__syscall(SYS_rt_sigprocmask, how, set, old, 8);
+	ret = -__syscall(SYS_rt_sigprocmask, how, set, old, 8);
+	if (!ret && old) {
+		if (sizeof old->__bits[0] == 8) {
+			old->__bits[0] &= ~0x380000000ULL;
+		} else {
+			old->__bits[0] &= ~0x80000000UL;
+			old->__bits[1] &= ~0x3UL;
+		}
+	}
+	return ret;
 }
