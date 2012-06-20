9799560f79d37fb53d821b2f0b591a6ff260ebc3
Rich Felker <dalias@aerifal.cx>
2012-06-20 14:47:34 -0400

    fix invalid memory access in pclose

--- src/stdio/pclose.c
+++ src/stdio/pclose.c
@@ -3,8 +3,9 @@
 int pclose(FILE *f)
 {
 	int status;
+	pid_t pid = f->pipe_pid;
 	fclose(f);
-	while (waitpid(f->pipe_pid, &status, 0) == -1)
+	while (waitpid(pid, &status, 0) == -1)
 		if (errno != EINTR) return -1;
 	return status;
 }
