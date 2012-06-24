9cf9c39e48342a5d4007824d49b88132d93d83e3
Rich Felker <dalias@aerifal.cx>
2012-06-23 21:38:47 -0400

    add process_vm_readv and process_vm_writev syscall wrappers
    based on a patch submitted by Kristian L. <email@thexception.net>

--- include/sys/uio.h
+++ include/sys/uio.h
@@ -9,10 +9,19 @@ extern "C" {
 #define __NEED_ssize_t
 #define __NEED_struct_iovec
 
+#ifdef _GNU_SOURCE
+#define __NEED_pid_t
+#endif
+
 #include <bits/alltypes.h>
 
 ssize_t readv (int, const struct iovec *, int);
 ssize_t writev (int, const struct iovec *, int);
+
+#ifdef _GNU_SOURCE
+ssize_t process_vm_writev(pid_t, const struct iovec *, unsigned long, const struct iovec *, unsigned long, unsigned long);
+ssize_t process_vm_readv(pid_t, const struct iovec *, unsigned long, const struct iovec *, unsigned long, unsigned long);
+#endif
 
 #ifdef __cplusplus
 }
