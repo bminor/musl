8bbc3be4b48e2e3b3e772a217a4277ce04762aff
Rich Felker <dalias@aerifal.cx>
2012-07-08 21:32:45 -0400

    make arm syscalls (still non-inline) more efficient
    no need to pass zero for unused arguments; just omit them.

--- arch/arm/bits/syscall.h
+++ arch/arm/bits/syscall.h
@@ -7,32 +7,32 @@ long (__syscall)(long, ...);
 
 static inline long __syscall0(long n)
 {
-	return (__syscall)(n, 0, 0, 0, 0, 0, 0);
+	return (__syscall)(n);
 }
 
 static inline long __syscall1(long n, long a)
 {
-	return (__syscall)(n, a, 0, 0, 0, 0, 0);
+	return (__syscall)(n, a);
 }
 
 static inline long __syscall2(long n, long a, long b)
 {
-	return (__syscall)(n, a, b, 0, 0, 0, 0);
+	return (__syscall)(n, a, b);
 }
 
 static inline long __syscall3(long n, long a, long b, long c)
 {
-	return (__syscall)(n, a, b, c, 0, 0, 0);
+	return (__syscall)(n, a, b, c);
 }
 
 static inline long __syscall4(long n, long a, long b, long c, long d)
 {
-	return (__syscall)(n, a, b, c, d, 0, 0);
+	return (__syscall)(n, a, b, c, d);
 }
 
 static inline long __syscall5(long n, long a, long b, long c, long d, long e)
 {
-	return (__syscall)(n, a, b, c, d, e, 0);
+	return (__syscall)(n, a, b, c, d, e);
 }
 
 static inline long __syscall6(long n, long a, long b, long c, long d, long e, long f)
