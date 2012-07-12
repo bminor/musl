c89f130f39b413d1fb1733166ca63d694685c529
Rich Felker <dalias@aerifal.cx>
2012-07-12 11:14:12 -0400

    fix pthread_kill unlocking
    it had not been updated for the futex-based locks

--- src/thread/pthread_kill.c
+++ src/thread/pthread_kill.c
@@ -5,6 +5,6 @@ int pthread_kill(pthread_t t, int sig)
 	int r;
 	__lock(&t->killlock);
 	r = t->dead ? ESRCH : -__syscall(SYS_tgkill, t->pid, t->tid, sig);
-	a_store(&t->killlock, 0);
+	__unlock(&t->killlock);
 	return r;
 }
