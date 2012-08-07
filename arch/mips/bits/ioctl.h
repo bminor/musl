ff02f94e2d3a1e96ffc1918abd06e09ddd11fd06
Rich Felker <dalias@aerifal.cx>
2012-08-07 19:19:21 -0400

    fix another mips gratuitous-incompatibility bug: ioctl numbers

--- arch/mips/bits/ioctl.h
+++ arch/mips/bits/ioctl.h
@@ -1,7 +1,7 @@
 #define _IOC(a,b,c,d) ( ((a)<<29) | ((b)<<8) | (c) | ((d)<<16) )
 #define _IOC_NONE  1U
-#define _IOC_WRITE 2U
-#define _IOC_READ  4U
+#define _IOC_READ  2U
+#define _IOC_WRITE 4U
 
 #define _IO(a,b) _IOC(_IOC_NONE,(a),(b),0)
 #define _IOW(a,b,c) _IOC(1,(a),(b),sizeof(c))
