453059571c9dc84dd168631eced25ec2d7afd98e
Rich Felker <dalias@aerifal.cx>
2012-09-06 23:27:55 -0400

    fix invalid implicit pointer conversion in gnulib-compat functions

--- src/stdio/ext2.c
+++ src/stdio/ext2.c
@@ -10,7 +10,7 @@ const char *__freadptr(FILE *f, size_t *sizep)
 	size_t size = f->rend - f->rpos;
 	if (!size) return 0;
 	*sizep = size;
-	return f->rpos;
+	return (const char *)f->rpos;
 }
 
 void __freadptrinc(FILE *f, size_t inc)
