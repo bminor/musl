bac03cdde1137c16b4c194e137310e2748661dcc
Rich Felker <dalias@aerifal.cx>
2012-09-06 20:28:42 -0400

    remove dependency of wmemmove on wmemcpy direction
    unlike the memmove commit, this one should be fine to leave in place.
    wmemmove is not performance-critical, and even if it were, it's
    already copying whole 32-bit words at a time instead of bytes.

--- src/string/wmemmove.c
+++ src/string/wmemmove.c
@@ -3,9 +3,9 @@
 
 wchar_t *wmemmove(wchar_t *d, const wchar_t *s, size_t n)
 {
-	if ((size_t)(d-s) < n) {
+	if ((size_t)(d-s) < n)
 		while (n--) d[n] = s[n];
-		return d;
-	}
-	return wmemcpy(d, s, n);
+	else
+		while (n--) *d++ = *s++;
+	return d;
 }
