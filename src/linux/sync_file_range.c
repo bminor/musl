41c5ee50eee769f85253946e4c4b5706de37e9e9
Rich Felker <dalias@aerifal.cx>
2012-09-08 22:48:22 -0400

    disable sync_file_range for now
    something is wrong with the logic for the argument layout, resulting
    in compile errors on mips due to too many args to syscall... further
    information on how it's supposed to work will be needed before it can
    be reactivated.

--- src/linux/sync_file_range.c
+++ src/linux/sync_file_range.c
@@ -1,13 +1,14 @@
 #define _GNU_SOURCE
 #include <fcntl.h>
+#include <errno.h>
 #include "syscall.h"
 
 int sync_file_range(int fd, off_t pos, off_t len, unsigned flags)
 {
-#if defined(SYS_sync_file_range2)
+#if 0 && defined(SYS_sync_file_range2)
 	return syscall(SYS_sync_file_range2, fd, flags,
 		__SYSCALL_LL_E(pos), __SYSCALL_LL_E(len));
-#elif defined(SYS_sync_file_range)
+#elif 0 && defined(SYS_sync_file_range)
 	return syscall(SYS_sync_file_range, fd,
 		__SYSCALL_LL_O(pos), __SYSCALL_LL_E(len), flags);
 #else
