5e3c243d8df6b1504db9c1e6442d47ed38937e02
Rich Felker <dalias@aerifal.cx>
2012-09-09 01:29:19 -0400

    inline syscall support for arm
    most pure-syscall-wrapper functions compile to the smallest/simplest
    code possible (save r7 ; load syscall # ; svc 0 ; restore r7 ; tail
    call to __syscall_ret).

--- arch/arm/syscall_arch.h
+++ arch/arm/syscall_arch.h
@@ -5,6 +5,57 @@
 
 #define __SYSCALL_SSLEN 8
 
+#ifndef __clang__
+
+#define __asm_syscall(...) do { \
+	__asm__ __volatile__ ( "svc 0" \
+	: "=r"(r0) : __VA_ARGS__ : "memory"); \
+	return r0; \
+	} while (0)
+
+static inline long __syscall0(long n)
+{
+	register long r7 __asm__("r7") = n;
+	register long r0 __asm__("r0");
+	__asm_syscall("r"(r7));
+}
+
+static inline long __syscall1(long n, long a)
+{
+	register long r7 __asm__("r7") = n;
+	register long r0 __asm__("r0") = a;
+	__asm_syscall("r"(r7), "r"(r0));
+}
+
+static inline long __syscall2(long n, long a, long b)
+{
+	register long r7 __asm__("r7") = n;
+	register long r0 __asm__("r0") = a;
+	register long r1 __asm__("r1") = b;
+	__asm_syscall("r"(r7), "r"(r0), "r"(r1));
+}
+
+static inline long __syscall3(long n, long a, long b, long c)
+{
+	register long r7 __asm__("r7") = n;
+	register long r0 __asm__("r0") = a;
+	register long r1 __asm__("r1") = b;
+	register long r2 __asm__("r2") = c;
+	__asm_syscall("r"(r7), "r"(r0), "r"(r1), "r"(r2));
+}
+
+static inline long __syscall4(long n, long a, long b, long c, long d)
+{
+	register long r7 __asm__("r7") = n;
+	register long r0 __asm__("r0") = a;
+	register long r1 __asm__("r1") = b;
+	register long r2 __asm__("r2") = c;
+	register long r3 __asm__("r3") = d;
+	__asm_syscall("r"(r7), "r"(r0), "r"(r1), "r"(r2), "r"(r3));
+}
+
+#else
+
 static inline long __syscall0(long n)
 {
 	return (__syscall)(n);
@@ -29,6 +80,8 @@ static inline long __syscall4(long n, long a, long b, long c, long d)
 {
 	return (__syscall)(n, a, b, c, d);
 }
+
+#endif
 
 static inline long __syscall5(long n, long a, long b, long c, long d, long e)
 {
