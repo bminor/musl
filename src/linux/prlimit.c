f600105eef74a636a0efd1f1d07170ce5df9aa64
Rich Felker <dalias@aerifal.cx>
2012-09-21 04:05:01 -0400

    LFS64 alias for prlimit
    issue reported/requested by Justin Cormack

--- src/linux/prlimit.c
+++ src/linux/prlimit.c
@@ -1,8 +1,11 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include "syscall.h"
+#include "libc.h"
 
 int prlimit(pid_t pid, int resource, const struct rlimit *new_limit, struct rlimit *old_limit)
 {
 	return syscall(SYS_prlimit64, pid, resource, new_limit, old_limit);
 }
+
+LFS64(prlimit);
