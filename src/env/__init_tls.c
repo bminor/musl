ebee8c2b477807fe58fd35d35783633ea08b049c
Rich Felker <dalias@aerifal.cx>
2012-10-19 01:33:52 -0400

    fix crashes in static-linked multithreaded programs without TLS

--- src/env/__init_tls.c
+++ src/env/__init_tls.c
@@ -60,6 +60,8 @@ void __init_tls(size_t *aux)
 	Phdr *phdr, *tls_phdr=0;
 	size_t base = 0;
 
+	libc.tls_size = sizeof(struct pthread);
+
 	for (p=(void *)aux[AT_PHDR],n=aux[AT_PHNUM]; n; n--,p+=aux[AT_PHENT]) {
 		phdr = (void *)p;
 		if (phdr->p_type == PT_PHDR)
