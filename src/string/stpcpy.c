838951c97edc05cb2a25948ee542415b55b8f605
Rich Felker <dalias@aerifal.cx>
2012-10-22 15:17:09 -0400

    simplify logic in stpcpy; avoid copying first aligned byte twice
    gcc seems to be generating identical or near-identical code for both
    versions, but the newer code is more expressive of what it's doing.

--- src/string/stpcpy.c
+++ src/string/stpcpy.c
@@ -4,7 +4,7 @@
 #include <limits.h>
 #include "libc.h"
 
-#define ALIGN (sizeof(size_t)-1)
+#define ALIGN (sizeof(size_t))
 #define ONES ((size_t)-1/UCHAR_MAX)
 #define HIGHS (ONES * (UCHAR_MAX/2+1))
 #define HASZERO(x) ((x)-ONES & ~(x) & HIGHS)
@@ -14,9 +14,9 @@ char *__stpcpy(char *restrict d, const char *restrict s)
 	size_t *wd;
 	const size_t *ws;
 
-	if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
-		for (; (*d=*s) && ((uintptr_t)s & ALIGN); s++, d++);
-		if (!*s) return d;
+	if ((uintptr_t)s % ALIGN == (uintptr_t)d % ALIGN) {
+		for (; (uintptr_t)s % ALIGN; s++, d++)
+			if (!(*d=*s)) return d;
 		wd=(void *)d; ws=(const void *)s;
 		for (; !HASZERO(*ws); *wd++ = *ws++);
 		d=(void *)wd; s=(const void *)ws;
