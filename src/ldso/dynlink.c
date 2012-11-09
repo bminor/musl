65465101ee23447bf30adc3a9ebf817d58f5d956
Rich Felker <dalias@aerifal.cx>
2012-11-09 13:49:40 -0500

    support ldso path files without final newline

--- src/ldso/dynlink.c
+++ src/ldso/dynlink.c
@@ -478,8 +478,11 @@ static struct dso *load_library(const char *name)
 			if (!sys_path) {
 				FILE *f = fopen(ETC_LDSO_PATH, "rbe");
 				if (f) {
-					if (getline(&sys_path, (size_t[1]){0}, f) > 0)
-						sys_path[strlen(sys_path)-1]=0;
+					if (getline(&sys_path, (size_t[1]){0}, f) > 0) {
+						size_t l = strlen(sys_path);
+						if (l && sys_path[l-1]=='\n')
+							sys_path[-1] = 0;
+					}
 					fclose(f);
 				}
 			}
