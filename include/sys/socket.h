91738d0241aed345cac8685422d4518ea5f3cdf0
Rich Felker <dalias@aerifal.cx>
2012-11-15 19:16:10 -0500

    disable SO_REUSEPORT in sys/socket.h
    although a number is reserved for it, this option is not implemented
    on Linux and does not work. defining it causes some applications to
    use it, and subsequently break due to its failure.

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -143,7 +143,7 @@ struct linger
 #define SO_PRIORITY     12
 #define SO_LINGER       13
 #define SO_BSDCOMPAT    14
-#define SO_REUSEPORT    15
+/* #define SO_REUSEPORT    15 */
 #define SO_PASSCRED     16
 #define SO_PEERCRED     17
 #define SO_RCVLOWAT     18
