7718e7cce5730265f2707fe378ec263b4c35624d
Rich Felker <dalias@aerifal.cx>
2012-11-16 01:19:42 -0500

    fix ppc stat structure definition

--- arch/powerpc/bits/stat.h
+++ arch/powerpc/bits/stat.h
@@ -4,21 +4,20 @@
 struct stat
 {
 	dev_t st_dev;
-	int __st_dev_padding;
-	long __st_ino_truncated;
+	ino_t st_ino;
 	mode_t st_mode;
 	nlink_t st_nlink;
 	uid_t st_uid;
 	gid_t st_gid;
 	dev_t st_rdev;
-	int __st_rdev_padding;
+	short __st_rdev_padding;
 	off_t st_size;
 	blksize_t st_blksize;
 	blkcnt_t st_blocks;
 	struct timespec st_atim;
 	struct timespec st_mtim;
 	struct timespec st_ctim;
-	ino_t st_ino;
+	unsigned __unused[2];
 };
 
 #ifdef _BSD_SOURCE
