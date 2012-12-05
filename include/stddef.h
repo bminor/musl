a7c1f9727a099da0d4722d4cf81056acd205158b
Rich Felker <dalias@aerifal.cx>
2012-12-05 00:00:42 -0500

    use __builtin_offsetof to implement offsetof when possible
    apparently recent gcc versions have intentionally broken the
    traditional definition by treating it as a non-constant expression.
    the traditional definition may also be problematic for c++ programs.

--- include/stddef.h
+++ include/stddef.h
@@ -14,6 +14,10 @@
 
 #include <bits/alltypes.h>
 
+#if __GNUC__ > 3
+#define offsetof(type, member) __builtin_offsetof(type, member)
+#else
 #define offsetof(type, member) ((size_t)( (char *)&(((type *)0)->member) - (char *)0 ))
+#endif
 
 #endif
