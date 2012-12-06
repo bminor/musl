30d2c1f5bf68183e6361c5dfa812c6e42a8ec78c
rofl0r <retnyg@gmx.net>
2012-12-06 22:14:37 +0100

    sigandset/sigorset: do not check for NULL pointers.
    that way it's consistent with existing sig* functions, and saves
    some code size.

--- src/signal/sigandset.c
+++ src/signal/sigandset.c
@@ -1,15 +1,10 @@
 #define _GNU_SOURCE
 #include <signal.h>
-#include <errno.h>
 
 #define SST_SIZE (_NSIG/8)
 
 int sigandset(sigset_t *dest, sigset_t *left, sigset_t *right)
 {
-	if(!dest || !left || !right) {
-		errno = EINVAL;
-		return -1;
-	}
 	unsigned long i = 0, *d = (void*) dest, *l = (void*) left, *r = (void*) right;
 	for(; i < SST_SIZE; i++) d[i] = l[i] & r[i];
 	return 0;
