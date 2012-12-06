b6f75ab07f56a6ed3701231bd48601f3bc45dce5
rofl0r <retnyg@gmx.net>
2012-12-06 21:07:19 +0100

    add arch_prctl syscall (amd64/x32 only)

---
+++ src/misc/arch_prctl.c
@@ -0,0 +1,9 @@
+#include "syscall.h"
+#ifdef SYS_arch_prctl
+#include "libc.h"
+int __arch_prctl(int code, unsigned long addr)
+{
+	return syscall(SYS_arch_prctl, code, addr);	
+}
+weak_alias(__arch_prctl, arch_prctl);
+#endif
