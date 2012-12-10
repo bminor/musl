f1c1a5ea8295a3f8e9ea2db8961c5a68e1a3f9ed
Rich Felker <dalias@aerifal.cx>
2012-12-10 18:31:39 -0500

    document self-synchronized destruction issue for stdio locking

--- src/stdio/__lockfile.c
+++ src/stdio/__lockfile.c
@@ -14,5 +14,15 @@ int __lockfile(FILE *f)
 void __unlockfile(FILE *f)
 {
 	a_store(&f->lock, 0);
+
+	/* The following read is technically invalid under situations
+	 * of self-synchronized destruction. Another thread may have
+	 * called fclose as soon as the above store has completed.
+	 * Nonetheless, since FILE objects always live in memory
+	 * obtained by malloc from the heap, it's safe to assume
+	 * the dereferences below will not fault. In the worst case,
+	 * a spurious syscall will be made. If the implementation of
+	 * malloc changes, this assumption needs revisiting. */
+
 	if (f->waiters) __wake(&f->lock, 1, 1);
 }
