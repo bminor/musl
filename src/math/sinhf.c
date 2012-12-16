f143458223f90262a9c2d929f9e815a74e3aa139
Szabolcs Nagy <nsz@port70.net>
2012-12-16 19:49:55 +0100

    math: sinh.c cleanup similar to the cosh one
    comments are kept in the double version of the function

--- src/math/sinhf.c
+++ src/math/sinhf.c
@@ -1,57 +1,31 @@
-/* origin: FreeBSD /usr/src/lib/msun/src/e_sinhf.c */
-/*
- * Conversion to float by Ian Lance Taylor, Cygnus Support, ian@cygnus.com.
- */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
-
 #include "libm.h"
-
-static const float huge = 1.0e37;
 
 float sinhf(float x)
 {
-	float t, h;
-	int32_t ix, jx;
-
-	GET_FLOAT_WORD(jx, x);
-	ix = jx & 0x7fffffff;
-
-	/* x is INF or NaN */
-	if (ix >= 0x7f800000)
-		return x + x;
+	union {float f; uint32_t i;} u = {.f = x};
+	uint32_t w;
+	float t, h, absx;
 
 	h = 0.5;
-	if (jx < 0)
+	if (u.i >> 31)
 		h = -h;
-	/* |x| in [0,9], return sign(x)*0.5*(E+E/(E+1))) */
-	if (ix < 0x41100000) {   /* |x|<9 */
-		if (ix < 0x39800000)  /* |x|<2**-12 */
-			/* raise inexact, return x */
-			if (huge+x > 1.0f)
+	/* |x| */
+	u.i &= 0x7fffffff;
+	absx = u.f;
+	w = u.i;
+
+	/* |x| < log(FLT_MAX) */
+	if (w < 0x42b17217) {
+		t = expm1f(absx);
+		if (w < 0x3f800000) {
+			if (w < 0x3f800000 - (12<<23))
 				return x;
-		t = expm1f(fabsf(x));
-		if (ix < 0x3f800000)
-			return h*(2.0f*t - t*t/(t+1.0f));
-		return h*(t + t/(t+1.0f));
+			return h*(2*t - t*t/(t+1));
+		}
+		return h*(t + t/(t+1));
 	}
 
-	/* |x| in [9, logf(maxfloat)] return 0.5*exp(|x|) */
-	if (ix < 0x42b17217)
-		return h*expf(fabsf(x));
-
-	/* |x| in [logf(maxfloat), overflowthresold] */
-	if (ix <= 0x42b2d4fc)
-		return h * 2.0f * __expo2f(fabsf(x)); /* h is for sign only */
-
-	/* |x| > overflowthresold, sinh(x) overflow */
-	return x*huge;
+	/* |x| > logf(FLT_MAX) or nan */
+	t = 2*h*__expo2f(absx);
+	return t;
 }
--- src/math/sinhl.c
+++ src/math/sinhl.c
@@ -1,32 +1,3 @@
-/* origin: OpenBSD /usr/src/lib/libm/src/ld80/e_sinhl.c */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
-/* sinhl(x)
- * Method :
- * mathematically sinh(x) if defined to be (exp(x)-exp(-x))/2
- *      1. Replace x by |x| (sinhl(-x) = -sinhl(x)).
- *      2.
- *                                                   E + E/(E+1)
- *          0        <= x <= 25     :  sinhl(x) := --------------, E=expm1l(x)
- *                                                       2
- *
- *          25       <= x <= lnovft :  sinhl(x) := expl(x)/2
- *          lnovft   <= x <= ln2ovft:  sinhl(x) := expl(x/2)/2 * expl(x/2)
- *          ln2ovft  <  x           :  sinhl(x) := x*huge (overflow)
- *
- * Special cases:
- *      sinhl(x) is |x| if x is +INF, -INF, or NaN.
- *      only sinhl(0)=0 is exact for finite x.
- */
-
 #include "libm.h"
 
 #if LDBL_MANT_DIG == 53 && LDBL_MAX_EXP == 1024
@@ -35,47 +6,35 @@ long double sinhl(long double x)
 	return sinh(x);
 }
 #elif LDBL_MANT_DIG == 64 && LDBL_MAX_EXP == 16384
-static const long double huge = 1.0e4931L;
-
 long double sinhl(long double x)
 {
-	long double t,w,h;
-	uint32_t jx,ix,i0,i1;
-
-	/* Words of |x|. */
-	GET_LDOUBLE_WORDS(jx, i0, i1, x);
-	ix = jx & 0x7fff;
-
-	/* x is INF or NaN */
-	if (ix == 0x7fff) return x + x;
+	union {
+		long double f;
+		struct{uint64_t m; uint16_t se; uint16_t pad;} i;
+	} u = {.f = x};
+	unsigned ex = u.i.se & 0x7fff;
+	long double h, t, absx;
 
 	h = 0.5;
-	if (jx & 0x8000)
+	if (u.i.se & 0x8000)
 		h = -h;
-	/* |x| in [0,25], return sign(x)*0.5*(E+E/(E+1))) */
-	if (ix < 0x4003 || (ix == 0x4003 && i0 <= 0xc8000000)) { /* |x| < 25 */
-		if (ix < 0x3fdf)  /* |x|<2**-32 */
-			if (huge + x > 1.0)
-				return x;/* sinh(tiny) = tiny with inexact */
-		t = expm1l(fabsl(x));
-		if (ix < 0x3fff)
-			return h*(2.0*t - t*t/(t + 1.0));
-		return h*(t + t/(t + 1.0));
+	/* |x| */
+	u.i.se = ex;
+	absx = u.f;
+
+	/* |x| < log(LDBL_MAX) */
+	if (ex < 0x3fff+13 || (ex == 0x3fff+13 && u.i.m>>32 < 0xb17217f7)) {
+		t = expm1l(absx);
+		if (ex < 0x3fff) {
+			if (ex < 0x3fff-32)
+				return x;
+			return h*(2*t - t*t/(1+t));
+		}
+		return h*(t + t/(t+1));
 	}
 
-	/* |x| in [25, log(maxdouble)] return 0.5*exp(|x|) */
-	if (ix < 0x400c || (ix == 0x400c && i0 < 0xb17217f7))
-		return h*expl(fabsl(x));
-
-	/* |x| in [log(maxdouble), overflowthreshold] */
-	if (ix < 0x400c || (ix == 0x400c && (i0 < 0xb174ddc0 ||
-	     (i0 == 0xb174ddc0 && i1 <= 0x31aec0ea)))) {
-		w = expl(0.5*fabsl(x));
-		t = h*w;
-		return t*w;
-	}
-
-	/* |x| > overflowthreshold, sinhl(x) overflow */
-	return x*huge;
+	/* |x| > log(LDBL_MAX) or nan */
+	t = expl(0.5*absx);
+	return h*t*t;
 }
 #endif
