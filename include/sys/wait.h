e895ddc0cd560ffb217c741139721a836c5f4b7f
rofl0r <retnyg@gmx.net>
2013-01-01 08:07:26 +0100

    wait.h: add linux specific, thread-related waitpid() flags
    these flags are needed in order to be able to handle lwp id's
    which the kernel returns after clone() calls for new threads
    via ptrace(PTRACE_GETEVENTMSG).
    
    fortunately, they're the same for all archs and in the reserved
    namespace.

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -33,6 +33,10 @@ pid_t wait4 (pid_t, int *, int, struct rusage *);
 #define WCONTINUED 8
 #define WNOWAIT    0x1000000
 
+#define __WNOTHREAD 0x20000000
+#define __WALL      0x40000000
+#define __WCLONE    0x80000000
+
 #define P_ALL  0
 #define P_PID  1
 #define P_PGID 2
