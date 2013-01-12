d650631dad1691ad778b2d7afd77ee77d8546d9a
Szabolcs Nagy <nsz@port70.net>
2013-01-12 19:13:59 +0100

    add SWAP_FLAG_DISCARD to sys/swap.h

--- include/sys/swap.h
+++ include/sys/swap.h
@@ -9,6 +9,7 @@ extern "C" {
 #define	SWAP_FLAG_PREFER        0x8000
 #define	SWAP_FLAG_PRIO_MASK     0x7fff
 #define	SWAP_FLAG_PRIO_SHIFT    0
+#define SWAP_FLAG_DISCARD       0x10000
 
 int swapon (const char *, int);
 int swapoff (const char *);
