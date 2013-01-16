4d98280388a21db6913911ff647b2e56338d46cf
Rich Felker <dalias@aerifal.cx>
2013-01-16 11:49:00 -0500

    fix warning building dynlink.c stub for static libc
    struct dso was not defined in this case, and it's not needed in the
    code that was using it anyway; void pointers work just as well.

--- src/ldso/dynlink.c
+++ src/ldso/dynlink.c
@@ -1091,7 +1091,7 @@ end:
 	return p;
 }
 
-static int invalid_dso_handle(struct dso *h)
+static int invalid_dso_handle(void *h)
 {
 	struct dso *p;
 	for (p=head; p; p=p->next) if (h==p) return 0;
@@ -1246,7 +1246,7 @@ int dl_iterate_phdr(int(*callback)(struct dl_phdr_info *info, size_t size, void 
 	return ret;
 }
 #else
-static int invalid_dso_handle(struct dso *h)
+static int invalid_dso_handle(void *h)
 {
 	snprintf(errbuf, sizeof errbuf, "Invalid library handle %p", (void *)h);
 	errflag = 1;
