ae7399bfd86c59b8717bb81c70b2acb20acd0f9a
Rich Felker <dalias@aerifal.cx>
2013-03-25 11:34:22 -0400

    in pipe2, use pipe() rather than __syscall(SYS_pipe, ...) for fallback
    SYS_pipe is not usable directly in general, since mips has a very
    broken calling convention for the pipe syscall. instead, just call the
    function, so that the mips-specific ugliness is isolated in
    mips/pipe.s and not copied elsewhere.

--- src/unistd/pipe2.c
+++ src/unistd/pipe2.c
@@ -5,11 +5,11 @@
 
 int pipe2(int fd[2], int flag)
 {
-	if (!flag) return syscall(SYS_pipe, fd);
+	if (!flag) return pipe(fd);
 	int ret = __syscall(SYS_pipe2, fd, flag);
 	if (ret != -ENOSYS) return __syscall_ret(ret);
-	ret = __syscall(SYS_pipe, fd);
-	if (ret) return __syscall_ret(ret);
+	ret = pipe(fd);
+	if (ret) return ret;
 	if (flag & O_CLOEXEC) {
 		__syscall(SYS_fcntl, fd[0], F_SETFD, FD_CLOEXEC);
 		__syscall(SYS_fcntl, fd[1], F_SETFD, FD_CLOEXEC);
