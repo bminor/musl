d53c92c972dab717f8f8e521eb0e9c4404cef4bd
Rich Felker <dalias@aerifal.cx>
2013-04-26 19:02:23 -0400

    optimize/debloat raise
    use __syscall rather than syscall when failure is not possible or not
    to be considered.

--- src/signal/raise.c
+++ src/signal/raise.c
@@ -9,8 +9,8 @@ int raise(int sig)
 	int pid, tid, ret;
 	sigset_t set;
 	__syscall(SYS_rt_sigprocmask, SIG_BLOCK, SIGALL_SET, &set, _NSIG/8);
-	tid = syscall(SYS_gettid);
-	pid = syscall(SYS_getpid);
+	tid = __syscall(SYS_gettid);
+	pid = __syscall(SYS_getpid);
 	ret = syscall(SYS_tgkill, pid, tid, sig);
 	__syscall(SYS_rt_sigprocmask, SIG_SETMASK, &set, 0, _NSIG/8);
 	return ret;
