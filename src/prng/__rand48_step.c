0996faa3d789dd69e4c1c022757f0623908b0df5
Szabolcs Nagy <nsz@port70.net>
2013-06-08 13:49:12 +0000

    prng: make rand_r have 2^32 period instead of 2^31
    this is a minor fix to increase the period of the obsolete rand_r a bit.
    an include header in __rand48_step.c is fixed as well.

--- src/prng/__rand48_step.c
+++ src/prng/__rand48_step.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <inttypes.h>
+#include <stdint.h>
 
 uint64_t __rand48_step(unsigned short *xi, unsigned short *lc)
 {
--- src/prng/rand_r.c
+++ src/prng/rand_r.c
@@ -2,5 +2,5 @@
 
 int rand_r(unsigned *seed)
 {
-	return (*seed = *seed * 1103515245 + 12345) & 0x7fffffff;
+	return (*seed = *seed * 1103515245 + 12345)/2;
 }
