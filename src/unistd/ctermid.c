cdf0f53f8ba0e79dedb83c626851597bacec53ca
Rich Felker <dalias@aerifal.cx>
2013-07-09 00:42:09 -0400

    fix fd leak on races and cancellation in ctermid

--- src/unistd/ctermid.c
+++ src/unistd/ctermid.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <limits.h>
+#include "syscall.h"
 
 char *ctermid(char *s)
 {
@@ -13,11 +14,11 @@ char *ctermid(char *s)
 		if (!s2) s2 = malloc(L_ctermid);
 		s = s2;
 	}
-	fd = open("/dev/tty", O_WRONLY | O_NOCTTY);
+	fd = open("/dev/tty", O_WRONLY | O_NOCTTY | O_CLOEXEC);
 	if (fd < 0)
 		return strcpy(s, "");
 	if (ttyname_r(fd, s, L_ctermid))
 		strcpy(s, "");
-	close(fd);
+	__syscall(SYS_close, fd);
 	return s;
 }
