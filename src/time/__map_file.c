ac1bf93fc634274c14d234c44b2e846a93a5cc95
Rich Felker <dalias@aerifal.cx>
2013-07-17 09:29:26 -0400

    fix fd leak in file mapping code used in new zoneinfo support

--- src/time/__map_file.c
+++ src/time/__map_file.c
@@ -14,7 +14,7 @@ const char unsigned *__map_file(const char *pathname, size_t *size)
 	if (fd < 0) return 0;
 	if (!__syscall(SYS_fstat, fd, &st))
 		map = __mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
-	__syscall(SYS_close);
+	__syscall(SYS_close, fd);
 	*size = st.st_size;
 	return map == MAP_FAILED ? 0 : map;
 }
