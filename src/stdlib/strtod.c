17a60f9d327c6f8b5707a06f9497d846e75c01f2
Rich Felker <dalias@aerifal.cx>
2013-07-24 18:11:30 -0400

    add _l versions of strtod family functions, purely as aliases
    this is a cheat since the _l versions take an extra argument, but
    since these functions are only here for ABI purposes, it doesn't
    really matter as long as the ABI matches. if the non-__-prefixed
    versions are eventually made public, they should proabably be real
    functions rather than hacks like this.

--- src/stdlib/strtod.c
+++ src/stdlib/strtod.c
@@ -2,6 +2,7 @@
 #include "shgetc.h"
 #include "floatscan.h"
 #include "stdio_impl.h"
+#include "libc.h"
 
 static long double strtox(const char *s, char **p, int prec)
 {
@@ -30,3 +31,10 @@ long double strtold(const char *restrict s, char **restrict p)
 {
 	return strtox(s, p, 2);
 }
+
+weak_alias(strtof, strtof_l);
+weak_alias(strtod, strtod_l);
+weak_alias(strtold, strtold_l);
+weak_alias(strtof, __strtof_l);
+weak_alias(strtod, __strtod_l);
+weak_alias(strtold, __strtold_l);
