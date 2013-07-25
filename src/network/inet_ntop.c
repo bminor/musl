1cd417bdf10366d63cc875e285c6418709a58c17
Rich Felker <dalias@aerifal.cx>
2013-07-25 03:20:02 -0400

    make inet_ntop format v4-mapped ipv6 addresses properly
    based on a patch by orc. POSIX actually fails to specify the format of
    the ntop conversion; presumably, any output that will correctly
    round-trip back via the (well-specified) pton operation is acceptable.
    the new behavior is much more convenient than the old, however.
    
    this patch also affects getnameinfo, which is implemented in terms of
    inet_ntop and which is the preferred interface for performing this
    conversion.
    
    I've also removed some inexplicable cruft (filling the buffer with 'x'
    before doing anything) whose origin I was unable to track down.

--- src/network/inet_ntop.c
+++ src/network/inet_ntop.c
@@ -18,14 +18,20 @@ const char *inet_ntop(int af, const void *restrict a0, char *restrict s, socklen
 			return s;
 		break;
 	case AF_INET6:
-		memset(buf, 'x', sizeof buf);
-		buf[sizeof buf-1]=0;
-		snprintf(buf, sizeof buf,
-			"%x:%x:%x:%x:%x:%x:%x:%x",
-			256*a[0]+a[1],256*a[2]+a[3],
-			256*a[4]+a[5],256*a[6]+a[7],
-			256*a[8]+a[9],256*a[10]+a[11],
-			256*a[12]+a[13],256*a[14]+a[15]);
+		if (memcmp(a, "\0\0\0\0\0\0\0\0\0\0\377\377", 12))
+			snprintf(buf, sizeof buf,
+				"%x:%x:%x:%x:%x:%x:%x:%x",
+				256*a[0]+a[1],256*a[2]+a[3],
+				256*a[4]+a[5],256*a[6]+a[7],
+				256*a[8]+a[9],256*a[10]+a[11],
+				256*a[12]+a[13],256*a[14]+a[15]);
+		else
+			snprintf(buf, sizeof buf,
+				"%x:%x:%x:%x:%x:%x:%d.%d.%d.%d",
+				256*a[0]+a[1],256*a[2]+a[3],
+				256*a[4]+a[5],256*a[6]+a[7],
+				256*a[8]+a[9],256*a[10]+a[11],
+				a[12],a[13],a[14],a[15]);
 		/* Replace longest /(^0|:)[:0]{2,}/ with "::" */
 		for (i=best=0, max=2; buf[i]; i++) {
 			if (i && buf[i] != ':') continue;
