7406fdf5a18b37330de108abb0106f44ebdae2c6
Rich Felker <dalias@aerifal.cx>
2013-08-10 21:41:05 -0400

    add pthread_setaffinity_np and pthread_getaffinity_np functions

--- src/sched/sched_setaffinity.c
+++
@@ -1,8 +0,0 @@
-#define _GNU_SOURCE
-#include <sched.h>
-#include "syscall.h"
-
-int sched_setaffinity(pid_t tid, size_t size, const cpu_set_t *set)
-{
-	return syscall(SYS_sched_setaffinity, tid, size, set);
-}
