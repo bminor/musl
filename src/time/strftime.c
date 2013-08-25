242a4bb4406dad53a1983ea1134d5326a0c4afa3
Rich Felker <dalias@aerifal.cx>
2013-08-25 02:02:15 -0400

    add the %s (seconds since the epoch) format to strftime
    this is a nonstandard extension but will be required in the next
    version of POSIX, and it's widely used/useful in shell scripts
    utilizing the date utility.

--- src/time/strftime.c
+++ src/time/strftime.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <limits.h>
 #include "libc.h"
+#include "time_impl.h"
 
 const char *__nl_langinfo_l(nl_item, locale_t);
 
@@ -123,6 +124,9 @@ const char *__strftime_fmt_1(char (*s)[100], size_t *l, int f, const struct tm *
 	case 'R':
 		fmt = "%H:%M";
 		goto recu_strftime;
+	case 's':
+		val = __tm_to_secs(tm) + tm->__tm_gmtoff;
+		goto number;
 	case 'S':
 		val = tm->tm_sec;
 		goto number;
