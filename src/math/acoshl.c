101e6012856918440b5d7474739c3fc22a8d3b85
Szabolcs Nagy <nsz@port70.net>
2013-09-05 12:26:26 +0000

    math: fix acoshf on negative values
    acosh(x) is invalid for x<1, acoshf tried to be clever using
    signed comparisions to handle all x<2 the same way, but the
    formula was wrong on large negative values.

--- src/math/acoshl.c
+++ src/math/acoshl.c
@@ -13,10 +13,10 @@ long double acoshl(long double x)
 	int e = u.i.se & 0x7fff;
 
 	if (e < 0x3fff + 1)
-		/* x < 2, invalid if x < 1 or nan */
+		/* |x| < 2, invalid if x < 1 or nan */
 		return log1pl(x-1 + sqrtl((x-1)*(x-1)+2*(x-1)));
 	if (e < 0x3fff + 32)
-		/* x < 0x1p32 */
+		/* |x| < 0x1p32 */
 		return logl(2*x - 1/(x+sqrtl(x*x-1)));
 	return logl(x) + 0.693147180559945309417232121458176568L;
 }
