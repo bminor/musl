4b2c8fee3599c7676a6eca1e6880b8e042794f53
Timo Teräs <timo.teras@iki.fi>
2013-10-14 08:28:19 +0300

    add linux tcp state enums

--- include/netinet/tcp.h
+++ include/netinet/tcp.h
@@ -27,6 +27,18 @@
 #define TCP_FASTOPEN     23
 #define TCP_TIMESTAMP    24
 
+#define TCP_ESTABLISHED  1
+#define TCP_SYN_SENT     2
+#define TCP_SYN_RECV     3
+#define TCP_FIN_WAIT1    4
+#define TCP_FIN_WAIT2    5
+#define TCP_TIME_WAIT    6
+#define TCP_CLOSE        7
+#define TCP_CLOSE_WAIT   8
+#define TCP_LAST_ACK     9
+#define TCP_LISTEN       10
+#define TCP_CLOSING      11
+
 #if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 #define SOL_TCP 6
 #include <sys/types.h>
