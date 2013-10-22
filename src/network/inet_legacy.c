bb93ac335846424662212eea840859e7f0cc16b5
Rich Felker <dalias@aerifal.cx>
2013-10-21 22:05:29 -0400

    split inet_addr and inet_ntoa back into their own files
    despite being practically deprecated, these functions are still part
    of the standard and thus cannot reside in a file that also contains
    namespace pollution. this reverts some of the changes made in commit
    e40f48a421a9176e3e298b5bac75f0355b219e58.

--- src/network/inet_legacy.c
+++ src/network/inet_legacy.c
@@ -1,15 +1,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdio.h>
 #include "__dns.h"
-
-in_addr_t inet_addr(const char *p)
-{
-	struct sockaddr_in sin;
-	if (__ipparse(&sin, AF_INET, p)) return -1;
-	return sin.sin_addr.s_addr;
-}
 
 in_addr_t inet_network(const char *p)
 {
@@ -19,14 +11,6 @@ in_addr_t inet_network(const char *p)
 int inet_aton(const char *cp, struct in_addr *inp)
 {
 	return inet_pton(AF_INET, cp, (void *)inp) > 0;
-}
-
-char *inet_ntoa(struct in_addr in)
-{
-	static char buf[16];
-	unsigned char *a = (void *)&in;
-	snprintf(buf, sizeof buf, "%d.%d.%d.%d", a[0], a[1], a[2], a[3]);
-	return buf;
 }
 
 struct in_addr inet_makeaddr(int net, int host)
