3fd1acbfeed025ac833b4d6d2bc45dac4d3562c8
Michael Forney <mforney@mforney.org>
2013-11-04 23:34:21 -0800

    putgrent: Add missing newline

--- src/passwd/putgrent.c
+++ src/passwd/putgrent.c
@@ -9,6 +9,7 @@ int putgrent(const struct group *gr, FILE *f)
 	if ((r = fprintf(f, "%s:%s:%d:", gr->gr_name, gr->gr_passwd, gr->gr_gid))<0) goto done;
 	if (gr->gr_mem) for (i=0; gr->gr_mem[i]; i++)
 		if ((r = fprintf(f, "%s%s", i?",":"", gr->gr_mem[i]))<0) goto done;
+	r = fputc('\n', f);
 done:
 	funlockfile(f);
 	return r<0 ? -1 : 0;
