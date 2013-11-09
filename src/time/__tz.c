1d0d2df609b2e47615c7798d1a09e69a2caff24b
Rich Felker <dalias@aerifal.cx>
2013-11-08 20:50:59 -0500

    fix handling of overly-long TZ environment variable values
    the rest of the code is not prepared to handle an empty TZ string, so
    falling back to __gmt ("GMT"), just as if TZ had been blank or unset,
    is the preferable action.

--- src/time/__tz.c
+++ src/time/__tz.c
@@ -138,7 +138,7 @@ static void do_tzset()
 	 * free so as not to pull it into static programs. Growth
 	 * strategy makes it so free would have minimal benefit anyway. */
 	i = strlen(s);
-	if (i > PATH_MAX+1) s = "", i = 0;
+	if (i > PATH_MAX+1) s = __gmt, i = 3;
 	if (i >= old_tz_size) {
 		old_tz_size *= 2;
 		if (i >= old_tz_size) old_tz_size = i+1;
