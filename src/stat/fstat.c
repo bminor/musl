65ea604c74f3fecbc61a266a22fdf527764995b6
Rich Felker <dalias@aerifal.cx>
2013-12-19 14:24:55 -0500

    fix failure of fchmod, fstat, fchdir, and fchown to produce EBADF
    the workaround/fallback code for supporting O_PATH file descriptors
    when the kernel lacks support for performing these operations on them
    caused EBADF to get replaced by ENOENT (due to missing entry in
    /proc/self/fd). this is unlikely to affect real-world code (calls that
    might yield EBADF are generally unsafe, especially in library code)
    but it was breaking some test cases.
    
    the fix I've applied is something of a tradeoff: it adds one syscall
    to these operations on kernels where the workaround is needed. the
    alternative would be to catch ENOENT from the /proc lookup and
    translate it to EBADF, but I want to avoid doing that in the interest
    of not touching/depending on /proc at all in these functions as long
    as the kernel correctly supports the operations. this is following the
    general principle of isolating hacks to code paths that are taken on
    broken systems, and keeping the code for correct systems completely
    hack-free.

--- src/stat/fstat.c
+++ src/stat/fstat.c
@@ -1,5 +1,6 @@
 #include <sys/stat.h>
 #include <errno.h>
+#include <fcntl.h>
 #include "syscall.h"
 #include "libc.h"
 
@@ -8,7 +9,8 @@ void __procfdname(char *, unsigned);
 int fstat(int fd, struct stat *st)
 {
 	int ret = __syscall(SYS_fstat, fd, st);
-	if (ret != -EBADF || fd < 0) return __syscall_ret(ret);
+	if (ret != -EBADF || __syscall(SYS_fcntl, fd, F_GETFD) < 0)
+		return __syscall_ret(ret);
 
 	char buf[15+3*sizeof(int)];
 	__procfdname(buf, fd);
--- src/unistd/fchdir.c
+++ src/unistd/fchdir.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 #include "syscall.h"
 
 void __procfdname(char *, unsigned);
@@ -7,7 +8,8 @@ void __procfdname(char *, unsigned);
 int fchdir(int fd)
 {
 	int ret = __syscall(SYS_fchdir, fd);
-	if (ret != -EBADF || fd < 0) return __syscall_ret(ret);
+	if (ret != -EBADF || __syscall(SYS_fcntl, fd, F_GETFD) < 0)
+		return __syscall_ret(ret);
 
 	char buf[15+3*sizeof(int)];
 	__procfdname(buf, fd);
