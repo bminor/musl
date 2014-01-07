74998fbec13cb2d1c6a0952141b90f4216ab89e6
Rich Felker <dalias@aerifal.cx>
2014-01-07 02:58:28 -0500

    fix signedness of pgoff argument to remap_file_pages
    both the kernel and glibc agree that this argument is unsigned; the
    incorrect type ssize_t came from erroneous man pages.

--- src/linux/remap_file_pages.c
+++ src/linux/remap_file_pages.c
@@ -2,7 +2,7 @@
 #include <sys/mman.h>
 #include "syscall.h"
 
-int remap_file_pages(void *addr, size_t size, int prot, ssize_t pgoff, int flags)
+int remap_file_pages(void *addr, size_t size, int prot, size_t pgoff, int flags)
 {
 	return syscall(SYS_remap_file_pages, addr, size, prot, pgoff, flags);
 }
