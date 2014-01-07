2750337379d1314026d174cd5b49e3f8ea590637
Rich Felker <dalias@aerifal.cx>
2014-01-07 02:41:27 -0500

    fix incorrect type for wd argument of inotify_rm_watch
    this was wrong since the original commit adding inotify, and I don't
    see any explanation for it. not even the man pages have it wrong. it
    was most likely a copy-and-paste error.

--- src/linux/inotify.c
+++ src/linux/inotify.c
@@ -15,7 +15,7 @@ int inotify_add_watch(int fd, const char *pathname, uint32_t mask)
 	return syscall(SYS_inotify_add_watch, fd, pathname, mask);
 }
 
-int inotify_rm_watch(int fd, uint32_t wd)
+int inotify_rm_watch(int fd, int wd)
 {
 	return syscall(SYS_inotify_rm_watch, fd, wd);
 }
