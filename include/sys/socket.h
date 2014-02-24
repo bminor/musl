099ce3631c682e353e8090f49f713980d438f94e
Szabolcs Nagy <nsz@port70.net>
2014-02-25 00:22:04 +0100

    sys/socket.h: add the SO_MAX_PACING_RATE SOL_SOCKET option
    introduced in linux v3.13, 62748f32d501f5d3712a7c372bbb92abc7c62bc7

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -202,6 +202,7 @@ struct linger
 #define SO_LOCK_FILTER          44
 #define SO_SELECT_ERR_QUEUE     45
 #define SO_BUSY_POLL            46
+#define SO_MAX_PACING_RATE      47
 
 #ifndef SOL_SOCKET
 #define SOL_SOCKET      1
