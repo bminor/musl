f6e2f7e13f0edb1bb1d94e6ad4bf44abffd048d1
Rich Felker <dalias@aerifal.cx>
2014-03-11 15:27:13 -0400

    move struct semid_ds to from shared sys/sem.h to bits
    the definition was found to be incorrect at least for powerpc, and
    fixing this cleanly requires making the definition arch-specific. this
    will allow cleaning up the definition for other archs to make it more
    specific, and reversing some of the ugliness (time_t hacks) introduced
    with the x32 port.
    
    this first commit simply copies the existing definition to each arch
    without any changes. this is intentional, to make it easier to review
    changes made on a per-arch basis.

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -27,22 +27,7 @@ extern "C" {
 
 #include <endian.h>
 
-struct semid_ds {
-	struct ipc_perm sem_perm;
-	time_t sem_otime;
-	time_t __unused1;
-	time_t sem_ctime;
-	time_t __unused2;
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-	unsigned short sem_nsems;
-	char __sem_nsems_pad[sizeof(time_t)-sizeof(short)];
-#else
-	char __sem_nsems_pad[sizeof(time_t)-sizeof(short)];
-	unsigned short sem_nsems;
-#endif
-	time_t __unused3;
-	time_t __unused4;
-};
+#include <bits/sem.h>
 
 #define _SEM_SEMUN_UNDEFINED 1
 
