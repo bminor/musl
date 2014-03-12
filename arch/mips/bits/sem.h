3ceb89ed3fb5a43fc267ce818f90f38f55bf4e71
Szabolcs Nagy <nsz@port70.net>
2014-03-12 11:46:17 +0100

    fix semid_ds structure on mips
    This used to be broken when all archs had the same semid_ds definition:
    there is no padding around the time_t members on mips.

--- arch/mips/bits/sem.h
+++ arch/mips/bits/sem.h
@@ -1,9 +1,7 @@
 struct semid_ds {
 	struct ipc_perm sem_perm;
 	time_t sem_otime;
-	time_t __unused1;
 	time_t sem_ctime;
-	time_t __unused2;
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 	unsigned short sem_nsems;
 	char __sem_nsems_pad[sizeof(time_t)-sizeof(short)];
