7673acd31503016f2af93e187aac98da07af42b4
Szabolcs Nagy <nsz@port70.net>
2014-03-12 15:59:09 +0100

    fix statfs struct on mips
    The mips statfs struct layout is different than on other archs, so the
    statfs, fstatfs, statvfs and fstatvfs APIs were broken on mips.
    Now the ordering is fixed, the types are kept consistent with other archs.

--- arch/mips/bits/statfs.h
+++ arch/mips/bits/statfs.h
@@ -1,7 +1,8 @@
 struct statfs {
-	unsigned long f_type, f_bsize;
-	fsblkcnt_t f_blocks, f_bfree, f_bavail;
+	unsigned long f_type, f_bsize, f_frsize;
+	fsblkcnt_t f_blocks, f_bfree;
 	fsfilcnt_t f_files, f_ffree;
+	fsblkcnt_t f_bavail;
 	fsid_t f_fsid;
-	unsigned long f_namelen, f_frsize, f_flags, f_spare[4];
+	unsigned long f_namelen, f_flags, f_spare[5];
 };
