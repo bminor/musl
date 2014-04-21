5c4f11d995cf178b3146cde0734d6988c145f243
Rich Felker <dalias@aerifal.cx>
2014-04-21 18:11:42 -0400

    allow zoneinfo-path-relative filenames with no slashes in TZ variable
    since the form TZ=name is reserved for POSIX-form time zone strings,
    TZ=:name needs to be used when the zoneinfo filename is in the
    top-level zoneinfo directory and therefore does not contain a slash.
    previously the leading colon was merely dropped, making it impossible
    to access such zones without a full absolute pathname.
    
    changes based on patch by Timo Teräs.

--- src/time/__tz.c
+++ src/time/__tz.c
@@ -121,7 +121,7 @@ int __munmap(void *, size_t);
 static void do_tzset()
 {
 	char buf[NAME_MAX+25], *pathname=buf+24;
-	const char *try, *s;
+	const char *try, *s, *p;
 	const unsigned char *map = 0;
 	size_t i;
 	static const char search[] =
@@ -147,19 +147,16 @@ static void do_tzset()
 	}
 	if (old_tz) memcpy(old_tz, s, i+1);
 
-	if (*s == ':') s++;
-
 	/* Non-suid can use an absolute tzfile pathname or a relative
 	 * pathame beginning with "."; in secure mode, only the
 	 * standard path will be searched. */
-	if (*s == '/' || *s == '.') {
-		if (!libc.secure) map = __map_file(s, &map_size);
-	} else {
-		for (i=0; s[i] && s[i]!=','; i++) {
-			if (s[i]=='/') {
-				size_t l = strlen(s);
-				if (l > NAME_MAX || strchr(s, '.'))
-					break;
+	if (*s == ':' || ((p=strchr(s, '/')) && !memchr(s, ',', p-s))) {
+		if (*s == ':') s++;
+		if (*s == '/' || *s == '.') {
+			if (!libc.secure) map = __map_file(s, &map_size);
+		} else {
+			size_t l = strlen(s);
+			if (l <= NAME_MAX && !strchr(s, '.')) {
 				memcpy(pathname, s, l+1);
 				pathname[l] = 0;
 				for (try=search; !map && *try; try+=l+1) {
@@ -167,7 +164,6 @@ static void do_tzset()
 					memcpy(pathname-l, try, l);
 					map = __map_file(pathname-l, &map_size);
 				}
-				break;
 			}
 		}
 	}
