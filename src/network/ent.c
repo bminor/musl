934aa1350b96461f205ad69c95e8f6f035f6b62c
Rich Felker <dalias@aerifal.cx>
2014-06-04 04:39:34 -0400

    remove some dummy "ent" function aliases that duplicated real ones
    the service and protocol functions are defined also in other files,
    and the protocol ones are actually non-nops elsewhere, so the weak
    definitions in ent.c could have prevented the strong definitions from
    getting pulled in and used in some static programs.

--- src/network/ent.c
+++ src/network/ent.c
@@ -16,11 +16,3 @@ void endhostent(void)
 weak_alias(sethostent, setnetent);
 weak_alias(gethostent, getnetent);
 weak_alias(endhostent, endnetent);
-
-weak_alias(sethostent, setservent);
-weak_alias(gethostent, getservent);
-weak_alias(endhostent, endservent);
-
-weak_alias(sethostent, setprotoent);
-weak_alias(gethostent, getprotoent);
-weak_alias(endhostent, endprotoent);
