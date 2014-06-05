bminor/musl

commit fcc522c92335783293ac19df318415cd97fbf66b

de-duplicate dn_expand, fix return value and signature, clean up
the duplicate code in dn_expand and its incorrect return values are
both results of the history of the code: the version in __dns.c was
originally written with no awareness of the legacy resolver API, and
was later copy-and-paste duplicated to provide the legacy API.

this commit is the first of a series that will restructure the
internal dns code to share as much code as possible with the legacy
resolver API functions.

I have also removed the loop detection logic, since the output buffer
length limit naturally prevents loops. in order to avoid long runtime
when encountering a loop if the caller provided a ridiculously long
buffer, the caller-provided length is clamped at the maximum dns name
length.

src/network/dn_expand.c
@@ -1,28 +1,27 @@
 #include <resolv.h>
-#include <string.h>
+#include "libc.h"
 
-#define BITOP(a,b,op) \
- ((a)[(size_t)(b)/(8*sizeof *(a))] op (size_t)1<<((size_t)(b)%(8*sizeof *(a))))
-
-int dn_expand(unsigned char *b, unsigned char *pz, unsigned char *p, unsigned char *s, int outlen)
+int __dn_expand(const unsigned char *base, const unsigned char *end, const unsigned char *src, char *dest, int space)
 {
-	/* Remember jump destinations to detect loops and abort */
-	size_t seen[512/8/sizeof(size_t)] = { 0 };
-	unsigned char *sz = s + outlen;
-	if (pz-b > 512) return -1;
+	const unsigned char *p = src;
+	int len = -1, j;
+	if (space > 256) space = 256;
+	if (p==end) return -1;
 	for (;;) {
-		if (p>=pz) return -1;
-		else if (*p&0xc0) {
-			int j = (p[0]&1) | p[1];
-			if (BITOP(seen, j, &)) return -1;
-			BITOP(seen, j, |=);
-			p = b + j;
+		if (*p & 0xc0) {
+			if (p+1==end) return -1;
+			j = (p[0]&1) | p[1];
+			if (len < 0) len = p+2-src;
+			if (j >= end-base) return -1;
+			p = base+j;
 		} else if (*p) {
-			if (p+*p+1>=pz || s+*p>=sz) return -1;
-			memcpy(s, p+1, *p);
-			s += *p+1;
-			p += *p+1;
-			s[-1] = *p ? '.' : 0;
-		} else return 0;
+			j = *p+1;
+			if (len < 0) len = p+1-src;
+			if (j>=end-p || j>space) return -1;
+			while (--j) *dest++ = *p++;
+			*dest++ = *++p ? '.' : 0;
+		} else return len;
 	}
 }
+
+weak_alias(__dn_expand, dn_expand);

include/resolv.h
@@ -134,7 +134,7 @@ int res_search(const char *, int, int, unsigned char *, int);
 int res_mkquery(int, const char *, int, int, char *, int, struct rrec *, char *, int);
 int res_send(const char *, int, char *, int);
 int dn_comp(unsigned char *, unsigned char *, int, unsigned char **, unsigned char *, unsigned char **);
-int dn_expand(unsigned char *, unsigned char *, unsigned char *, unsigned char *, int);
+int dn_expand(const unsigned char *, const unsigned char *, const unsigned char *, char *, int);
 int dn_skipname(const unsigned char *, const unsigned char *);
 
 #ifdef __cplusplus

src/network/__dns.c
@@ -195,32 +195,7 @@ int __dns_query(unsigned char *r, const void *a, int family, int ptr)
 	return __dns_doqueries(r, a, rr, rrcnt);
 }
 
-
-#define BITOP(a,b,op) \
- ((a)[(size_t)(b)/(8*sizeof *(a))] op (size_t)1<<((size_t)(b)%(8*sizeof *(a))))
-
-static int decname(char *s, const unsigned char *b, const unsigned char *p)
-{
-	/* Remember jump destinations to detect loops and abort */
-	size_t seen[PACKET_MAX/8/sizeof(size_t)] = { 0 };
-	char *sz = s + HOST_NAME_MAX;
-	const unsigned char *pz = b+512;
-	for (;;) {
-		if (p>=pz) return -1;
-		else if (*p&0xc0) {
-			int j = (p[0]&1) | p[1];
-			if (BITOP(seen, j, &)) return -1;
-			BITOP(seen, j, |=);
-			p = b + j;
-		} else if (*p) {
-			if (p+*p+1>=pz || s+*p>=sz) return -1;
-			memcpy(s, p+1, *p);
-			s += *p+1;
-			p += *p+1;
-			s[-1] = *p ? '.' : 0;
-		} else return 0;
-	}
-}
+int __dn_expand(const unsigned char *, const unsigned char *, const unsigned char *, char *, int);
 
 int __dns_get_rr(void *dest, size_t stride, size_t maxlen, size_t limit, const unsigned char *r, int rr, int dec)
 {
@@ -249,7 +224,8 @@ int __dns_get_rr(void *dest, size_t stride, size_t maxlen, size_t limit, const u
 		len = p[8]*256 + p[9];
 		if (p+len > r+512) return -1;
 		if (p[1]==rr && len <= maxlen) {
-			if (dec && decname(tmp, r, p+10)<0) return -1;
+			if (dec && __dn_expand(r, r+512, p+10, tmp, sizeof tmp)<0)
+				return -1;
 			if (dest && limit) {
 				if (dec) strcpy(dest, tmp);
 				else memcpy(dest, p+10, len);

commit b3d9e0b94ea73c68ef4169ec82c898ce59a4e30a

fix multiple validation issues in dns response label parsing
Due to an error introduced in commit fcc522c92335783293ac19df318415cd97fbf66b,
checking of the remaining output buffer space was not performed correctly,
allowing malformed input to write past the end of the buffer.

In addition, the loop detection logic failed to account for the possibility
of infinite loops with no output, which would hang the function.

The output size is now limited more strictly so only names with valid length
are accepted.

src/network/dn_expand.c
@@ -4,10 +4,11 @@
 int __dn_expand(const unsigned char *base, const unsigned char *end, const unsigned char *src, char *dest, int space)
 {
 	const unsigned char *p = src;
-	int len = -1, j;
-	if (space > 256) space = 256;
+	char *dend = dest + (space > 254 ? 254 : space);
+	int len = -1, i, j;
 	if (p==end || !*p) return -1;
-	for (;;) {
+	/* detect reference loop using an iteration counter */
+	for (i=0; i < end-base; i+=2) {
 		if (*p & 0xc0) {
 			if (p+1==end) return -1;
 			j = ((p[0] & 0x3f) << 8) | p[1];
@@ -16,7 +17,7 @@ int __dn_expand(const unsigned char *base, const unsigned char *end, const unsig
 			p = base+j;
 		} else if (*p) {
 			j = *p+1;
-			if (j>=end-p || j>space) return -1;
+			if (j>=end-p || j>dend-dest) return -1;
 			while (--j) *dest++ = *++p;
 			*dest++ = *++p ? '.' : 0;
 		} else {
@@ -24,6 +25,7 @@ int __dn_expand(const unsigned char *base, const unsigned char *end, const unsig
 			return len;
 		}
 	}
+	return -1;
 }
 
 weak_alias(__dn_expand, dn_expand);