4e5c7a2176773c9a1564c6603240337b3ad6b496
Clément Vasseur <clement.vasseur@gmail.com>
2014-06-13 18:27:56 +0200

    fix missing argument to syscall in fanotify_mark

--- src/linux/fanotify.c
+++ src/linux/fanotify.c
@@ -9,6 +9,6 @@ int fanotify_init(unsigned flags, unsigned event_f_flags)
 int fanotify_mark(int fanotify_fd, unsigned flags, unsigned long long mask,
 	          int dfd, const char *pathname)
 {
-	return syscall(SYS_fanotify_mark, flags, __SYSCALL_LL_O(mask), dfd, pathname);
+	return syscall(SYS_fanotify_mark, fanotify_fd, flags, __SYSCALL_LL_E(mask), dfd, pathname);
 }
 
