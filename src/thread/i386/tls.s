880c479f0e3a798d826c1c91b38d33e2b3e36580
Rich Felker <dalias@aerifal.cx>
2014-06-19 02:48:45 -0400

    optimize i386 ___tls_get_addr asm

--- src/thread/i386/tls.s
+++ src/thread/i386/tls.s
@@ -2,7 +2,14 @@
 .global ___tls_get_addr
 .type ___tls_get_addr,@function
 ___tls_get_addr:
-	push %eax
+	mov %gs:4,%edx
+	mov (%eax),%ecx
+	cmp %ecx,(%edx)
+	jc 1f
+	mov 4(%eax),%eax
+	add (%edx,%ecx,4),%eax
+	ret
+1:	push %eax
 	call __tls_get_addr
 	pop %edx
 	ret
