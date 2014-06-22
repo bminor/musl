76f2bcc7d673d0a1b06ea4b2545b4096979ad518
Rich Felker <dalias@aerifal.cx>
2014-06-22 00:29:32 -0400

    consolidate __xstat abi-compat functions into a single source file
    these are mostly intended for use with dynamic linking (although they
    can also be used statically with object files compiled against glibc
    headers), so having them broken down into separate source files to
    optimize for static linking is unlikely to be worth the cost having
    more files in the source tree (which contributes to libc.a overhead,
    compile time, link time, ar/linker command line size exhaustion, and
    so on).

--- src/stat/__xstat.c
+++ src/stat/__xstat.c
@@ -1,9 +1,27 @@
 #include <sys/stat.h>
 #include "libc.h"
 
+int __fxstat(int ver, int fd, struct stat *buf)
+{
+	return fstat(fd, buf);
+}
+
+int __fxstatat(int ver, int fd, const char *path, struct stat *buf, int flag)
+{
+	return fstatat(fd, path, buf, flag);
+}
+
+int __lxstat(int ver, const char *path, struct stat *buf)
+{
+	return lstat(path, buf);
+}
+
 int __xstat(int ver, const char *path, struct stat *buf)
 {
 	return stat(path, buf);
 }
 
+LFS64(__fxstat);
+LFS64(__fxstatat);
+LFS64(__lxstat);
 LFS64(__xstat);
