1fd0f6e31f157cc9be115c29f0c537d8a225cc86
Rich Felker <dalias@aerifal.cx>
2014-06-22 00:37:12 -0400

    add __xmknod and __xmknodat abi-compat functions
    these are put alongside the similar functions for __xstat, etc. in
    __xstat.c to avoid bloating the number of source files.

--- src/stat/__xstat.c
+++ src/stat/__xstat.c
@@ -25,3 +25,13 @@ LFS64(__fxstat);
 LFS64(__fxstatat);
 LFS64(__lxstat);
 LFS64(__xstat);
+
+int __xmknod(int ver, const char *path, mode_t mode, dev_t *dev)
+{
+	return mknod(path, mode, *dev);
+}
+
+int __xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t *dev)
+{
+	return mknodat(fd, path, mode, *dev);
+}
