9d836ea7a69a6441fcdca815328d274e4ed6b707
Rich Felker <dalias@aerifal.cx>
2014-10-30 20:03:56 -0400

    fix failure of open to read variadic mode argument for O_TMPFILE

--- src/fcntl/open.c
+++ src/fcntl/open.c
@@ -7,7 +7,7 @@ int open(const char *filename, int flags, ...)
 {
 	mode_t mode = 0;
 
-	if (flags & O_CREAT) {
+	if ((flags & O_CREAT) || (flags & O_TMPFILE) == O_TMPFILE) {
 		va_list ap;
 		va_start(ap, flags);
 		mode = va_arg(ap, mode_t);
