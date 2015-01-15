472e8b71f7a90ab23f1499fd721b872541f52de9
Rich Felker <dalias@aerifal.cx>
2015-01-15 07:09:14 -0500

    for multithreaded set*id/setrlimit, handle case where callback does not run
    in the current version of __synccall, the callback is always run, so
    failure to handle this case did not matter. however, the upcoming
    overhaul of __synccall will have failure cases, in which case the
    callback does not run and errno is already set. the changes being
    committed now are in preparation for that.

--- src/unistd/setxid.c
+++ src/unistd/setxid.c
@@ -32,7 +32,7 @@ int __setxid(int nr, int id, int eid, int sid)
 	struct ctx c = { .nr = nr, .id = id, .eid = eid, .sid = sid, .err = -1 };
 	__synccall(do_setxid, &c);
 	if (c.err) {
-		errno = c.err;
+		if (c.err>0) errno = c.err;
 		return -1;
 	}
 	return 0;
