3f92f92cb9c448ba82da5ff2cbbea692c7701c0c
Szabolcs Nagy <nsz@port70.net>
2015-02-08 17:41:56 +0100

    math: fix __fpclassifyl(-0.0) for IEEE binary128
    The sign bit was not cleared before checking for 0 so -0.0
    was misclassified as FP_SUBNORMAL instead of FP_ZERO.

--- src/math/__fpclassifyl.c
+++ src/math/__fpclassifyl.c
@@ -24,12 +24,11 @@ int __fpclassifyl(long double x)
 {
 	union ldshape u = {x};
 	int e = u.i.se & 0x7fff;
+	u.i.se = 0;
 	if (!e)
 		return u.i2.lo | u.i2.hi ? FP_SUBNORMAL : FP_ZERO;
-	if (e == 0x7fff) {
-		u.i.se = 0;
+	if (e == 0x7fff)
 		return u.i2.lo | u.i2.hi ? FP_NAN : FP_INFINITE;
-	}
 	return FP_NORMAL;
 }
 #endif
