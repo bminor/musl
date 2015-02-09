339cc250f6dda06e7a26fcbcc66e3a1bbec2b494
Szabolcs Nagy <nsz@port70.net>
2015-02-09 22:11:52 +0100

    use the internal macro name FUTEX_PRIVATE in __wait
    the name was recently added for the setxid/synccall rework,
    so use the name now that we have it.

--- src/thread/__wait.c
+++ src/thread/__wait.c
@@ -3,7 +3,7 @@
 void __wait(volatile int *addr, volatile int *waiters, int val, int priv)
 {
 	int spins=100;
-	if (priv) priv = 128;
+	if (priv) priv = FUTEX_PRIVATE;
 	while (spins-- && (!waiters || !*waiters)) {
 		if (*addr==val) a_spin();
 		else return;
