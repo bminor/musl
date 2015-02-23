2de85a985654d2c944931267645d9a0686242dfe
Rich Felker <dalias@aerifal.cx>
2015-02-23 18:53:01 -0500

    fix possible isatty false positives and unwanted device state changes
    the equivalent checks for newly opened stdio output streams, used to
    determine buffering mode, are also fixed.
    
    on most archs, the TCGETS ioctl command shares a value with
    SNDCTL_TMR_TIMEBASE, part of the OSS sound API which was apparently
    used with certain MIDI and timer devices. for file descriptors
    referring to such a device, TCGETS will not fail with ENOTTY as
    expected; it may produce a different error, or may succeed, and if it
    succeeds it changes the mode of the device. while it's unlikely that
    such devices are in use, this is in principle very harmful behavior
    for an operation which is supposed to do nothing but query whether the
    fd refers to a tty.
    
    TIOCGWINSZ, used to query logical window size for a terminal, was
    chosen as an alternate ioctl to perform the isatty check. it does not
    share a value with any other ioctl commands, and it succeeds on any
    tty device.
    
    this change also cleans up strace output to be less ugly and
    misleading.

--- src/stdio/__fdopen.c
+++ src/stdio/__fdopen.c
@@ -1,6 +1,5 @@
 #include "stdio_impl.h"
 #include <stdlib.h>
-#include <termios.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -9,7 +8,7 @@
 FILE *__fdopen(int fd, const char *mode)
 {
 	FILE *f;
-	struct termios tio;
+	struct winsize wsz;
 
 	/* Check for valid initial mode character */
 	if (!strchr("rwa", *mode)) {
@@ -43,7 +42,7 @@ FILE *__fdopen(int fd, const char *mode)
 
 	/* Activate line buffered mode for terminals */
 	f->lbf = EOF;
-	if (!(f->flags & F_NOWR) && !__syscall(SYS_ioctl, fd, TCGETS, &tio))
+	if (!(f->flags & F_NOWR) && !__syscall(SYS_ioctl, fd, TIOCGWINSZ, &wsz))
 		f->lbf = '\n';
 
 	/* Initialize op ptrs. No problem if some are unneeded. */
