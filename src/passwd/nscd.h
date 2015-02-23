34b423d2077a4c799d2089068d3bec91fb800256
Josiah Worcester <josiahw@gmail.com>
2015-02-22 20:58:10 -0600

    support alternate backends for the passwd and group dbs
    when we fail to find the entry in the commonly accepted files,  we
    query a server over a Unix domain socket on /var/run/nscd/socket.
    the protocol used here is compatible with glibc's nscd protocol on
    most systems (all that use 32-bit numbers for all the protocol fields,
    which appears to be everything but Alpha).

---
+++ src/passwd/nscd.h
@@ -0,0 +1,38 @@
+#ifndef NSCD_H
+#define NSCD_H
+
+#include <stdint.h>
+
+#define NSCDVERSION 2
+#define GETPWBYNAME 0
+#define GETPWBYUID 1
+#define GETGRBYNAME 2
+#define GETGRBYGID 3
+
+#define REQVERSION 0
+#define REQTYPE 1
+#define REQKEYLEN 2
+#define REQ_LEN 3
+
+#define PWVERSION 0
+#define PWFOUND 1
+#define PWNAMELEN 2
+#define PWPASSWDLEN 3
+#define PWUID 4
+#define PWGID 5
+#define PWGECOSLEN 6
+#define PWDIRLEN 7
+#define PWSHELLLEN 8
+#define PW_LEN 9
+
+#define GRVERSION 0
+#define GRFOUND 1
+#define GRNAMELEN 2
+#define GRPASSWDLEN 3
+#define GRGID 4
+#define GRMEMCNT 5
+#define GR_LEN 6
+
+FILE *__nscd_query(int32_t req, const char *key, int32_t *buf, size_t len, int *swap);
+
+#endif
