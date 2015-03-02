a3de8f7fbc5a2cc838b96e8702363ef7682da5ae
Rich Felker <dalias@aerifal.cx>
2015-03-02 18:48:58 -0500

    fix namespace issue in pthread_join affecting thrd_join
    pthread_testcancel is not in the ISO C reserved namespace and thus
    cannot be used here. use the namespace-protected version of the
    function instead.

--- src/thread/pthread_join.c
+++ src/thread/pthread_join.c
@@ -2,11 +2,12 @@
 #include <sys/mman.h>
 
 int __munmap(void *, size_t);
+void __pthread_testcancel(void);
 
 int __pthread_join(pthread_t t, void **res)
 {
 	int tmp;
-	pthread_testcancel();
+	__pthread_testcancel();
 	while ((tmp = t->tid)) __timedwait_cp(&t->tid, tmp, 0, 0, 0);
 	if (res) *res = t->result;
 	if (t->map_base) __munmap(t->map_base, t->map_size);
