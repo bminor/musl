fd427c4eaeed4d1099acc7b6188e41c6cfa5bca5
Rich Felker <dalias@aerifal.cx>
2015-04-01 19:31:06 -0400

    move O_PATH definition back to arch bits
    while it's the same for all presently supported archs, it differs at
    least on sparc, and conceptually it's no less arch-specific than the
    other O_* macros. O_SEARCH and O_EXEC are still defined in terms of
    O_PATH in the main fcntl.h.

--- include/fcntl.h
+++ include/fcntl.h
@@ -37,9 +37,8 @@ int openat(int, const char *, int, ...);
 int posix_fadvise(int, off_t, off_t, int);
 int posix_fallocate(int, off_t, off_t);
 
-#define O_SEARCH  010000000
-#define O_EXEC    010000000
-#define O_PATH    010000000
+#define O_SEARCH  O_PATH
+#define O_EXEC    O_PATH
 
 #define O_ACCMODE (03|O_SEARCH)
 #define O_RDONLY  00
