4e50b2e4b5c32b804324eb6967a9ca72c9a2b654
Szabolcs Nagy <nsz@port70.net>
2015-04-30 18:47:39 +0100

    aarch64: fix CRTJMP in reloc.h
    commit f3ddd173806fd5c60b3f034528ca24542aecc5b9 broke the build by
    using "bx" instead of "br".

--- arch/aarch64/reloc.h
+++ arch/aarch64/reloc.h
@@ -23,4 +23,4 @@
 #define REL_TLSDESC     R_AARCH64_TLSDESC
 
 #define CRTJMP(pc,sp) __asm__ __volatile__( \
-	"mov sp,%1 ; bx %0" : : "r"(pc), "r"(sp) : "memory" )
+	"mov sp,%1 ; br %0" : : "r"(pc), "r"(sp) : "memory" )

bminor/musl

commit f3ddd173806fd5c60b3f034528ca24542aecc5b9

dynamic linker bootstrap overhaul
this overhaul further reduces the amount of arch-specific code needed
by the dynamic linker and removes a number of assumptions, including:

- that symbolic function references inside libc are bound at link time
  via the linker option -Bsymbolic-functions.

- that libc functions used by the dynamic linker do not require
  access to data symbols.

- that static/internal function calls and data accesses can be made
  without performing any relocations, or that arch-specific startup
  code handled any such relocations needed.

removing these assumptions paves the way for allowing libc.so itself
to be built with stack protector (among other things), and is achieved
by a three-stage bootstrap process:

1. relative relocations are processed with a flat function.
2. symbolic relocations are processed with no external calls/data.
3. main program and dependency libs are processed with a
   fully-functional libc/ldso.

reduction in arch-specific code is achived through the following:

- crt_arch.h, used for generating crt1.o, now provides the entry point
  for the dynamic linker too.

- asm is no longer responsible for skipping the beginning of argv[]
  when ldso is invoked as a command.

- the functionality previously provided by __reloc_self for heavily
  GOT-dependent RISC archs is now the arch-agnostic stage-1.

- arch-specific relocation type codes are mapped directly as macros
  rather than via an inline translation function/switch statement.

arch/aarch64/reloc.h
@@ -1,5 +1,3 @@
-#include <string.h>
-#include <elf.h>
 #include <endian.h>
 
 #if __BYTE_ORDER == __BIG_ENDIAN
@@ -14,27 +12,15 @@
 
 #define TPOFF_K 16
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_AARCH64_ABS64:
-		return REL_SYMBOLIC;
-	case R_AARCH64_GLOB_DAT:
-		return REL_GOT;
-	case R_AARCH64_JUMP_SLOT:
-		return REL_PLT;
-	case R_AARCH64_RELATIVE:
-		return REL_RELATIVE;
-	case R_AARCH64_COPY:
-		return REL_COPY;
-	case R_AARCH64_TLS_DTPMOD64:
-		return REL_DTPMOD;
-	case R_AARCH64_TLS_DTPREL64:
-		return REL_DTPOFF;
-	case R_AARCH64_TLS_TPREL64:
-		return REL_TPOFF;
-	case R_AARCH64_TLSDESC:
-		return REL_TLSDESC;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_AARCH64_ABS64
+#define REL_GOT         R_AARCH64_GLOB_DAT
+#define REL_PLT         R_AARCH64_JUMP_SLOT
+#define REL_RELATIVE    R_AARCH64_RELATIVE
+#define REL_COPY        R_AARCH64_COPY
+#define REL_DTPMOD      R_AARCH64_TLS_DTPMOD64
+#define REL_DTPOFF      R_AARCH64_TLS_DTPREL64
+#define REL_TPOFF       R_AARCH64_TLS_TPREL64
+#define REL_TLSDESC     R_AARCH64_TLSDESC
+
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"mov sp,%1 ; bx %0" : : "r"(pc), "r"(sp) : "memory" )

arch/arm/reloc.h
@@ -1,5 +1,3 @@
-#include <string.h>
-#include <elf.h>
 #include <endian.h>
 
 #if __BYTE_ORDER == __BIG_ENDIAN
@@ -20,25 +18,15 @@
 
 #define TPOFF_K 8
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_ARM_ABS32:
-		return REL_SYMBOLIC;
-	case R_ARM_GLOB_DAT:
-		return REL_GOT;
-	case R_ARM_JUMP_SLOT:
-		return REL_PLT;
-	case R_ARM_RELATIVE:
-		return REL_RELATIVE;
-	case R_ARM_COPY:
-		return REL_COPY;
-	case R_ARM_TLS_DTPMOD32:
-		return REL_DTPMOD;
-	case R_ARM_TLS_DTPOFF32:
-		return REL_DTPOFF;
-	case R_ARM_TLS_TPOFF32:
-		return REL_TPOFF;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_ARM_ABS32
+#define REL_GOT         R_ARM_GLOB_DAT
+#define REL_PLT         R_ARM_JUMP_SLOT
+#define REL_RELATIVE    R_ARM_RELATIVE
+#define REL_COPY        R_ARM_COPY
+#define REL_DTPMOD      R_ARM_TLS_DTPMOD32
+#define REL_DTPOFF      R_ARM_TLS_DTPOFF32
+#define REL_TPOFF       R_ARM_TLS_TPOFF32
+//#define REL_TLSDESC     R_ARM_TLS_DESC
+
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"mov sp,%1 ; tst %0,#1 ; moveq pc,%0 ; bx %0" : : "r"(pc), "r"(sp) : "memory" )

arch/i386/reloc.h
@@ -1,33 +1,16 @@
-#include <string.h>
-#include <elf.h>
-
 #define LDSO_ARCH "i386"
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_386_32:
-		return REL_SYMBOLIC;
-	case R_386_PC32:
-		return REL_OFFSET;
-	case R_386_GLOB_DAT:
-		return REL_GOT;
-	case R_386_JMP_SLOT:
-		return REL_PLT;
-	case R_386_RELATIVE:
-		return REL_RELATIVE;
-	case R_386_COPY:
-		return REL_COPY;
-	case R_386_TLS_DTPMOD32:
-		return REL_DTPMOD;
-	case R_386_TLS_DTPOFF32:
-		return REL_DTPOFF;
-	case R_386_TLS_TPOFF:
-		return REL_TPOFF;
-	case R_386_TLS_TPOFF32:
-		return REL_TPOFF_NEG;
-	case R_386_TLS_DESC:
-		return REL_TLSDESC;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_386_32
+#define REL_OFFSET      R_386_PC32
+#define REL_GOT         R_386_GLOB_DAT
+#define REL_PLT         R_386_JMP_SLOT
+#define REL_RELATIVE    R_386_RELATIVE
+#define REL_COPY        R_386_COPY
+#define REL_DTPMOD      R_386_TLS_DTPMOD32
+#define REL_DTPOFF      R_386_TLS_DTPOFF32
+#define REL_TPOFF       R_386_TLS_TPOFF
+#define REL_TPOFF_NEG   R_386_TLS_TPOFF32
+#define REL_TLSDESC     R_386_TLS_DESC
+
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"mov %1,%%esp ; jmp *%0" : : "r"(pc), "r"(sp) : "memory" )

arch/microblaze/reloc.h
@@ -1,5 +1,3 @@
-#include <string.h>
-#include <elf.h>
 #include <endian.h>
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
@@ -12,40 +10,13 @@
 
 #define TPOFF_K 0
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_MICROBLAZE_32:
-		return REL_SYMBOLIC;
-	case R_MICROBLAZE_GLOB_DAT:
-		return REL_GOT;
-	case R_MICROBLAZE_JUMP_SLOT:
-		return REL_PLT;
-	case R_MICROBLAZE_REL:
-		return REL_RELATIVE;
-	case R_MICROBLAZE_COPY:
-		return REL_COPY;
-	case R_MICROBLAZE_TLSDTPMOD32:
-		return REL_DTPMOD;
-	case R_MICROBLAZE_TLSDTPREL32:
-		return REL_DTPOFF;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_MICROBLAZE_32
+#define REL_GOT         R_MICROBLAZE_GLOB_DAT
+#define REL_PLT         R_MICROBLAZE_JUMP_SLOT
+#define REL_RELATIVE    R_MICROBLAZE_REL
+#define REL_COPY        R_MICROBLAZE_COPY
+#define REL_DTPMOD      R_MICROBLAZE_TLSDTPMOD32
+#define REL_DTPOFF      R_MICROBLAZE_TLSDTPREL32
 
-#include "syscall.h"
-void __reloc_self(int c, size_t *a, size_t *dynv)
-{
-	char dot = '.', ex = 'x';
-	char *base;
-	size_t t[20], n;
-	for (a+=c+1; *a; a++);
-	for (a++; *a; a+=2) if (*a<20) t[*a] = a[1];
-	base = (char *)t[AT_BASE];
-	if (!base) base = (char *)(t[AT_PHDR] & -t[AT_PAGESZ]);
-	for (a=dynv; *a; a+=2) if (*a<20) t[*a] = a[1];
-	n = t[DT_RELASZ];
-	for (a=(void *)(base+t[DT_RELA]); n; a+=3, n-=12)
-		if (a[1]%256 == R_MICROBLAZE_REL)
-			*(size_t *)(base+a[0]) = (size_t)base + a[2];
-}
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"addik r1,%1,0 ; bra %0" : : "r"(pc), "r"(sp) : "memory" )

arch/mips/reloc.h
@@ -1,5 +1,3 @@
-#include <string.h>
-#include <elf.h>
 #include <endian.h>
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
@@ -18,72 +16,16 @@
 
 #define TPOFF_K (-0x7000)
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_MIPS_REL32:
-		return REL_SYM_OR_REL;
-	case R_MIPS_JUMP_SLOT:
-		return REL_PLT;
-	case R_MIPS_COPY:
-		return REL_COPY;
-	case R_MIPS_TLS_DTPMOD32:
-		return REL_DTPMOD;
-	case R_MIPS_TLS_DTPREL32:
-		return REL_DTPOFF;
-	case R_MIPS_TLS_TPREL32:
-		return REL_TPOFF;
-	}
-	return 0;
-}
+#define REL_SYM_OR_REL  R_MIPS_REL32
+#define REL_PLT         R_MIPS_JUMP_SLOT
+#define REL_COPY        R_MIPS_COPY
+#define REL_DTPMOD      R_MIPS_TLS_DTPMOD32
+#define REL_DTPOFF      R_MIPS_TLS_DTPREL32
+#define REL_TPOFF       R_MIPS_TLS_TPREL32
 
-void __reloc_self(int c, size_t *a, size_t *dynv, size_t *got)
-{
-	char *base;
-	size_t t[20], n;
-	for (a+=c+1; *a; a++);
-	for (a++; *a; a+=2) if (*a<20) t[*a] = a[1];
-	base = (char *)t[AT_BASE];
-	if (!base) base = (char *)(t[AT_PHDR] & -t[AT_PAGESZ]);
-	for (a=dynv; *a; a+=2) if (*a-0x70000000UL<20) t[*a&31] = a[1];
-	n = t[DT_MIPS_LOCAL_GOTNO - 0x70000000];
-	for (a=got; n; a++, n--) *a += (size_t)base;
-}
-
-static void do_relocs(struct dso *dso, size_t *rel, size_t rel_size, size_t stride);
-
-static void do_arch_relocs(struct dso *this, struct dso *head)
-{
-	unsigned char *base = this->base;
-	size_t *dynv = this->dynv;
-	size_t dyn[20] = {0};
-	size_t i;
-	size_t rel[2], got=0;
-	Sym *sym;
-
-	for (i=0; dynv[i]; i+=2) {
-		if (dynv[i]-0x70000000UL<20)
-			dyn[dynv[i]&31] = dynv[i+1];
-		else if (dynv[i] == DT_PLTGOT)
-			got = dynv[i+1];
-	}
-	i = dyn[DT_MIPS_LOCAL_GOTNO-0x70000000];
-	if (this->shortname && !strcmp(this->shortname, "libc.so")) {
-		got += sizeof(size_t) * i;
-	} else {
-		for (; i; i--, got+=sizeof(size_t))
-			*(size_t *)(base+got) += (size_t)base;
-	}
-	sym = this->syms + dyn[DT_MIPS_GOTSYM-0x70000000];
-	i = dyn[DT_MIPS_SYMTABNO-0x70000000] - dyn[DT_MIPS_GOTSYM-0x70000000];
-	for (; i; i--, got+=sizeof(size_t), sym++) {
-		rel[0] = got;
-		rel[1] = sym-this->syms << 8 | R_MIPS_JUMP_SLOT;
-		*(size_t *)(base+got) = 0;
-		do_relocs(this, rel, sizeof rel, 2);
-	}
-}
-
-#define NEED_ARCH_RELOCS 1
+#define NEED_MIPS_GOT_RELOCS 1
 #define DYNAMIC_IS_RO 1
 #define ARCH_SYM_REJECT_UND(s) (!((s)->st_other & STO_MIPS_PLT))
+
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"move $sp,%1 ; jr %0" : : "r"(pc), "r"(sp) : "memory" )

arch/or1k/reloc.h
@@ -1,47 +1,15 @@
-#include <string.h>
-#include <elf.h>
-#include <endian.h>
-
 #define LDSO_ARCH "or1k"
 
 #define TPOFF_K 0
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_OR1K_32:
-		return REL_SYMBOLIC;
-	case R_OR1K_GLOB_DAT:
-		return REL_GOT;
-	case R_OR1K_JMP_SLOT:
-		return REL_PLT;
-	case R_OR1K_RELATIVE:
-		return REL_RELATIVE;
-	case R_OR1K_COPY:
-		return REL_COPY;
-	case R_OR1K_TLS_DTPMOD:
-		return REL_DTPMOD;
-	case R_OR1K_TLS_DTPOFF:
-		return REL_DTPOFF;
-	case R_OR1K_TLS_TPOFF:
-		return REL_TPOFF;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_OR1K_32
+#define REL_GOT         R_OR1K_GLOB_DAT
+#define REL_PLT         R_OR1K_JMP_SLOT
+#define REL_RELATIVE    R_OR1K_RELATIVE
+#define REL_COPY        R_OR1K_COPY
+#define REL_DTPMOD      R_OR1K_TLS_DTPMOD
+#define REL_DTPOFF      R_OR1K_TLS_DTPOFF
+#define REL_TPOFF       R_OR1K_TLS_TPOFF
 
-#include "syscall.h"
-void __reloc_self(int c, size_t *a, size_t *dynv)
-{
-	char dot = '.', ex = 'x';
-	char *base;
-	size_t t[20], n;
-	for (a+=c+1; *a; a++);
-	for (a++; *a; a+=2) if (*a<20) t[*a] = a[1];
-	base = (char *)t[AT_BASE];
-	if (!base) base = (char *)(t[AT_PHDR] & -t[AT_PAGESZ]);
-	for (a=dynv; *a; a+=2) if (*a<20) t[*a] = a[1];
-	n = t[DT_RELASZ];
-	for (a=(void *)(base+t[DT_RELA]); n; a+=3, n-=12)
-		if (a[1]%256 == R_OR1K_RELATIVE)
-			*(size_t *)(base+a[0]) = (size_t)base + a[2];
-}
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"l.jr %0 ; l.ori r1,%1,0" : : "r"(pc), "r"(sp) : "memory" )

arch/powerpc/reloc.h
@@ -1,44 +1,15 @@
-#include <string.h>
-#include <elf.h>
-
 #define LDSO_ARCH "powerpc"
 
 #define TPOFF_K (-0x7000)
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_PPC_ADDR32:
-		return REL_SYMBOLIC;
-	case R_PPC_GLOB_DAT:
-		return REL_GOT;
-	case R_PPC_JMP_SLOT:
-		return REL_PLT;
-	case R_PPC_RELATIVE:
-		return REL_RELATIVE;
-	case R_PPC_COPY:
-		return REL_COPY;
-	case R_PPC_DTPMOD32:
-		return REL_DTPMOD;
-	case R_PPC_DTPREL32:
-		return REL_DTPOFF;
-	case R_PPC_TPREL32:
-		return REL_TPOFF;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_PPC_ADDR32
+#define REL_GOT         R_PPC_GLOB_DAT
+#define REL_PLT         R_PPC_JMP_SLOT
+#define REL_RELATIVE    R_PPC_RELATIVE
+#define REL_COPY        R_PPC_COPY
+#define REL_DTPMOD      R_PPC_DTPMOD32
+#define REL_DTPOFF      R_PPC_DTPREL32
+#define REL_TPOFF       R_PPC_TPREL32
 
-void __reloc_self(int c, size_t *a, size_t *dynv)
-{
-	char *base;
-	size_t t[20], n;
-	for (a+=c+1; *a; a++);
-	for (a++; *a; a+=2) if (*a<20) t[*a] = a[1];
-	base = (char *)t[AT_BASE];
-	if (!base) base = (char *)(t[AT_PHDR] & -t[AT_PAGESZ]);
-	for (a=dynv; *a; a+=2) if (*a<20) t[*a] = a[1];
-	n = t[DT_RELASZ];
-	for (a=(void *)(base+t[DT_RELA]); n; a+=3, n-=12)
-		if (a[1]%256 == R_PPC_RELATIVE)
-			*(size_t *)(base+a[0]) = (size_t)base + a[2];
-}
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"mr 1,%1 ; mtlr %0 ; blr" : : "r"(pc), "r"(sp) : "memory" )

arch/sh/reloc.h
@@ -8,27 +8,15 @@
 
 #define TPOFF_K 8
 
-static int remap_rel(int type)
-{
-	switch(type) {
-	case R_SH_DIR32:
-		return REL_SYMBOLIC;
-	case R_SH_REL32:
-		return REL_OFFSET;
-	case R_SH_GLOB_DAT:
-		return REL_GOT;
-	case R_SH_JMP_SLOT:
-		return REL_PLT;
-	case R_SH_RELATIVE:
-		return REL_RELATIVE;
-	case R_SH_COPY:
-		return REL_COPY;
-	case R_SH_TLS_DTPMOD32:
-		return REL_DTPMOD;
-	case R_SH_TLS_DTPOFF32:
-		return REL_DTPOFF;
-	case R_SH_TLS_TPOFF32:
-		return REL_TPOFF;
-	}
-	return 0;
-}
+#define REL_SYMBOLIC    R_SH_DIR32
+#define REL_OFFSET      R_SH_REL32
+#define REL_GOT         R_SH_GLOB_DAT
+#define REL_PLT         R_SH_JMP_SLOT
+#define REL_RELATIVE    R_SH_RELATIVE
+#define REL_COPY        R_SH_COPY
+#define REL_DTPMOD      R_SH_TLS_DTPMOD32
+#define REL_DTPOFF      R_SH_TLS_DTPOFF32
+#define REL_TPOFF       R_SH_TLS_TPOFF32
+
+#define CRTJMP(pc,sp) __asm__ __volatile__( \
+	"jmp @%0 ; mov %1,r15" : : "r"(pc), "r"(sp) : "memory" )

commit 4e50b2e4b5c32b804324eb6967a9ca72c9a2b654

aarch64: fix CRTJMP in reloc.h
commit f3ddd173806fd5c60b3f034528ca24542aecc5b9 broke the build by
using "bx" instead of "br".

arch/aarch64/reloc.h
@@ -23,4 +23,4 @@
 #define REL_TLSDESC     R_AARCH64_TLSDESC
 
 #define CRTJMP(pc,sp) __asm__ __volatile__( \
-	"mov sp,%1 ; bx %0" : : "r"(pc), "r"(sp) : "memory" )
+	"mov sp,%1 ; br %0" : : "r"(pc), "r"(sp) : "memory" )