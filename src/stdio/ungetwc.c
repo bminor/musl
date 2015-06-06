312eea2ea4f4363fb01b73660c08bfcf43dd3bb4
Rich Felker <dalias@aerifal.cx>
2015-06-06 18:20:30 +0000

    remove another invalid skip of locking in ungetwc

--- src/stdio/ungetwc.c
+++ src/stdio/ungetwc.c
@@ -9,14 +9,12 @@ wint_t ungetwc(wint_t c, FILE *f)
 	unsigned char mbc[MB_LEN_MAX];
 	int l=1;
 
-	if (c == WEOF) return c;
-
 	FLOCK(f);
 
 	f->mode |= f->mode+1;
 
 	if (!f->rpos) __toread(f);
-	if (!f->rpos || f->rpos < f->buf - UNGET + l ||
+	if (!f->rpos || f->rpos < f->buf - UNGET + l || c == WEOF ||
 	    (!isascii(c) && (l = wctomb((void *)mbc, c)) < 0)) {
 		FUNLOCK(f);
 		return WEOF;
