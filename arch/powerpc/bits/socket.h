3975577922aedab7d60788dd320a2c8e4e94bc6e
Roman Yeryomin <roman@ubnt.com>
2015-07-02 12:29:00 +0300

    socket.h: cleanup/reorder mips and powerpc bits/socket.h
    ....to be somewhat consistent and easily comparable with asm/socket.h
    
    Signed-off-by: Roman Yeryomin <roman@ubnt.com>

--- arch/powerpc/bits/socket.h
+++ arch/powerpc/bits/socket.h
@@ -24,8 +24,6 @@ struct cmsghdr
 #define SO_BROADCAST    6
 #define SO_SNDBUF       7
 #define SO_RCVBUF       8
-#define SO_SNDBUFFORCE  32
-#define SO_RCVBUFFORCE  33
 #define SO_KEEPALIVE    9
 #define SO_OOBINLINE    10
 #define SO_NO_CHECK     11
@@ -40,5 +38,7 @@ struct cmsghdr
 #define SO_PASSCRED     20
 #define SO_PEERCRED     21
 #define SO_ACCEPTCONN   30
+#define SO_SNDBUFFORCE  32
+#define SO_RCVBUFFORCE  33
 #define SO_PROTOCOL     38
 #define SO_DOMAIN       39
