4ccc1a01e037251a0ddc3dd07dd1a1fb91a8aa16
Rich Felker <dalias@aerifal.cx>
2015-09-12 03:18:08 +0000

    add fdpic version of entry point code for sh
    this version of the entry point is only suitable for static linking in
    ET_EXEC form. neither dynamic linking nor pie is supported yet. at
    some point in the future the fdpic and non-fdpic versions of this code
    may be unified but for now it's easiest to work with them separately.

--- arch/sh/crt_arch.h
+++ arch/sh/crt_arch.h
@@ -1,3 +1,30 @@
+#ifdef __SH_FDPIC__
+
+__asm__(
+".text \n"
+".global " START " \n"
+START ": \n"
+"	mov r8, r4 \n"
+"	mova 1f, r0 \n"
+"	mov.l 1f, r5 \n"
+"	mov.l 1f+4, r6 \n"
+"	add r0, r5 \n"
+"	bsr __fdpic_fixup \n"
+"	 add r0, r6 \n"
+"	mov r0, r12 \n"
+"	mov r15, r4 \n"
+"	mov #-16, r0 \n"
+"	bsr " START "_c \n"
+"	 and r0, r15 \n"
+".align 2 \n"
+"1:	.long __ROFIXUP_LIST__@PCREL \n"
+"	.long __ROFIXUP_END__@PCREL + 4 \n"
+);
+
+#include "fdpic_crt.h"
+
+#else
+
 __asm__(
 ".text \n"
 ".global " START " \n"
@@ -15,6 +42,8 @@ START ": \n"
 ".hidden _DYNAMIC \n"
 "1:	.long _DYNAMIC-. \n"
 );
+
+#endif
 
 /* used by gcc for switching the FPU between single and double precision */
 #ifdef SHARED
