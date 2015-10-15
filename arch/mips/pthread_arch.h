11da520c7af5b190e69c0906c6e95000c4e59f33
Rich Felker <dalias@aerifal.cx>
2015-10-15 00:55:41 -0400

    add comment documenting hard-coded opcode for reading mips thread pointer

--- arch/mips/pthread_arch.h
+++ arch/mips/pthread_arch.h
@@ -5,6 +5,7 @@ static inline struct pthread *__pthread_self()
 	__asm__ __volatile__ (".word 0x7c03e83b ; move %0, $3" : "=r" (tp) : : "$3" );
 #else
 	register char *tp __asm__("$3");
+	/* rdhwr $3,$29 */
 	__asm__ __volatile__ (".word 0x7c03e83b" : "=r" (tp) );
 #endif
 	return (pthread_t)(tp - 0x7000 - sizeof(struct pthread));
