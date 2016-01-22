230bfe1a7d46c85601e5e67497cc607e15d08990
Rich Felker <dalias@aerifal.cx>
2016-01-22 03:46:00 +0000

    move sh __unmapself code from arch/sh/src to main src tree

--- src/thread/sh/__unmapself.c
+++ src/thread/sh/__unmapself.c
@@ -10,7 +10,7 @@ void __unmapself_sh_nommu(void *, size_t);
 #define CRTJMP(pc,sp) __asm__ __volatile__( \
 	"mov.l @%0+,r0 ; mov.l @%0,r12 ; jmp @r0 ; mov %1,r15" \
 	: : "r"(pc), "r"(sp) : "r0", "memory" )
-#include "../../../src/thread/__unmapself.c"
+#include "../__unmapself.c"
 #undef __unmapself
 extern __attribute__((__visibility__("hidden"))) unsigned __sh_nommu;
 #else
