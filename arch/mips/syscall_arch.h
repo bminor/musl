b0bf52f3e9fd246762958821fdc2851add05d423
Hauke Mehrtens <hauke@hauke-m.de>
2016-01-26 21:26:34 +0100

    mips: add vdso support
    vdso support is available on mips starting with kernel 4.4, see kernel
    commit a7f4df4e21 "MIPS: VDSO: Add implementations of gettimeofday()
    and clock_gettime()" for details.
    
    In Linux kernel 4.4.0 the mips code returns -ENOSYS in case it can not
    handle the vdso call and assumes the libc will call the original
    syscall in this case. Handle this case in musl. Currently Linux kernel
    4.4.0 handles the following types: CLOCK_REALTIME_COARSE,
    CLOCK_MONOTONIC_COARSE, CLOCK_REALTIME and CLOCK_MONOTONIC.

--- arch/mips/syscall_arch.h
+++ arch/mips/syscall_arch.h
@@ -161,3 +161,7 @@ static inline long __syscall6(long n, long a, long b, long c, long d, long e, lo
 	if (n == SYS_fstatat) __stat_fix(c);
 	return r2;
 }
+
+#define VDSO_USEFUL
+#define VDSO_CGT_SYM "__vdso_clock_gettime"
+#define VDSO_CGT_VER "LINUX_2.6"
