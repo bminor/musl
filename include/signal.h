22f84829afa06bba9d220bd0f7b1bce912987297
Szabolcs Nagy <nsz@port70.net>
2016-01-27 00:40:32 +0000

    move bits/signal.h include close to the top of signal.h
    only have code above the bits/signal.h include that is necessary.
    (some types are used for the ucontext struct and mips has to
    override a few macro definitions)
    
    this way mips bits/signal.h will be able to affect siginfo_t.

--- include/signal.h
+++ include/signal.h
@@ -27,8 +27,6 @@ extern "C" {
 
 #include <bits/alltypes.h>
 
-#define SIG_HOLD ((void (*)(int)) 2)
-
 #define SIG_BLOCK     0
 #define SIG_UNBLOCK   1
 #define SIG_SETMASK   2
@@ -42,6 +40,18 @@ extern "C" {
 #define SI_QUEUE (-1)
 #define SI_USER 0
 #define SI_KERNEL 128
+
+typedef struct sigaltstack stack_t;
+
+#endif
+
+#include <bits/signal.h>
+
+#if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) \
+ || defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) \
+ || defined(_BSD_SOURCE)
+
+#define SIG_HOLD ((void (*)(int)) 2)
 
 #define FPE_INTDIV 1
 #define FPE_INTOVF 2
@@ -77,8 +87,6 @@ extern "C" {
 #define CLD_TRAPPED 4
 #define CLD_STOPPED 5
 #define CLD_CONTINUED 6
-
-typedef struct sigaltstack stack_t;
 
 union sigval {
 	int sival_int;
@@ -239,8 +247,6 @@ int sigandset(sigset_t *, const sigset_t *, const sigset_t *);
 #define SA_NOMASK SA_NODEFER
 #define SA_ONESHOT SA_RESETHAND
 #endif
-
-#include <bits/signal.h>
 
 #define SIG_ERR  ((void (*)(int))-1)
 #define SIG_DFL  ((void (*)(int)) 0)
