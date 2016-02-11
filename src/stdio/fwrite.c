500c6886c654fd45e4926990fee2c61d816be197
Rich Felker <dalias@aerifal.cx>
2016-02-10 19:44:19 -0500

    fix return value for fread/fwrite when size argument is 0
    when the size argument was zero but nmemb was nonzero, these functions
    were returning nmemb, despite no data having been written.
    conceptually this is not wrong, but the standard requires a return
    value of zero in this case.

--- src/stdio/fwrite.c
+++ src/stdio/fwrite.c
@@ -28,6 +28,7 @@ size_t __fwritex(const unsigned char *restrict s, size_t l, FILE *restrict f)
 size_t fwrite(const void *restrict src, size_t size, size_t nmemb, FILE *restrict f)
 {
 	size_t k, l = size*nmemb;
+	if (!size) nmemb = 0;
 	FLOCK(f);
 	k = __fwritex(src, l, f);
 	FUNLOCK(f);
