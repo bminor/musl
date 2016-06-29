804debee2b1550d10b5ea7290240860dca40ea72
Rich Felker <dalias@aerifal.cx>
2016-06-29 17:45:40 -0400

    in posix_fadvise, don't bypass __syscall macro infrastructure
    when commit 0b6eb2dfb2e84a8a51906e7634f3d5edc230b058 added the
    parentheses around __syscall to invoke the function directly, there
    was no __syscall7 in the syscall macro infrastructure, so this hack
    was needed. commit 9a3bbce447403d735282586786dc436ec1ffbad4 fixed that
    but failed to remove the hack.

--- src/fcntl/posix_fadvise.c
+++ src/fcntl/posix_fadvise.c
@@ -4,7 +4,7 @@
 
 int posix_fadvise(int fd, off_t base, off_t len, int advice)
 {
-	return -(__syscall)(SYS_fadvise, fd, __SYSCALL_LL_O(base),
+	return -__syscall(SYS_fadvise, fd, __SYSCALL_LL_O(base),
 		__SYSCALL_LL_E(len), advice);
 }
 
