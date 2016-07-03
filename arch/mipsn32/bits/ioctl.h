b76d4e06f1686337713a31a9e7274e1321ae41b6
Szabolcs Nagy <nsz@port70.net>
2016-07-03 17:05:36 +0200

    fix mips, mips64, mipsn32 TIOCM_* macros in ioctl.h
    TIOCM_ macros were wrongly using the asm-generic/termios.h definitions
    instead of the mips specific ones from asm/termios.h

--- arch/mipsn32/bits/ioctl.h
+++ arch/mipsn32/bits/ioctl.h
@@ -107,20 +107,20 @@ struct winsize {
 	unsigned short ws_ypixel;
 };
 
-#define TIOCM_LE        0x001
-#define TIOCM_DTR       0x002
-#define TIOCM_RTS       0x004
-#define TIOCM_ST        0x008
-#define TIOCM_SR        0x010
-#define TIOCM_CTS       0x020
-#define TIOCM_CAR       0x040
-#define TIOCM_RNG       0x080
-#define TIOCM_DSR       0x100
-#define TIOCM_CD        TIOCM_CAR
-#define TIOCM_RI        TIOCM_RNG
-#define TIOCM_OUT1      0x2000
-#define TIOCM_OUT2      0x4000
-#define TIOCM_LOOP      0x8000
+#define TIOCM_LE	0x001
+#define TIOCM_DTR	0x002
+#define TIOCM_RTS	0x004
+#define TIOCM_ST	0x010
+#define TIOCM_SR	0x020
+#define TIOCM_CTS	0x040
+#define TIOCM_CAR	0x100
+#define TIOCM_CD	TIOCM_CAR
+#define TIOCM_RNG	0x200
+#define TIOCM_RI	TIOCM_RNG
+#define TIOCM_DSR	0x400
+#define TIOCM_OUT1	0x2000
+#define TIOCM_OUT2	0x4000
+#define TIOCM_LOOP	0x8000
 
 #define N_TTY           0
 #define N_SLIP          1
