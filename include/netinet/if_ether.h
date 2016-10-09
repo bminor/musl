9487cc5f2253b2fc82a3e19f3aaa0ade2a630de5
Szabolcs Nagy <nsz@port70.net>
2016-10-09 19:00:39 +0200

    add ETH_P_NCSI to netinet/if_ether.h from linux v4.8
    see linux commit 6389eaa7fa9c3ee6c7d39f6087b86660d17236ac

--- include/netinet/if_ether.h
+++ include/netinet/if_ether.h
@@ -55,6 +55,7 @@
 #define ETH_P_8021AH	0x88E7
 #define ETH_P_MVRP	0x88F5
 #define ETH_P_1588	0x88F7
+#define ETH_P_NCSI	0x88F8
 #define ETH_P_PRP	0x88FB
 #define ETH_P_FCOE	0x8906
 #define ETH_P_TDLS	0x890D
