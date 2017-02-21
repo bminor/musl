e6def544358afd5648a428d2e02c147a1f901048
Szabolcs Nagy <nsz@port70.net>
2017-02-21 00:07:34 +0000

    fix arm run-time abi string functions
    in arm rtabi these __aeabi_* functions have special abi (they are
    only allowed to clobber r0,r1,r2,r3,ip,lr,cpsr), so they cannot
    be simple wrappers around normal string functions (which may
    clobber other registers), the safest solution is to write them in
    asm, a minimalistic implementation works because these are not
    supposed to be emitted by compilers or used in general.

--- src/string/arm/__aeabi_memmove.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include "libc.h"
-
-void __aeabi_memmove(void *dest, const void *src, size_t n)
-{
-	memmove(dest, src, n);
-}
-weak_alias(__aeabi_memmove, __aeabi_memmove4);
-weak_alias(__aeabi_memmove, __aeabi_memmove8);
