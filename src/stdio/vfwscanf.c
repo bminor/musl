733d1ea759119bcd0554f25034d1b4113b910900
Rich Felker <dalias@aerifal.cx>
2017-03-14 15:06:58 -0400

    fix wide scanf's use of a compound literal past its lifetime

--- src/stdio/vfwscanf.c
+++ src/stdio/vfwscanf.c
@@ -214,11 +214,12 @@ int vfwscanf(FILE *restrict f, const wchar_t *restrict fmt, va_list ap)
 				set = L"";
 			} else if (t == 's') {
 				invert = 1;
-				set = (const wchar_t[]){
+				static const wchar_t spaces[] = {
 					' ', '\t', '\n', '\r', 11, 12,  0x0085,
 					0x2000, 0x2001, 0x2002, 0x2003, 0x2004, 0x2005,
 					0x2006, 0x2008, 0x2009, 0x200a,
 					0x2028, 0x2029, 0x205f, 0x3000, 0 };
+				set = spaces;
 			} else {
 				if (*++p == '^') p++, invert = 1;
 				else invert = 0;
