834ef7aff5695e79b1feeacfdc15eaba6a42cab9
Julien Ramseier <j.ramseier@gmail.com>
2017-03-21 12:30:45 -0400

    fix processing of strptime %p format
    string pointer was not advanced after matching.

--- src/time/strptime.c
+++ src/time/strptime.c
@@ -94,6 +94,7 @@ char *strptime(const char *restrict s, const char *restrict f, struct tm *restri
 			len = strlen(ex);
 			if (!strncasecmp(s, ex, len)) {
 				tm->tm_hour %= 12;
+				s += len;
 				break;
 			}
 			ex = nl_langinfo(PM_STR);
@@ -101,6 +102,7 @@ char *strptime(const char *restrict s, const char *restrict f, struct tm *restri
 			if (!strncasecmp(s, ex, len)) {
 				tm->tm_hour %= 12;
 				tm->tm_hour += 12;
+				s += len;
 				break;
 			}
 			return 0;
