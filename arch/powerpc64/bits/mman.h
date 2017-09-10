c10bc61508dc52b8315084e628f36a6c3c2dabb1
Szabolcs Nagy <nsz@port70.net>
2017-09-10 13:34:54 +0200

    powerpc{64}: fix MAP_NORESERVE and MAP_LOCKED in mman.h
    MAP_{NORESERVE,LOCKED} have different values on powerpc than in generic.

--- arch/powerpc64/bits/mman.h
+++ arch/powerpc64/bits/mman.h
@@ -1,5 +1,10 @@
 #define PROT_SAO       0x10
 
+#undef MAP_NORESERVE
+#define MAP_NORESERVE   0x40
+#undef MAP_LOCKED
+#define MAP_LOCKED	0x80
+
 #undef MCL_CURRENT
 #define MCL_CURRENT     0x2000
 #undef MCL_FUTURE
