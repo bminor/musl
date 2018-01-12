1bc10ffeaa7c7ce44b3e214e02e302642511c7c7
Rostislav Skudnov <rostislav@tuxera.com>
2018-01-12 17:06:03 +0000

    add _DIRENT_HAVE_D_* macros to dirent.h

--- include/dirent.h
+++ include/dirent.h
@@ -17,6 +17,10 @@ extern "C" {
 
 typedef struct __dirstream DIR;
 
+#define _DIRENT_HAVE_D_RECLEN
+#define _DIRENT_HAVE_D_OFF
+#define _DIRENT_HAVE_D_TYPE
+
 struct dirent {
 	ino_t d_ino;
 	off_t d_off;
