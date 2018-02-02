9b57db3f958d9adc3b1c7371b5c6723aaee448b7
Szabolcs Nagy <nsz@port70.net>
2018-02-02 20:10:09 +0000

    add MAP_SYNC and MAP_SHARED_VALIDATE from linux v4.15
    for synchronous page faults, new in linux commit
    1c9725974074a047f6080eecc62c50a8e840d050 and
    b6fb293f2497a9841d94f6b57bd2bb2cd222da43
    note that only targets that use asm-generic/mman.h have this new
    flag defined, so undef it on other targets (mips*, powerpc*).

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -20,6 +20,7 @@ extern "C" {
 
 #define MAP_SHARED     0x01
 #define MAP_PRIVATE    0x02
+#define MAP_SHARED_VALIDATE 0x03
 #define MAP_TYPE       0x0f
 #define MAP_FIXED      0x10
 #define MAP_ANON       0x20
@@ -33,6 +34,7 @@ extern "C" {
 #define MAP_NONBLOCK   0x10000
 #define MAP_STACK      0x20000
 #define MAP_HUGETLB    0x40000
+#define MAP_SYNC       0x80000
 #define MAP_FILE       0
 
 #define MAP_HUGE_SHIFT 26
