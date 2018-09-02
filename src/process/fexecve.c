e36f80cba6d5eefcc1ee664f16c2c72054b83134
Joseph C. Sible <josephcsible@gmail.com>
2018-09-02 13:42:26 -0400

    implement fexecve in terms of execveat when it exists
    This lets fexecve work even when /proc isn't mounted.

--- src/process/fexecve.c
+++ src/process/fexecve.c
@@ -1,10 +1,15 @@
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include "syscall.h"
 
 void __procfdname(char *, unsigned);
 
 int fexecve(int fd, char *const argv[], char *const envp[])
 {
+	int r = __syscall(SYS_execveat, fd, "", argv, envp, AT_EMPTY_PATH);
+	if (r != -ENOSYS) return __syscall_ret(r);
 	char buf[15 + 3*sizeof(int)];
 	__procfdname(buf, fd);
 	execve(buf, argv, envp);
