72bc6cbfed34f9787cc740e55c793ebbf3a2fe67
Rich Felker <dalias@aerifal.cx>
2018-09-07 22:48:19 -0400

    move __tm_to_tzname declaration to time_impl.h with related functions
    this function was added later for strftime use and the existence of
    time_impl.h as the appropriate place for it seems to have been
    overlooked.

--- src/time/strftime.c
+++ src/time/strftime.c
@@ -45,7 +45,6 @@ static int week_num(const struct tm *tm)
 	return val;
 }
 
-const char *__tm_to_tzname(const struct tm *);
 size_t __strftime_l(char *restrict, size_t, const char *restrict, const struct tm *restrict, locale_t);
 
 const char *__strftime_fmt_1(char (*s)[100], size_t *l, int f, const struct tm *tm, locale_t loc, int pad)
--- src/time/time_impl.h
+++ src/time/time_impl.h
@@ -4,6 +4,7 @@ int __days_in_month(int, int);
 int __month_to_secs(int, int);
 long long __year_to_secs(long long, int *);
 long long __tm_to_secs(const struct tm *);
+const char *__tm_to_tzname(const struct tm *);
 int __secs_to_tm(long long, struct tm *);
 void __secs_to_zone(long long, int, int *, long *, long *, const char **);
 const unsigned char *__map_file(const char *, size_t *);
