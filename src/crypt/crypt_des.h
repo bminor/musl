7ab1578d05f5d6790e218867158a28d9147419ec
Rich Felker <dalias@aerifal.cx>
2018-09-10 20:42:35 -0400

    add crypt_des header for declarations shared with encrypt function

---
+++ src/crypt/crypt_des.h
@@ -0,0 +1,14 @@
+#ifndef CRYPT_DES_H
+#define CRYPT_DES_H
+
+#include <stdint.h>
+
+struct expanded_key {
+	uint32_t l[16], r[16];
+};
+
+void __des_setkey(const unsigned char *, struct expanded_key *);
+void __do_des(uint32_t, uint32_t, uint32_t *, uint32_t *,
+              uint32_t, uint32_t, const struct expanded_key *);
+
+#endif
