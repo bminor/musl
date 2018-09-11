ebb6afde680fc420f85a124d548831d59650b611
Rich Felker <dalias@aerifal.cx>
2018-09-11 14:09:20 -0400

    remove unused __getdents, rename and move file
    the __-prefixed filename does not make sense when the only purpose of
    this file is implementing a public function that's not used as a
    backend for implementing the standard dirent functions.

--- src/linux/getdents.c
+++ src/linux/getdents.c
@@ -1,12 +1,9 @@
 #include <dirent.h>
 #include "syscall.h"
-#include "libc.h"
 
-int __getdents(int fd, struct dirent *buf, size_t len)
+int getdents(int fd, struct dirent *buf, size_t len)
 {
 	return syscall(SYS_getdents, fd, buf, len);
 }
-
-weak_alias(__getdents, getdents);
 
 LFS64(getdents);
