c04c6993dc87d596e47f2d8a66ef9252890b7468
Rich Felker <dalias@aerifal.cx>
2018-09-11 15:10:01 -0400

    declare __env_rm_add in wrapper stdlib.h
    this is perhaps not the ideal place, but no better alternatives stand
    out.

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -4,6 +4,7 @@
 #include "../../include/stdlib.h"
 
 int __putenv(char *, size_t, char *);
+void __env_rm_add(char *, char *);
 int __mkostemps(char *, int, int);
 int __ptsname_r(int, char *, size_t);
 char *__randname(char *);
