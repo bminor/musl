8cd738bbee3b4e60a99b71599c338bf444070f18
Rich Felker <dalias@aerifal.cx>
2018-09-23 02:51:01 -0400

    optimize nop case of wmemmove

--- src/string/wmemmove.c
+++ src/string/wmemmove.c
@@ -4,6 +4,7 @@
 wchar_t *wmemmove(wchar_t *d, const wchar_t *s, size_t n)
 {
 	wchar_t *d0 = d;
+	if (d == s) return d;
 	if ((uintptr_t)d-(uintptr_t)s < n * sizeof *d)
 		while (n--) d[n] = s[n];
 	else
