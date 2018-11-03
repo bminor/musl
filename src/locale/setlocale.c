bminor/musl

commit d88e5dfa8b989dafff4b748bfb3cba3512c8482e

adapt setlocale to support possibility of failure
introduce a new LOC_MAP_FAILED sentinel for errors, since null
pointers for a category's locale map indicate the C locale. at this
time, __get_locale does not fail, so there should be no functional
change by this commit.

src/locale/setlocale.c
@@ -7,19 +7,10 @@
 
 static char buf[LC_ALL*(LOCALE_NAME_MAX+1)];
 
-static char *setlocale_one_unlocked(int cat, const char *name)
-{
-	const struct __locale_map *lm;
-
-	if (name) libc.global_locale.cat[cat] = lm = __get_locale(cat, name);
-	else lm = libc.global_locale.cat[cat];
-
-	return lm ? (char *)lm->name : "C";
-}
-
 char *setlocale(int cat, const char *name)
 {
 	static volatile int lock[1];
+	const struct __locale_map *lm;
 
 	if ((unsigned)cat > LC_ALL) return 0;
 
@@ -33,6 +24,7 @@ char *setlocale(int cat, const char *name)
 	if (cat == LC_ALL) {
 		int i;
 		if (name) {
+			struct __locale_struct tmp_locale;
 			char part[LOCALE_NAME_MAX+1] = "C.UTF-8";
 			const char *p = name;
 			for (i=0; i<LC_ALL; i++) {
@@ -42,8 +34,14 @@ char *setlocale(int cat, const char *name)
 					part[z-p] = 0;
 					if (*z) p = z+1;
 				}
-				setlocale_one_unlocked(i, part);
+				lm = __get_locale(i, name);
+				if (lm == LOC_MAP_FAILED) {
+					UNLOCK(lock);
+					return 0;
+				}
+				tmp_locale.cat[i] = lm;
 			}
+			libc.global_locale = tmp_locale;
 		}
 		char *s = buf;
 		const char *part;
@@ -63,7 +61,17 @@ char *setlocale(int cat, const char *name)
 		return same==LC_ALL ? (char *)part : buf;
 	}
 
-	char *ret = setlocale_one_unlocked(cat, name);
+	if (name) {
+		lm = __get_locale(cat, name);
+		if (lm == LOC_MAP_FAILED) {
+			UNLOCK(lock);
+			return 0;
+		}
+		libc.global_locale.cat[cat] = lm;
+	} else {
+		lm = libc.global_locale.cat[cat];
+	}
+	char *ret = lm ? (char *)lm->name : "C";
 
 	UNLOCK(lock);
 

src/internal/locale_impl.h
@@ -27,6 +27,8 @@ hidden const char *__lctrans_impl(const char *, const struct __locale_map *);
 hidden int __loc_is_allocated(locale_t);
 hidden char *__gettextdomain(void);
 
+#define LOC_MAP_FAILED ((const struct __locale_map *)-1)
+
 #define LCTRANS(msg, lc, loc) __lctrans(msg, (loc)->cat[(lc)])
 #define LCTRANS_CUR(msg) __lctrans_cur(msg)
 

commit 04e18b61dfde85e34ddea15e4a7d49f24c47bb73

fix regression in setlocale for LC_ALL with per-category setting
commit d88e5dfa8b989dafff4b748bfb3cba3512c8482e inadvertently changed
the argument pased to __get_locale from part (the current ;-delimited
component) to name (the full string).

src/locale/setlocale.c
@@ -34,7 +34,7 @@ char *setlocale(int cat, const char *name)
 					part[z-p] = 0;
 					if (*z) p = z+1;
 				}
-				lm = __get_locale(i, name);
+				lm = __get_locale(i, part);
 				if (lm == LOC_MAP_FAILED) {
 					UNLOCK(lock);
 					return 0;