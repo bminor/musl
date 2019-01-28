1dd915c37090b32a6220488f99ce0f9be86eb46d
Bobby Bingham <koorogi@koorogi.info>
2019-01-27 22:02:51 -0600

    update line discipline constants

--- include/sys/ioctl.h
+++ include/sys/ioctl.h
@@ -22,6 +22,18 @@ extern "C" {
 #define N_HDLC          13
 #define N_SYNC_PPP      14
 #define N_HCI           15
+#define N_GIGASET_M101  16
+#define N_SLCAN         17
+#define N_PPS           18
+#define N_V253          19
+#define N_CAIF          20
+#define N_GSM0710       21
+#define N_TI_WL         22
+#define N_TRACESINK     23
+#define N_TRACEROUTER   24
+#define N_NCI           25
+#define N_SPEAKUP       26
+#define N_NULL          27
 
 #define TIOCPKT_DATA       0
 #define TIOCPKT_FLUSHREAD  1
