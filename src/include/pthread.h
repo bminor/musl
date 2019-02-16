bminor/musl

commit 84d061d5a31c9c773e29e1e2b1ffe8cb9557bc58

fix deletion of pthread tsd keys that still have non-null values stored
per POSIX, deletion of a key for which some threads still have values
stored is permitted, and newly created keys must initially hold the
null value in all threads. these properties were not met by our
implementation; if a key was deleted with values left and a new key
was created in the same slot, the old values were still visible.

moreover, due to lack of any synchronization in pthread_key_delete,
there was a TOCTOU race whereby a concurrent pthread_exit could
attempt to call a null destructor pointer for the newly orphaned
value.

this commit introduces a solution based on __synccall, stopping the
world to zero out the values for deleted keys, but only does so lazily
when all key slots have been exhausted. pthread_key_delete is split
off into a separate translation unit so that static-linked programs
which only create keys but never delete them will not pull in the
__synccall machinery.

a global rwlock is added to synchronize creation and deletion of keys
with dtor execution. since the dtor execution loop now has to release
and retake the lock around its call to each dtor, checks are made not
to call the nodtor dummy function for keys which lack a dtor.

src/thread/pthread_key_create.c
@@ -3,54 +3,123 @@
 volatile size_t __pthread_tsd_size = sizeof(void *) * PTHREAD_KEYS_MAX;
 void *__pthread_tsd_main[PTHREAD_KEYS_MAX] = { 0 };
 
-static void (*volatile keys[PTHREAD_KEYS_MAX])(void *);
+static void (*keys[PTHREAD_KEYS_MAX])(void *);
+
+static pthread_rwlock_t key_lock = PTHREAD_RWLOCK_INITIALIZER;
+
+static pthread_key_t next_key;
 
 static void nodtor(void *dummy)
 {
 }
 
+static void dirty(void *dummy)
+{
+}
+
+struct cleanup_args {
+	pthread_t caller;
+	int ret;
+};
+
+static void clean_dirty_tsd_callback(void *p)
+{
+	struct cleanup_args *args = p;
+	pthread_t self = __pthread_self();
+	pthread_key_t i;
+	for (i=0; i<PTHREAD_KEYS_MAX; i++) {
+		if (keys[i] == dirty && self->tsd[i])
+			self->tsd[i] = 0;
+	}
+	/* Arbitrary choice to avoid data race. */
+	if (args->caller == self) args->ret = 0;
+}
+
+static int clean_dirty_tsd(void)
+{
+	struct cleanup_args args = {
+		.caller = __pthread_self(),
+		.ret = EAGAIN
+	};
+	__pthread_key_delete_synccall(clean_dirty_tsd_callback, &args);
+	return args.ret;
+}
+
 int __pthread_key_create(pthread_key_t *k, void (*dtor)(void *))
 {
-	unsigned i = (uintptr_t)&k / 16 % PTHREAD_KEYS_MAX;
-	unsigned j = i;
+	pthread_key_t j = next_key;
 	pthread_t self = __pthread_self();
+	int found_dirty = 0;
 
 	/* This can only happen in the main thread before
 	 * pthread_create has been called. */
 	if (!self->tsd) self->tsd = __pthread_tsd_main;
 
+	/* Purely a sentinel value since null means slot is free. */
 	if (!dtor) dtor = nodtor;
+
+	pthread_rwlock_wrlock(&key_lock);
 	do {
-		if (!a_cas_p(keys+j, 0, (void *)dtor)) {
-			*k = j;
+		if (!keys[j]) {
+			keys[next_key = *k = j] = dtor;
+			pthread_rwlock_unlock(&key_lock);
 			return 0;
+		} else if (keys[j] == dirty) {
+			found_dirty = 1;
 		}
-	} while ((j=(j+1)%PTHREAD_KEYS_MAX) != i);
-	return EAGAIN;
+	} while ((j=(j+1)%PTHREAD_KEYS_MAX) != next_key);
+
+	/* It's possible that all slots are in use or __synccall fails. */
+	if (!found_dirty || clean_dirty_tsd()) {
+		pthread_rwlock_unlock(&key_lock);
+		return EAGAIN;
+	}
+
+	/* If this point is reached there is necessarily a newly-cleaned
+	 * slot to allocate to satisfy the caller's request. Find it and
+	 * mark any additional previously-dirty slots clean. */
+	for (j=0; j<PTHREAD_KEYS_MAX; j++) {
+		if (keys[j] == dirty) {
+			if (dtor) {
+				keys[next_key = *k = j] = dtor;
+				dtor = 0;
+			} else {
+				keys[j] = 0;
+			}
+		}
+	}
+
+	pthread_rwlock_unlock(&key_lock);
+	return 0;
 }
 
-int __pthread_key_delete(pthread_key_t k)
+int __pthread_key_delete_impl(pthread_key_t k)
 {
-	keys[k] = 0;
+	pthread_rwlock_wrlock(&key_lock);
+	keys[k] = dirty;
+	pthread_rwlock_unlock(&key_lock);
 	return 0;
 }
 
 void __pthread_tsd_run_dtors()
 {
 	pthread_t self = __pthread_self();
-	int i, j, not_finished = self->tsd_used;
-	for (j=0; not_finished && j<PTHREAD_DESTRUCTOR_ITERATIONS; j++) {
-		not_finished = 0;
+	int i, j;
+	for (j=0; self->tsd_used && j<PTHREAD_DESTRUCTOR_ITERATIONS; j++) {
+		pthread_rwlock_rdlock(&key_lock);
+		self->tsd_used = 0;
 		for (i=0; i<PTHREAD_KEYS_MAX; i++) {
-			if (self->tsd[i] && keys[i]) {
-				void *tmp = self->tsd[i];
-				self->tsd[i] = 0;
-				keys[i](tmp);
-				not_finished = 1;
+			void *val = self->tsd[i];
+			void (*dtor)(void *) = keys[i];
+			self->tsd[i] = 0;
+			if (val && dtor && dtor != nodtor && dtor != dirty) {
+				pthread_rwlock_unlock(&key_lock);
+				dtor(val);
+				pthread_rwlock_rdlock(&key_lock);
 			}
 		}
+		pthread_rwlock_unlock(&key_lock);
 	}
 }
 
-weak_alias(__pthread_key_delete, pthread_key_delete);
 weak_alias(__pthread_key_create, pthread_key_create);

src/internal/pthread_impl.h
@@ -148,6 +148,9 @@ hidden void __do_cleanup_push(struct __ptcb *);
 hidden void __do_cleanup_pop(struct __ptcb *);
 hidden void __pthread_tsd_run_dtors();
 
+hidden void __pthread_key_delete_synccall(void (*)(void *), void *);
+hidden int __pthread_key_delete_impl(pthread_key_t);
+
 extern hidden volatile int __block_new_threads;
 extern hidden volatile size_t __pthread_tsd_size;
 extern hidden void *__pthread_tsd_main[];

src/thread/pthread_key_delete.c
@@ -1,0 +1,14 @@
+#include "pthread_impl.h"
+#include "libc.h"
+
+void __pthread_key_delete_synccall(void (*f)(void *), void *p)
+{
+	__synccall(f, p);
+}
+
+int __pthread_key_delete(pthread_key_t k)
+{
+	return __pthread_key_delete_impl(k);
+}
+
+weak_alias(__pthread_key_delete, pthread_key_delete);

commit 639bcf251e549f634da9a3e7ef8528eb2ec12505

introduce namespace-safe rwlock aliases; use in pthread_key_create
commit 84d061d5a31c9c773e29e1e2b1ffe8cb9557bc58 inadvertently
introduced namespace violations by using the pthread-namespace rwlock
functions in pthread_key_create, which is in turn used for C11 tss.
fix that and possible future uses of rwlocks elsewhere.

src/include/pthread.h
@@ -18,5 +18,12 @@ hidden int __private_cond_signal(pthread_cond_t *, int);
 hidden int __pthread_cond_timedwait(pthread_cond_t *restrict, pthread_mutex_t *restrict, const struct timespec *restrict);
 hidden int __pthread_key_create(pthread_key_t *, void (*)(void *));
 hidden int __pthread_key_delete(pthread_key_t);
+hidden int __pthread_rwlock_rdlock(pthread_rwlock_t *);
+hidden int __pthread_rwlock_tryrdlock(pthread_rwlock_t *);
+hidden int __pthread_rwlock_timedrdlock(pthread_rwlock_t *__restrict, const struct timespec *__restrict);
+hidden int __pthread_rwlock_wrlock(pthread_rwlock_t *);
+hidden int __pthread_rwlock_trywrlock(pthread_rwlock_t *);
+hidden int __pthread_rwlock_timedwrlock(pthread_rwlock_t *__restrict, const struct timespec *__restrict);
+hidden int __pthread_rwlock_unlock(pthread_rwlock_t *);
 
 #endif

src/thread/pthread_key_create.c
@@ -32,16 +32,16 @@ int __pthread_key_create(pthread_key_t *k, void (*dtor)(void *))
 	/* Purely a sentinel value since null means slot is free. */
 	if (!dtor) dtor = nodtor;
 
-	pthread_rwlock_wrlock(&key_lock);
+	__pthread_rwlock_wrlock(&key_lock);
 	do {
 		if (!keys[j]) {
 			keys[next_key = *k = j] = dtor;
-			pthread_rwlock_unlock(&key_lock);
+			__pthread_rwlock_unlock(&key_lock);
 			return 0;
 		}
 	} while ((j=(j+1)%PTHREAD_KEYS_MAX) != next_key);
 
-	pthread_rwlock_unlock(&key_lock);
+	__pthread_rwlock_unlock(&key_lock);
 	return EAGAIN;
 }
 
@@ -57,9 +57,9 @@ int __pthread_key_delete(pthread_key_t k)
 	__tl_unlock();
 	__restore_sigs(&set);
 
-	pthread_rwlock_wrlock(&key_lock);
+	__pthread_rwlock_wrlock(&key_lock);
 	keys[k] = 0;
-	pthread_rwlock_unlock(&key_lock);
+	__pthread_rwlock_unlock(&key_lock);
 
 	return 0;
 }
@@ -69,19 +69,19 @@ void __pthread_tsd_run_dtors()
 	pthread_t self = __pthread_self();
 	int i, j;
 	for (j=0; self->tsd_used && j<PTHREAD_DESTRUCTOR_ITERATIONS; j++) {
-		pthread_rwlock_rdlock(&key_lock);
+		__pthread_rwlock_rdlock(&key_lock);
 		self->tsd_used = 0;
 		for (i=0; i<PTHREAD_KEYS_MAX; i++) {
 			void *val = self->tsd[i];
 			void (*dtor)(void *) = keys[i];
 			self->tsd[i] = 0;
 			if (val && dtor && dtor != nodtor) {
-				pthread_rwlock_unlock(&key_lock);
+				__pthread_rwlock_unlock(&key_lock);
 				dtor(val);
-				pthread_rwlock_rdlock(&key_lock);
+				__pthread_rwlock_rdlock(&key_lock);
 			}
 		}
-		pthread_rwlock_unlock(&key_lock);
+		__pthread_rwlock_unlock(&key_lock);
 	}
 }
 

src/thread/pthread_rwlock_rdlock.c
@@ -1,6 +1,8 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_rdlock(pthread_rwlock_t *rw)
+int __pthread_rwlock_rdlock(pthread_rwlock_t *rw)
 {
-	return pthread_rwlock_timedrdlock(rw, 0);
+	return __pthread_rwlock_timedrdlock(rw, 0);
 }
+
+weak_alias(__pthread_rwlock_rdlock, pthread_rwlock_rdlock);

src/thread/pthread_rwlock_timedrdlock.c
@@ -1,6 +1,6 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_timedrdlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
+int __pthread_rwlock_timedrdlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
 {
 	int r, t;
 
@@ -10,7 +10,7 @@ int pthread_rwlock_timedrdlock(pthread_rwlock_t *restrict rw, const struct times
 	int spins = 100;
 	while (spins-- && rw->_rw_lock && !rw->_rw_waiters) a_spin();
 
-	while ((r=pthread_rwlock_tryrdlock(rw))==EBUSY) {
+	while ((r=__pthread_rwlock_tryrdlock(rw))==EBUSY) {
 		if (!(r=rw->_rw_lock) || (r&0x7fffffff)!=0x7fffffff) continue;
 		t = r | 0x80000000;
 		a_inc(&rw->_rw_waiters);
@@ -21,3 +21,5 @@ int pthread_rwlock_timedrdlock(pthread_rwlock_t *restrict rw, const struct times
 	}
 	return r;
 }
+
+weak_alias(__pthread_rwlock_timedrdlock, pthread_rwlock_timedrdlock);

src/thread/pthread_rwlock_timedwrlock.c
@@ -1,6 +1,6 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_timedwrlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
+int __pthread_rwlock_timedwrlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
 {
 	int r, t;
 	
@@ -10,7 +10,7 @@ int pthread_rwlock_timedwrlock(pthread_rwlock_t *restrict rw, const struct times
 	int spins = 100;
 	while (spins-- && rw->_rw_lock && !rw->_rw_waiters) a_spin();
 
-	while ((r=pthread_rwlock_trywrlock(rw))==EBUSY) {
+	while ((r=__pthread_rwlock_trywrlock(rw))==EBUSY) {
 		if (!(r=rw->_rw_lock)) continue;
 		t = r | 0x80000000;
 		a_inc(&rw->_rw_waiters);
@@ -21,3 +21,5 @@ int pthread_rwlock_timedwrlock(pthread_rwlock_t *restrict rw, const struct times
 	}
 	return r;
 }
+
+weak_alias(__pthread_rwlock_timedwrlock, pthread_rwlock_timedwrlock);

src/thread/pthread_rwlock_tryrdlock.c
@@ -1,6 +1,6 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_tryrdlock(pthread_rwlock_t *rw)
+int __pthread_rwlock_tryrdlock(pthread_rwlock_t *rw)
 {
 	int val, cnt;
 	do {
@@ -11,3 +11,5 @@ int pthread_rwlock_tryrdlock(pthread_rwlock_t *rw)
 	} while (a_cas(&rw->_rw_lock, val, val+1) != val);
 	return 0;
 }
+
+weak_alias(__pthread_rwlock_tryrdlock, pthread_rwlock_tryrdlock);

src/thread/pthread_rwlock_trywrlock.c
@@ -1,7 +1,9 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_trywrlock(pthread_rwlock_t *rw)
+int __pthread_rwlock_trywrlock(pthread_rwlock_t *rw)
 {
 	if (a_cas(&rw->_rw_lock, 0, 0x7fffffff)) return EBUSY;
 	return 0;
 }
+
+weak_alias(__pthread_rwlock_trywrlock, pthread_rwlock_trywrlock);

src/thread/pthread_rwlock_unlock.c
@@ -1,6 +1,6 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_unlock(pthread_rwlock_t *rw)
+int __pthread_rwlock_unlock(pthread_rwlock_t *rw)
 {
 	int val, cnt, waiters, new, priv = rw->_rw_shared^128;
 
@@ -16,3 +16,5 @@ int pthread_rwlock_unlock(pthread_rwlock_t *rw)
 
 	return 0;
 }
+
+weak_alias(__pthread_rwlock_unlock, pthread_rwlock_unlock);

src/thread/pthread_rwlock_wrlock.c
@@ -1,6 +1,8 @@
 #include "pthread_impl.h"
 
-int pthread_rwlock_wrlock(pthread_rwlock_t *rw)
+int __pthread_rwlock_wrlock(pthread_rwlock_t *rw)
 {
-	return pthread_rwlock_timedwrlock(rw, 0);
+	return __pthread_rwlock_timedwrlock(rw, 0);
 }
+
+weak_alias(__pthread_rwlock_wrlock, pthread_rwlock_wrlock);