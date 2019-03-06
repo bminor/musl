292bda7ceb1033488af08ba0ae8df27e38e8eb00
Szabolcs Nagy <nsz@port70.net>
2019-03-06 22:24:05 +0000

    netinet/udp.h: add UDP_GRO from linux v5.0
    sockopt to enable gro for udp.
    added in linux commit e20cf8d3f1f763ad28a9cb3b41305b8a8a42653e

--- include/netinet/udp.h
+++ include/netinet/udp.h
@@ -27,6 +27,7 @@ struct udphdr {
 #define UDP_NO_CHECK6_TX 101
 #define UDP_NO_CHECK6_RX 102
 #define UDP_SEGMENT	103
+#define UDP_GRO		104
 
 #define UDP_ENCAP_ESPINUDP_NON_IKE 1
 #define UDP_ENCAP_ESPINUDP	2
