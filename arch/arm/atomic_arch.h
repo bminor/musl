bminor/musl

commit 06fbefd10046a0fae7e588b7c6d25fb51811b931

add a_clz_64 helper function
counts leading zero bits of a 64bit int, undefined on zero input.
(has nothing to do with atomics, added to atomic.h so target specific
helper functions are together.)

there is a logarithmic generic implementation and another in terms of
a 32bit a_clz_32 on targets where that's available.

arch/arm/atomic_arch.h
@@ -81,3 +81,10 @@ static inline void a_crash()
 #endif
 		: : : "memory");
 }
+
+#define a_clz_32 a_clz_32
+static inline int a_clz_32(uint32_t x)
+{
+	__asm__ ("clz %0, %1" : "=r"(x) : "r"(x));
+	return x;
+}

arch/aarch64/atomic_arch.h
@@ -73,3 +73,10 @@ static inline int a_ctz_64(uint64_t x)
 		: "=r"(x) : "r"(x));
 	return x;
 }
+
+#define a_clz_64 a_clz_64
+static inline int a_clz_64(uint64_t x)
+{
+	__asm__("clz %0, %1" : "=r"(x) : "r"(x));
+	return x;
+}

arch/i386/atomic_arch.h
@@ -99,3 +99,10 @@ static inline int a_ctz_l(unsigned long x)
 	__asm__( "bsf %1,%0" : "=r"(r) : "r"(x) );
 	return r;
 }
+
+#define a_clz_32 a_clz_32
+static inline int a_clz_32(uint32_t x)
+{
+	__asm__( "bsr %1,%0 ; xor $31,%0" : "=r"(x) : "r"(x) );
+	return x;
+}

arch/powerpc/atomic_arch.h
@@ -37,3 +37,10 @@ static inline void a_store(volatile int *p, int v)
 	*p = v;
 	a_post_llsc();
 }
+
+#define a_clz_32 a_clz_32
+static inline int a_clz_32(uint32_t x)
+{
+	__asm__ ("cntlzw %0, %1" : "=r"(x) : "r"(x));
+	return x;
+}

arch/powerpc64/atomic_arch.h
@@ -61,3 +61,10 @@ static inline void a_crash()
 {
 	__asm__ __volatile__ (".long 0");
 }
+
+#define a_clz_64 a_clz_64
+static inline int a_clz_64(uint64_t x)
+{
+	__asm__ ("cntlzd %0, %1" : "=r"(x) : "r"(x));
+	return x;
+}

arch/x32/atomic_arch.h
@@ -112,3 +112,10 @@ static inline int a_ctz_l(unsigned long x)
 	__asm__( "bsf %1,%0" : "=r"(x) : "r"(x) );
 	return x;
 }
+
+#define a_clz_64 a_clz_64
+static inline int a_clz_64(uint64_t x)
+{
+	__asm__( "bsr %1,%0 ; xor $63,%0" : "=r"(x) : "r"(x) );
+	return x;
+}

arch/x86_64/atomic_arch.h
@@ -114,3 +114,10 @@ static inline int a_ctz_64(uint64_t x)
 	__asm__( "bsf %1,%0" : "=r"(x) : "r"(x) );
 	return x;
 }
+
+#define a_clz_64 a_clz_64
+static inline int a_clz_64(uint64_t x)
+{
+	__asm__( "bsr %1,%0 ; xor $63,%0" : "=r"(x) : "r"(x) );
+	return x;
+}

src/internal/atomic.h
@@ -277,6 +277,27 @@ static inline int a_ctz_64(uint64_t x)
 }
 #endif
 
+#ifndef a_clz_64
+#define a_clz_64 a_clz_64
+static inline int a_clz_64(uint64_t x)
+{
+#ifdef a_clz_32
+	if (x>>32)
+		return a_clz_32(x>>32);
+	return a_clz_32(x) + 32;
+#else
+	uint32_t y;
+	int r;
+	if (x>>32) y=x>>32, r=0; else y=x, r=32;
+	if (y>>16) y>>=16; else r |= 16;
+	if (y>>8) y>>=8; else r |= 8;
+	if (y>>4) y>>=4; else r |= 4;
+	if (y>>2) y>>=2; else r |= 2;
+	return r | !(y>>1);
+#endif
+}
+#endif
+
 #ifndef a_ctz_l
 #define a_ctz_l a_ctz_l
 static inline int a_ctz_l(unsigned long x)

commit 980f80f79216c268c18fa36b7680c076c684dfa2

fix build failure on arm building C code in thumb1 mode
a fully thumb1 build is not supported because some asm files are
incompatible with thumb1, but apparently it works to compile the C
code as thumb1

commit 06fbefd10046a0fae7e588b7c6d25fb51811b931 caused this regression
but introducing use of the clz instruction, which is not supported in
arm mode prior to v5, and not supported in thumb prior to thumb2
(v6t2). commit 1b9406b03c0a94ebe2076a8fc1746a8c45e78a83 fixed the
issue only for arm mode pre-v5 but left thumb1 broken.

arch/arm/atomic_arch.h
@@ -83,7 +83,7 @@ static inline void a_crash()
 		: : : "memory");
 }
 
-#if __ARM_ARCH >= 5
+#if __ARM_ARCH >= 5 && (!__thumb__ || __thumb2__)
 
 #define a_clz_32 a_clz_32
 static inline int a_clz_32(uint32_t x)