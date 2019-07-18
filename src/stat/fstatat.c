01ae3fc6d48f4a45535189b7a6db286535af08ca
Rich Felker <dalias@aerifal.cx>
2019-07-18 19:38:12 -0400

    decouple struct stat from kernel type
    presently, all archs/ABIs have struct stat matching the kernel
    stat[64] type, except mips/mipsn32/mips64 which do conversion hacks in
    syscall_arch.h to work around bugs in the kernel type. this patch
    completely decouples them and adds a translation step to the success
    path of fstatat. at present, this is just a gratuitous copying, but it
    opens up multiple possibilities for future support for 64-bit time_t
    on 32-bit archs and for cleaned-up/unified ABIs.
    
    for clarity, the mips hacks are not yet removed in this commit, so the
    mips kstat structs still correspond to the output of the hacks in
    their syscall_arch.h files, not the raw kernel type. a subsequent
    commit will fix this.

--- src/stat/fstatat.c
+++ src/stat/fstatat.c
@@ -4,10 +4,12 @@
 #include <fcntl.h>
 #include <errno.h>
 #include "syscall.h"
+#include "kstat.h"
 
 int fstatat(int fd, const char *restrict path, struct stat *restrict st, int flag)
 {
 	int ret;
+	struct kstat kst;
 
 	if (flag==AT_EMPTY_PATH && fd>=0 && !*path) {
 		ret = __syscall(SYS_fstat, fd, st);
@@ -26,15 +28,36 @@ int fstatat(int fd, const char *restrict path, struct stat *restrict st, int fla
 	}
 #ifdef SYS_lstat
 	else if ((fd == AT_FDCWD || *path=='/') && flag==AT_SYMLINK_NOFOLLOW)
-		ret = __syscall(SYS_lstat, path, st);
+		ret = __syscall(SYS_lstat, path, &kst);
 #endif
 #ifdef SYS_stat
 	else if ((fd == AT_FDCWD || *path=='/') && !flag)
-		ret = __syscall(SYS_stat, path, st);
+		ret = __syscall(SYS_stat, path, &kst);
 #endif
-	else ret = __syscall(SYS_fstatat, fd, path, st, flag);
+	else ret = __syscall(SYS_fstatat, fd, path, &kst, flag);
 
-	return __syscall_ret(ret);
+	if (ret) return __syscall_ret(ret);
+
+	*st = (struct stat){
+		.st_dev = kst.st_dev,
+		.st_ino = kst.st_ino,
+		.st_mode = kst.st_mode,
+		.st_nlink = kst.st_nlink,
+		.st_uid = kst.st_uid,
+		.st_gid = kst.st_gid,
+		.st_rdev = kst.st_rdev,
+		.st_size = kst.st_size,
+		.st_blksize = kst.st_blksize,
+		.st_blocks = kst.st_blocks,
+		.st_atim.tv_sec = kst.st_atime_sec,
+		.st_atim.tv_nsec = kst.st_atime_nsec,
+		.st_mtim.tv_sec = kst.st_mtime_sec,
+		.st_mtim.tv_nsec = kst.st_mtime_nsec,
+		.st_ctim.tv_sec = kst.st_ctime_sec,
+		.st_ctim.tv_nsec = kst.st_ctime_nsec,
+	};
+
+	return 0;
 }
 
 weak_alias(fstatat, fstatat64);
