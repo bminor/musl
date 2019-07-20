107d68ad1737929a3e815bd28514cb56e4bedd57
Rich Felker <dalias@aerifal.cx>
2019-07-20 17:02:49 -0400

    refactor adjtimex in terms of clock_adjtime
    this sets the stage for having the conversion logic for 64-bit time_t
    all in one file, and as a bonus makes clock_adjtime for CLOCK_REALTIME
    work even on kernels too old to have the clock_adjtime syscall.

--- src/linux/clock_adjtime.c
+++ src/linux/clock_adjtime.c
@@ -1,7 +1,9 @@
 #include <sys/timex.h>
+#include <time.h>
 #include "syscall.h"
 
 int clock_adjtime (clockid_t clock_id, struct timex *utx)
 {
+	if (clock_id==CLOCK_REALTIME) return syscall(SYS_adjtimex, utx);
 	return syscall(SYS_clock_adjtime, clock_id, utx);
 }
