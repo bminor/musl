331993e3fc3623f111d95796d3d7f30b4f6552c1
Rich Felker <dalias@aerifal.cx>
2019-07-21 01:53:14 -0400

    refactor thrd_sleep and nanosleep in terms of clock_nanosleep
    for namespace-safety with thrd_sleep, this requires an alias, which is
    also added. this eliminates all but one direct call point for
    nanosleep syscalls, and arranges that 64-bit time_t conversion logic
    will only need to exist in one file rather than three.
    
    as a bonus, clock_nanosleep with CLOCK_REALTIME and empty flags is now
    implemented as SYS_nanosleep, thereby working on older kernels that
    may lack POSIX clocks functionality.

--- src/time/clock_nanosleep.c
+++ src/time/clock_nanosleep.c
@@ -2,8 +2,12 @@
 #include <errno.h>
 #include "syscall.h"
 
-int clock_nanosleep(clockid_t clk, int flags, const struct timespec *req, struct timespec *rem)
+int __clock_nanosleep(clockid_t clk, int flags, const struct timespec *req, struct timespec *rem)
 {
-	int r = -__syscall_cp(SYS_clock_nanosleep, clk, flags, req, rem);
-	return clk == CLOCK_THREAD_CPUTIME_ID ? EINVAL : r;
+	if (clk == CLOCK_THREAD_CPUTIME_ID) return EINVAL;
+	if (clk == CLOCK_REALTIME && !flags)
+		return -__syscall_cp(SYS_nanosleep, req, rem);
+	return -__syscall_cp(SYS_clock_nanosleep, clk, flags, req, rem);
 }
+
+weak_alias(__clock_nanosleep, clock_nanosleep);
