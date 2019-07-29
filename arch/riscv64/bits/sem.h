de5e56ce1626882dddfd5a7a900c0723af02d6ab
Rich Felker <dalias@aerifal.cx>
2019-07-29 18:17:43 -0400

    remove duplicates of new generic bits/sem.h
    some of these were not exact duplicates, but had gratuitously
    different naming for padding, or omitted the endian checks because the
    arch is fixed-endian.

--- arch/riscv64/bits/sem.h
+++
@@ -1,9 +0,0 @@
-struct semid_ds {
-	struct ipc_perm sem_perm;
-	time_t sem_otime;
-	time_t sem_ctime;
-	unsigned short sem_nsems;
-	char __sem_nsems_pad[sizeof(long)-sizeof(short)];
-	long __unused3;
-	long __unused4;
-};
