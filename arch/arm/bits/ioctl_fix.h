b60fdf133c033d4ad6b04a8237f253563fae5928
Rich Felker <dalias@aerifal.cx>
2019-11-01 20:22:41 -0400

    move time64 ioctl numbers to generic bits/ioctl.h
    now that all 32-bit archs have 64-bit time types, the values for the
    time-related ioctls can be shared. the mechanism for this is an
    arch/generic version of the bits header. archs which don't use the
    generic header still need to duplicate the definitions.
    
    x32, which does not use the new time64 values of the macros, already
    has its own overrides, so this commit does not affect it.

--- arch/arm/bits/ioctl_fix.h
+++ arch/arm/bits/ioctl_fix.h
@@ -1,7 +1,2 @@
 #undef FIOQSIZE
 #define FIOQSIZE 0x545e
-
-#undef SIOCGSTAMP
-#undef SIOCGSTAMPNS
-#define SIOCGSTAMP      _IOR(0x89, 6, char[16])
-#define SIOCGSTAMPNS    _IOR(0x89, 7, char[16])
--- arch/i386/bits/ioctl_fix.h
+++
@@ -1,4 +0,0 @@
-#undef SIOCGSTAMP
-#undef SIOCGSTAMPNS
-#define SIOCGSTAMP      _IOR(0x89, 6, char[16])
-#define SIOCGSTAMPNS    _IOR(0x89, 7, char[16])
