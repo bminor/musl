64d0e86576ef1d33e996a926d6a02d38fb88a768
Rich Felker <dalias@aerifal.cx>
2019-12-19 10:47:10 -0500

    improve ioctl time64 conversion fallback framework
    record offsets of individual slots that expand from 32- to 64-bit,
    rather than timespec/timeval pairs. this flexibility will be needed
    for some ioctls. reduce size of types in table. adjust representation
    of offsets to include a count rather than needing -1 padding so that
    the table is less ugly and doesn't need large diffs if we increase max
    number of slots.

--- src/misc/ioctl.c
+++ src/misc/ioctl.c
@@ -14,14 +14,18 @@
 #define WR 3
 
 struct ioctl_compat_map {
-	int new_req, old_req, old_size;
-	char dir, force_align;
-	int offsets[4];
+	int new_req, old_req;
+	unsigned char old_size, dir, force_align, noffs;
+	unsigned char offsets[8];
 };
 
+#define NINTH(a,b,c,d,e,f,g,h,i,...) i
+#define COUNT(...) NINTH(__VA_ARGS__,8,7,6,5,4,3,2,1,0)
+#define OFFS(...) COUNT(__VA_ARGS__), { __VA_ARGS__ }
+
 static const struct ioctl_compat_map compat_map[] = {
-	{ SIOCGSTAMP, SIOCGSTAMP_OLD, 8, R, 0, { 0, -1, -1, -1 } },
-	{ SIOCGSTAMPNS, SIOCGSTAMPNS_OLD, 8, R, 0, { 0, -1, -1, -1 } },
+	{ SIOCGSTAMP, SIOCGSTAMP_OLD, 8, R, 0, OFFS(0, 4) },
+	{ SIOCGSTAMPNS, SIOCGSTAMPNS_OLD, 8, R, 0, OFFS(0, 4) },
 };
 
 static void convert_ioctl_struct(const struct ioctl_compat_map *map, char *old, char *new, int dir)
@@ -30,28 +34,25 @@ static void convert_ioctl_struct(const struct ioctl_compat_map *map, char *old, 
 	int old_offset = 0;
 	int old_size = map->old_size;
 	if (!(dir & map->dir)) return;
-	for (int i=0; i < sizeof map->offsets / sizeof *map->offsets; i++) {
+	for (int i=0; i < map->noffs; i++) {
 		int ts_offset = map->offsets[i];
-		if (ts_offset < 0) break;
 		int len = ts_offset-old_offset;
 		if (dir==W) memcpy(old+old_offset, new+new_offset, len);
 		else memcpy(new+new_offset, old+old_offset, len);
 		new_offset += len;
 		old_offset += len;
-		long long new_ts[2];
-		long old_ts[2];
+		long long new_ts;
+		long old_ts;
 		int align = map->force_align ? sizeof(time_t) : alignof(time_t);
 		new_offset += (align-1) & -new_offset;
 		if (dir==W) {
-			memcpy(new_ts, new+new_offset, sizeof new_ts);
-			old_ts[0] = new_ts[0];
-			old_ts[1] = new_ts[1];
-			memcpy(old+old_offset, old_ts, sizeof old_ts);
+			memcpy(&new_ts, new+new_offset, sizeof new_ts);
+			old_ts = new_ts;
+			memcpy(old+old_offset, &old_ts, sizeof old_ts);
 		} else {
-			memcpy(old_ts, old+old_offset, sizeof old_ts);
-			new_ts[0] = old_ts[0];
-			new_ts[1] = old_ts[1];
-			memcpy(new+new_offset, new_ts, sizeof new_ts);
+			memcpy(&old_ts, old+old_offset, sizeof old_ts);
+			new_ts = old_ts;
+			memcpy(new+new_offset, &new_ts, sizeof new_ts);
 		}
 		new_offset += sizeof new_ts;
 		old_offset += sizeof old_ts;
