ef137da6428c342baabd3bcf9b5e91f75acefa64
Sören Tempel <soeren+git@soeren-tempel.net>
2020-01-29 12:20:07 +0100

    cuserid: support invocation with a null pointer argument
    this function was removed from the standard in 2001 but appeared in
    SUSv2 with an obligation to support calls with a null pointer
    argument, using a static buffer.

--- src/legacy/cuserid.c
+++ src/legacy/cuserid.c
@@ -5,10 +5,12 @@
 
 char *cuserid(char *buf)
 {
+	static char usridbuf[L_cuserid];
 	struct passwd pw, *ppw;
 	long pwb[256];
 	if (getpwuid_r(geteuid(), &pw, (void *)pwb, sizeof pwb, &ppw))
 		return 0;
+	if (!buf) buf = usridbuf;
 	snprintf(buf, L_cuserid, "%s", pw.pw_name);
 	return buf;
 }
