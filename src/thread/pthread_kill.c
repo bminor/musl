52ee0dd6d5ccefcbf9b55748896c85801bfafd17
Rich Felker <dalias@aerifal.cx>
2020-07-06 18:46:57 -0400

    fix async-cancel-safety of pthread_cancel
    the previous commit addressing async-signal-safety issues around
    pthread_kill did not fully fix pthread_cancel, which is also required
    (albeit rather irrationally) to be async-cancel-safe.
    
    without blocking implementation-internal signals, it's possible that,
    when async cancellation is enabled, a cancel signal sent by another
    thread interrupts pthread_kill while the killlock for a targeted
    thread is held. as a result, the calling thread will terminate due to
    cancellation without ever unlocking the targeted thread's killlock,
    and thus the targeted thread will be unable to exit.

--- src/thread/pthread_kill.c
+++ src/thread/pthread_kill.c
@@ -5,7 +5,10 @@ int pthread_kill(pthread_t t, int sig)
 {
 	int r;
 	sigset_t set;
-	__block_app_sigs(&set);
+	/* Block not just app signals, but internal ones too, since
+	 * pthread_kill is used to implement pthread_cancel, which
+	 * must be async-cancel-safe. */
+	__block_all_sigs(&set);
 	LOCK(t->killlock);
 	r = t->tid ? -__syscall(SYS_tkill, t->tid, sig)
 		: (sig+0U >= _NSIG ? EINVAL : 0);
