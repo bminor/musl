2c887f24da36fa30eccd72e50d91222828fa526e
Stefan O'Rear <sorear@fastmail.com>
2020-09-03 05:23:40 -0400

    riscv: fall back to syscall __riscv_flush_icache
    Matches glibc behavior and fixes a case where we could fall off the
    function without returning a value.

--- src/linux/cache.c
+++ src/linux/cache.c
@@ -45,6 +45,7 @@ int __riscv_flush_icache(void *start, void *end, unsigned long int flags)
 		if (!r) return r;
 		if (r != -ENOSYS) return __syscall_ret(r);
 	}
+	return syscall(SYS_riscv_flush_icache, start, end, flags);
 }
 weak_alias(__riscv_flush_icache, riscv_flush_icache);
 #endif
