3437e478ba932edbab18a90638c20be1f0141156
Rich Felker <dalias@aerifal.cx>
2020-10-26 18:06:18 -0400

    fix reintroduction of errno clobbering by atfork handlers
    commit bd153422f28634bb6e53f13f80beb8289d405267 reintroduced the bug
    fixed in c21051e90cd27a0b26be0ac66950b7396a156ba1 by refactoring the
    __syscall_ret into _Fork where it once again runs before the atfork
    handlers are called. since _Fork is a public interface that sets
    errno, this can't be fixed the way it was fixed last time without
    making new internal interfaces. instead, just save errno, and restore
    it only on error to ensure that a value of 0 is never restored.

--- src/process/fork.c
+++ src/process/fork.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <errno.h>
 #include "libc.h"
 
 static void dummy(int x) { }
@@ -8,6 +9,8 @@ pid_t fork(void)
 {
 	__fork_handler(-1);
 	pid_t ret = _Fork();
+	int errno_save = errno;
 	__fork_handler(!ret);
+	if (ret<0) errno = errno_save;
 	return ret;
 }
