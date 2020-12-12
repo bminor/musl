db981ffb3e242e32e13520fbaf47e91ae88d5ff4
Ariadne Conill <ariadne@dereferenced.org>
2020-12-12 04:30:53 +0000

    sh: fix incorrect mcontext_t member naming
    while the layouts match, the member member naming expected by software
    using mcontext_t omits the sc_ prefix.

--- arch/sh/pthread_arch.h
+++ arch/sh/pthread_arch.h
@@ -8,9 +8,9 @@ static inline uintptr_t __get_tp()
 #define TLS_ABOVE_TP
 #define GAP_ABOVE_TP 8
 
-#define MC_PC sc_pc
+#define MC_PC pc
 
 #ifdef __FDPIC__
-#define MC_GOT sc_regs[12]
+#define MC_GOT gregs[12]
 #define CANCEL_GOT (*(uintptr_t *)((char *)__syscall_cp_asm+sizeof(uintptr_t)))
 #endif
