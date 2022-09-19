f081d5336a80b68d3e1bed789cc373c5c3d6699b
Rich Felker <dalias@aerifal.cx>
2022-09-19 19:02:40 -0400

    fix return value of gethostnbyname[2]_r on result not found
    these functions are horribly underspecified, inconsistent between
    historical systems, and should never have been included. however, the
    signatures we have match the glibc ones, and the glibc behavior is to
    treat NxDomain and NODATA results as a success condition, not an
    ENOENT error.

--- src/network/gethostbyname2_r.c
+++ src/network/gethostbyname2_r.c
@@ -22,7 +22,7 @@ int gethostbyname2_r(const char *name, int af,
 	if (cnt<0) switch (cnt) {
 	case EAI_NONAME:
 		*err = HOST_NOT_FOUND;
-		return ENOENT;
+		return 0;
 	case EAI_AGAIN:
 		*err = TRY_AGAIN;
 		return EAGAIN;
