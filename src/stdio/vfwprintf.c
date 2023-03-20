d055e6a45a17673b8dd3ec16e786bb2fe1700dd5
Rich Felker <dalias@aerifal.cx>
2023-03-20 13:48:50 -0400

    fix wide printf forms ignoring width for %lc format specifier
    since the code path for %c was already doing it right, and the logic
    is identical, condense them into a single case.

--- src/stdio/vfwprintf.c
+++ src/stdio/vfwprintf.c
@@ -258,15 +258,12 @@ static int wprintf_core(FILE *f, const wchar_t *fmt, va_list *ap, union arg *nl_
 			}
 			continue;
 		case 'c':
+		case 'C':
 			if (w<1) w=1;
 			if (w>1 && !(fl&LEFT_ADJ)) fprintf(f, "%*s", w-1, "");
-			fputwc(btowc(arg.i), f);
+			fputwc(t=='C' ? arg.i : btowc(arg.i), f);
 			if (w>1 && (fl&LEFT_ADJ)) fprintf(f, "%*s", w-1, "");
 			l = w;
-			continue;
-		case 'C':
-			fputwc(arg.i, f);
-			l = 1;
 			continue;
 		case 'S':
 			a = arg.p;
