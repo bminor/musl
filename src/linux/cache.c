bd3138b74a06dd764508f399d1b58cb9d6c35c1f
gns <infiwang@proton.me>
2024-01-25 23:59:52 +0800

    riscv: correct symbol version of __vdso_flush_icache
    Previously, __riscv_flush_icache would not work correctly as
    __vdso_flush_icache had a wrong symbol version. Fix this by correcting
    symbol version.
    
    Fixes: 0a48860c27a8 ("add riscv64 architecture support")

--- src/linux/cache.c
+++ src/linux/cache.c
@@ -21,7 +21,7 @@ weak_alias(__cachectl, cachectl);
 #ifdef SYS_riscv_flush_icache
 
 #define VDSO_FLUSH_ICACHE_SYM "__vdso_flush_icache"
-#define VDSO_FLUSH_ICACHE_VER "LINUX_4.5"
+#define VDSO_FLUSH_ICACHE_VER "LINUX_4.15"
 
 static void *volatile vdso_func;
 
